7d772cc08b923943a621921be9c49eb44abf1004
rohitsraj12 <rohitsraj12@gmail.com>
2020-06-29 23:57:41 +0530

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,52 @@
+<?php
+    $page_title = "home page";
+
+?>
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title><?= $page_title;?> | faculty4you.com</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <div class="body-wrap">
+            <div class="body-header">
+                <div class="hamberger" id="hamberger">
+
+                </div>
+                <header>
+                    
+                </header>
+            </div>
+            <!-- end body header -->
+
+            <div class="body-banner">
+                
+            </div>
+            <!-- end body banner -->
+
+            <div class="body-container"></div>
+            <!-- end body container -->
+
+            <div class="body-footer"></div>
+            <!-- end body footer -->
+
+        </div>
+            <!-- end body wrap -->
+
+            <!-- script -->
+        <script src="" async defer></script>
+    </body>
+</html>
