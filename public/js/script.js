088ec29adb18f392c0e9c8e65fc497104ace8919
rohitsraj12 <rohitsraj12@gmail.com>
2020-07-27 16:41:43 +0530

    Update script.js

---
+++ public/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $(".hamberger").click(function () {
+    $(this).toggleClass("is__active");
+    $(".header__nav ul").slideToggle(400);
+  });
+  // testimonial-slider
+
+  $(".section-testimonial .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive: {
+      582: {
+        items: 3,
+      },
+
+      0: {
+        items: 1,
+      },
+    },
+  });
+});
