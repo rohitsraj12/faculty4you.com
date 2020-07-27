cfbe25aacde2ab1275bc77c2fcc2730c2a263890
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-07-28 00:03:39 +0530

    Update index.php

--- public/student/profile/index.php
+++ public/student/profile/index.php
@@ -1,78 +1,14 @@
 <?php
     $page_title = "profile";
 
-    require("../../private/config/config.php");
+    require("../../../private/config/config.php");
+    require("../include/header.php");
 ?>
 
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title><?= $page_title;?> | faculty4you.com</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- stylesheet -->
-        <link
-        href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-        rel="stylesheet"
-        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-        crossorigin="anonymous"
-        />
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/owl.carousel.css">
-        <link rel="stylesheet" href="css/owl.theme.default.css">
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-        
-        <div class="body-wrap">
-            <div class="body-header" data-aos="zoom-out-down" data-aos-duration="1000">
-                <div id="hamberger" class="hamberger">
-                    <span class="hamberger__line"></span>
-                    <span class="hamberger__line"></span>
-                    <span class="hamberger__line"></span>
-                </div>
-                <!-- end hamberger -->
-                <header class="wrap-container">
-                    <div class="header-brand">
-                        <a href="<?php base_url();?>index.php">
-                            <img src="img/brand/header-logo.png" alt="faculty 4 you">
-                        </a>
-                    </div>
-                    <!-- end header brand -->
-                    <nav class="header__nav">
-                        <ul>
-                            <li class="nav__list"><a href="" class="nav__link">become teacher</a></li>
-                            <li class="nav__list"><a href="" class="nav__link">log in</a></li>
-                            <li class="nav__list"><a href="" class="nav__link button-primary">sign up</a></li>
-                        </ul>
-                    </nav>
-                    <!-- end header nav -->
-                </header>
-            </div>
-            <!-- end body header -->
 
 
-            <div class="body-footer"></div>
-            <!-- end body footer -->
-        </div>
-        <!-- end body wrap -->
 
-        <!-- script -->
-            <script src="js/jquery-3.5.1.js"></script>
-            <script src="js/owl.carousel.js"></script>
-            <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-            <script src="js/script.js"></script>
-            
-            <script>
-                AOS.init();
-              </script>
-    </body>
-</html>
+
+<?php 
+    require("../include/footer.php");
+?>
