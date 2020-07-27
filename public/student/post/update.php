4f277f60a5e6402ddb849673056fe062bee7c4d7
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-07-28 00:03:32 +0530

    Create update.php

---
+++ public/student/post/update.php
@@ -0,0 +1,14 @@
+<?php
+    $page_title = "post update";
+
+    require("../../../private/config/config.php");
+    require("../include/header.php");
+?>
+
+
+
+
+
+<?php 
+    require("../include/footer.php");
+?>
