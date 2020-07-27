5760c589cf26ae878bfefdf267d4cd67e8ca2ae7
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-07-28 00:03:43 +0530

    Create post.php

---
+++ public/student/post/post.php
@@ -0,0 +1,14 @@
+<?php
+    $page_title = "profile";
+
+    require("../../../private/config/config.php");
+    require("../include/header.php");
+?>
+
+
+
+
+
+<?php 
+    require("../include/footer.php");
+?>
