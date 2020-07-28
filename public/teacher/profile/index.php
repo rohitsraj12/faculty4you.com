cc17ed4224f45000af08ba6b2cb23aff340c1c87
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-07-28 23:51:44 +0530

    Update index.php

--- public/teacher/profile/index.php
+++ public/teacher/profile/index.php
@@ -8,18 +8,113 @@
 
 <div class="body-container">
     <main class="wrap-container profile">
-        <section class="section-short-info">
-            <article>
-                <figure>
-                    <img src="" alt="">
-                </figure>
-                <header class="u-center-text">
-                    <h1 class="text-secondary">fsdf</h1>
+        <section class="section-profile">
+            <div class="section-header u-center-text"  data-aos="zoom-out-up" data-aos-duration="1000">
+                <heeader class="text-primary"> 
+                    my profile
                 </header>
-            </article>
-        </section>
-        <section class="section-detail-info">
-        
+                
+            </div>
+            <div class="section-body">
+                <section class="section-short-info">
+                    <article class="article-profile" data-aos="zoom-out-up" data-aos-duration="1000">
+                        <figure>
+                            <img src="" alt="">
+                        </figure>
+                        <header class="u-center-text">
+                            <h1 class="text-secondary">firstname lastname</h1>
+                        </header>
+                        <footer>
+                            <ul>
+                                <li>
+                                email
+                                </li>
+                                <li>
+                                phone
+                                </li>
+                            </ul>
+                        </footer>
+                    </article>
+                </section>
+                <section class="section-detail-info">
+                    <article class="article-profile" data-aos="zoom-out-up" data-aos-duration="1000">
+                        <header class="article__header">
+                            about me
+                        </header>
+                        <div class="article-body">
+                        <p>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, ipsa!
+                        </p>
+                        </div>
+                    </article>
+                    <article class="article-profile" data-aos="zoom-out-up" data-aos-duration="1000">
+                        <header class="article__header">
+                            personal detail
+                        </header>
+                        <div class="article-body">
+                            <div class="article-info">
+                                <ul>
+                                    <li>name</li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                            
+                            <div class="article-info">
+                                <ul>
+                                    <li>phone</li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                            
+                            <div class="article-info">
+                                <ul>
+                                    <li>email</li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                            <div class="article-info">
+                                <ul>
+                                    <li>address</li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </article>
+                    <article class="article-profile" data-aos="zoom-out-up" data-aos-duration="1000">
+                        <header class="article__header">
+                            professional detail
+                        </header>
+                        <div class="article-body">
+                        
+                        <div class="article-info">
+                                <ul>
+                                    <li>experience</li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                            
+                            <div class="article-info">
+                                <ul>
+                                    <li>tuition type</li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                            
+                            <div class="article-info">
+                                <ul>
+                                    <li>subject</li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </article>
+                </section>
+            </div>
         </section>
     </main>
 </div>
