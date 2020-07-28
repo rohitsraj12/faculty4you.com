aba28655f7c273a131e9df4202e8c824da55d5ab
rohitsraj12 <rohitsraj12@gmail.com>
2020-07-29 01:41:21 +0530

    Update index.php

--- public/teacher/index.php
+++ public/teacher/index.php
@@ -1,72 +1,26 @@
 <?php
 
+    require_once('../../private/config/db_connect.php');
+
     session_start();
     //back function false
     if(!isset($_SESSION['user_name'])){
         header('location: login.php');
-    } 
-
-
+    }
 
     $page_title = "profile";
     include_once("../../private/config/config.php");
+        include_once'include/header.inc.php';
+    while($row = mysqli_fetch_assoc($result)){
 ?>
 
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title><?= $page_title;?> | faculty4you.com</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- stylesheet -->
-        <link
-        href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-        rel="stylesheet"
-        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-        crossorigin="anonymous"
-        />
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-        <link rel="stylesheet" href="<?php base_url();?>css/owl.carousel.css">
-        <link rel="stylesheet" href="<?php base_url();?>css/owl.theme.default.css">
-        <link rel="stylesheet" href="<?php base_url();?>css/style.css">
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-        
-        <div class="body-wrap">
-            <div class="body-header" data-aos="zoom-out-down" data-aos-duration="1000">
-                <div id="hamberger" class="hamberger">
-                    <span class="hamberger__line"></span>
-                    <span class="hamberger__line"></span>
-                    <span class="hamberger__line"></span>
-                </div>
-                <!-- end hamberger -->
-                <header class="wrap-container">
-                    <div class="header-brand">
-                        <a href="<?php base_url();?>index.php">
-                            <img src="<?php base_url();?>img/brand/header-logo.png" alt="faculty 4 you">
-                        </a>
-                    </div>
-                    <!-- end header brand -->
-                    <nav class="header__nav">
-                        <ul>
-                            <li class="nav__list"><a href="<?php base_url();?>" class="nav__link">home</a></li>
-                            <li class="nav__list"><a href="<?php base_url();?>" class="nav__link">profile</a></li>
-                            <li class="nav__list"><a href="<?php base_url();?>" class="nav__link">contact</a></li>
-                        </ul>
-                    </nav>
-                    <!-- end header nav -->
-                </header>
-            </div>
-            <!-- end body header -->
-
+                
+<div class="header__profile u-right-text text-sub-primary">
+    <i class="fa fa-user" aria-hidden="true"></i>                        
+    <?php 
+        echo $row['teacher_user_name'];
+    ?>
+</div>
             <div class="body-banner" data-aos="zoom-out-up" data-aos-duration="1000">
 
                 <div class="banner-wrap">
@@ -99,18 +53,11 @@
             </div>
             <!-- end body banner -->
 
-                    <!-- script -->
-                    <script src="<?php base_url();?>js/jquery-3.5.1.js"></script>
-            <script src="<?php base_url();?>js/owl.carousel.js"></script>
-            <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-            <script src="<?php base_url();?>js/script.js"></script>
-            
-            <script>
-                AOS.init();
-              </script>
-    </body>
-</html>
+            <div class="body-container">
+
+            </div>
 
 <?php 
-    
+    }
+     require("include/footer.inc.php");    
 ?>
