47458328e8c832530937f0bca96c5eb95acb8193
rohitsraj12 <rohitsraj12@gmail.com>
2020-07-28 14:50:12 +0530

    Create banner.public.php

---
+++ private/required/public/banner/banner.public.php
@@ -0,0 +1,33 @@
+
+            <div class="body-banner" data-aos="zoom-out-up" data-aos-duration="1000">
+
+<div class="banner-wrap">
+    <section class="banner-content">
+        <article>
+            <header class="banner-content__header">
+                <h2 class="text-primary">Faculty4you take learning  to new heights</h2>
+                <h5 class="text-secondary">
+                    we provide top <strong>teachers</strong> for <strong class="highlight-primary">online</strong> and <strong class="highlight-primary">home tution</strong> all over india    
+                </h5>
+            </header>
+            <div class="banner-content-body">
+                
+                <form action="<?php base_url();?>search_result.php" method="POST" class="banner__form">
+                    <input type="text" name="search" placeholder="search top teachers city / subject / category"  class="banner-search" >
+                    <button type="submit" name="submit-search" class="banner__button" ><i class="fa fa-search"aria-hidden="true"></i></button>
+                </form>
+                <p>
+                    <span class="highlight-secondary">Current  trending courses:</span> <strong class="note-primary">IIT-JEE</strong>, <strong class="note-primary">NEET</strong>, <strong class="note-primary">Dance</strong>, <strong class="note-primary">Yoga</strong>
+                </p>
+            </div>
+        </article>
+    </section>
+    <!-- end banner content -->
+    <div class="banner-image">
+        <img src="<?php base_url();?>img/banner/teacher.svg" class="banner__image" alt="">
+    </div>
+    <!-- end banner imager -->
+</div>
+<!-- end banner wrap -->
+</div>
+<!-- end body banner -->
