a3732c6dd389bd506da249b87da3013692767c2c
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-07-28 18:00:23 +0530

    Update index.php

--- public/student/post/index.php
+++ public/student/post/index.php
@@ -1,100 +1,14 @@
 <?php
-    $page_title = "home page";
+    $page_title = "post";
 
-    include_once("../private/config/config.php");
+    require("../../private/config/config.php");
+    require("../include/header.php");
 ?>
 
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title><?= $page_title;?> | faculty4you.com</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- stylesheet -->
-        <link
-        href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-        rel="stylesheet"
-        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-        crossorigin="anonymous"
-        />
-        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-        <link rel="stylesheet" href="<?php base_url();?>css/owl.carousel.css">
-        <link rel="stylesheet" href="<?php base_url();?>css/owl.theme.default.css">
-        <link rel="stylesheet" href="<?php base_url();?>css/style.css">
-    </head>
-<body>
-<div class="body-wrap">
-            <div class="body-header" data-aos="zoom-out-down" data-aos-duration="1000">
-                <div id="hamberger" class="hamberger">
-                    <span class="hamberger__line"></span>
-                    <span class="hamberger__line"></span>
-                    <span class="hamberger__line"></span>
-                </div>
-                <!-- end hamberger -->
-                <header class="wrap-container">
-                    <div class="header-brand">
-                        <a href="<?php base_url();?>student/index.php">
-                            <img src="<?php base_url();?>img/brand/header-logo.png" alt="faculty4you">
-                        </a>
-                    </div>
-                    <!-- end header brand -->
-                    <nav class="header__nav">
-                        <ul>
-                            <li class="nav__list"><a href="<?php base_url();?>student/index.php" class="nav__link">home</a></li>
-                            <li class="nav__list"><a href="" class="nav__link">post</a></li>
-                            <li class="nav__list"><a href="" class="nav__link">log out</a></li>
-                        </ul>
-                    </nav>
-                    <!-- end header nav -->
-                </header>
-            </div>
-            <!-- end body header -->
 
-            <div class="body-banner" data-aos="zoom-out-up" data-aos-duration="1000">
 
-                <div class="banner-wrap">
-                    <section class="banner-content">
-                        <article>
-                            <header class="banner-content__header">
-                                <h2 class="text-primary">Faculty4you take learning  to new heights</h2>
-                                <h5 class="text-secondary">
-                                    we provide top <strong>teachers</strong> for <strong class="highlight-primary">online</strong> and <strong class="highlight-primary">home tution</strong> all over india    
-                                </h5>
-                            </header>
-                            <div class="banner-content-body">
-                                <form action="student/login.php" class="banner__form">
-                                    <input type="search" placeholder="search top teachers city / subject / category" class="banner-search" name="search">
-                                    <button class="banner__button" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
-                                </form>
-                                <p>
-                                    <span class="highlight-secondary">Current  trending courses:</span> <strong class="note-primary">IIT-JEE</strong>, <strong class="note-primary">NEET</strong>, <strong class="note-primary">Dance</strong>, <strong class="note-primary">Yoga</strong>
-                                </p>
-                            </div>
-                        </article>
-                    </section>
-                    <!-- end banner content -->
-                    <div class="banner-image">
-                        <img src="<?php base_url();?>img/banner/teacher.svg" class="banner__image" alt="">
-                    </div>
-                    <!-- end banner image -->
-                </div>
-                <!-- end banner wrap -->
-            </div>
-            <!-- end body banner -->
-              <!-- script -->
-              <script src="<?php base_url();?>js/jquery-3.5.1.js"></script>
-            <script src="<?php base_url();?>js/owl.carousel.js"></script>
-            <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-            <script src="<?php base_url();?>js/script.js"></script>
-            
-            <script>
-                AOS.init();
-              </script>
 
-</body>
-</html>
+
+<?php 
+    require("../include/footer.php");
+?>
