5379785c1c5de61e4113e0302a56e9d4f9d9ca3d
rohitsraj12 <rohitsraj12@gmail.com>
2020-07-28 20:55:52 +0530

    Update login.query.php

--- public/teacher/include/login.query.php
+++ public/teacher/include/login.query.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 if(isset($_POST['login-submit'])){
     require_once '../../../private/config/db_connect.php';
 
@@ -7,14 +8,14 @@ if(isset($_POST['login-submit'])){
 
     if(empty($mail_user_name) || empty($password)){
 
-        header("Location: ../login.php?error=emptyfields");
+        header("Location: ../teacher/teacher_login.php?error=emptyfields");
         exit();
     } else {
         $sql = "SELECT * FROM teachers WHERE teacher_user_name=? OR teacher_email=?";
         $stmt = mysqli_stmt_init($conn);
 
         if(!mysqli_stmt_prepare($stmt, $sql)){
-            header("Location: ../login.php?error=sqlerror");
+            header("Location: ../teacher/teacher_login.php?error=sqlerror");
             exit();
         } else {
 
@@ -23,35 +24,35 @@ if(isset($_POST['login-submit'])){
             $result = mysqli_stmt_get_result($stmt);
 
             if($row = mysqli_fetch_assoc($result)){
-                $password_check = password_verify($password, $row['user_password']);
+                $password_check = password_verify($password, $row['teacher_password']);
                 //$password_check = $row['user_password'];
 
                 if($password_check == false) {
                     
-                    header("Location: ../login.php?error=wrongPassword");
+                    header("Location: ../teacher/teacher_login.php?error=wrongPassword");
                     exit();
                 } else if($password_check == true){
                     session_start();
-                    $_SESSION["teacher_id"] = $row['id'];
-                    $_SESSION["teacher_first_name"] = $row['user_name'];
+                    $_SESSION["user_id"] = $row['teacher_id'];
+                    $_SESSION["user_name"] = $row['teacher_user_name'];
 
                     
                     header("Location: ../index.php");
                     exit();
                 } else {
                     
-                    header("Location: ../login.php?error=wrongPassword");
+                    header("Location: ../teacher/teacher_login.php?error=wrongPassword");
                     exit();
                 }
 
             } else {
-                header("Location: ../login.php?error=nouser");
+                header("Location: ../teacher/teacher_login.php?error=nouser");
                 exit();
             }
         }
     }
 
 } else {
-    header("Location: ../login.php");
+    header("Location: login.php");
     exit();
 }
