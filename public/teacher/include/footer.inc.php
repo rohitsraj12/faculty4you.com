ff4a3590d22c83a0dbad33e2a27dd5d0c76906a5
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-07-28 18:00:40 +0530

    Update footer.inc.php

---
+++ public/teacher/include/footer.inc.php
@@ -0,0 +1,11 @@
+         <!-- script -->
+         <script src="<?php base_url();?>js/jquery-3.5.1.js"></script>
+            <script src="<?php base_url();?>js/owl.carousel.js"></script>
+            <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+            <script src="<?php base_url();?>js/script.js"></script>
+            
+            <script>
+                AOS.init();
+              </script>
+    </body>
+</html>
