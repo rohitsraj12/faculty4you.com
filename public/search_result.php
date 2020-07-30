0c0ea0f57b94c12da8aa0d3e04fd26bd14eb054b
rohitsraj12 <rohitsraj12@gmail.com>
2020-07-30 16:28:59 +0530

    Update search_result.php

--- public/search_result.php
+++ public/search_result.php
@@ -1,5 +1,10 @@
 <?php 
     
+    // session_start();
+    // //back function false
+    // if(!isset($_SESSION['user_name'])){
+    //     header('location: login.php');
+    // }
     $page_title = "home page";
     require_once("../private/config/db_connect.php");
     include("../private/config/config.php");
@@ -11,7 +16,7 @@
 <div class="body-container">
 
     <main>
-        <section class="section-result wrap-container">
+        <section class="section-search-result wrap-container">
 
 <?php
 if(isset($_POST["submit-search"])){
@@ -27,53 +32,53 @@ if(isset($_POST["submit-search"])){
             <heeader class="text-primary"> 
                 search result
             </header>
-            <p class="u-padding-top-big u-left-text">
-                <?php 
-                        echo "there are ". $query_results . " results are matching";
-                ?>
+        </div>
+        
+        <div class="search-result-num" data-aos="zoom-out-up" data-aos-duration="1000">
+            <p>
+                    <?php echo $query_results; ?> results are matching
             </p>
         </div>
+        <section class="section-body"> 
     <?php 
-
-   
 
     if($query_results > 0){
         while($row = mysqli_fetch_assoc($result)){
 
             ?>      
             
-            
-          <article>
-            <div class="left__block">
-                <figure>
-                    <img src="" alt="">
-                </figure>
-            </div>
-            <div class="right__block">
-                <header>
-                    <h1>
-                        <?php echo $row['teacher_user_name'];?>
-                    </h1>
-                </header>
-                <div class="body">
-                    <p>
-                        <?php echo $row['teacher_about_me'];?>
-                    </p>
-                    <ul>
-                        <li>qualification: </li>
-                        <li>Experience: </li>
-                        <li>City:</li>
-                        <li><?php if($row["teacher_online_one_to_one"] == 1){echo "Online one to one";} ?> / <?php if($row["teacher_online_group"] == 1){echo "Online group";}?>/ <?php if($row["teacher_home_tuition"] == 1){echo "home tuition";}?> </li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </div>
-                
-                <footer>
-                <a href="detail.php?name=<?php echo $row['teacher_user_name']?>&batch=<?php echo $row['teacher_email']?>">more details</a>
-                </footer>
-            </div>
-          </article>
+                <article class="article-search-result" data-aos="zoom-out-up" data-aos-duration="1000">
+                    <div class="block-left">
+                        <figure class="article-search__figure">
+                            <img src="<?php base_url()?>img/teacher/profile_pic/rohit.jpg" alt="">
+                        </figure>
+                    </div>
+                    <div class="block-right">
+                        <header class="article-search__header">
+                            <h1 class="text-secondary">
+                                <?php echo $row['teacher_user_name'];?>
+                            </h1>
+                        </header>
+                        <div class="article-search-body">
+                                <ul>
+                                    <li>qualification: <strong></strong></li>
+                                    <li>Experience: <strong></strong></li>
+                                    <li>City:<strong></strong></li>
+                                    <li>available for : 
+                                        <strong><?php if($row["teacher_online_one_to_one"] == 1){echo "Online one to one";} ?></strong>
+                                        <strong> <?php if($row["teacher_online_group"] == 1){echo "Online group";}?></strong>
+                                        <strong> <?php if($row["teacher_home_tuition"] == 1){echo "home tuition";}?> </strong>
+                                    
+                                    </li>
+                                    
+                                </ul>
+                        </div>
+                        
+                        <footer class="article-search-footer">
+                            <a class="button-text-1" href="detail.php?name=<?php echo $row['teacher_user_name']?>&batch=<?php echo $row['teacher_email']?>">more details</a>
+                        </footer>
+                    </div>
+                </article>
             
             
             
@@ -81,11 +86,21 @@ if(isset($_POST["submit-search"])){
 
         }
     } else {
-        echo "there are no result";
+        // echo "there are no result";
+        ?> 
+        
+        <div class="search-result-num" data-aos="zoom-out-up" data-aos-duration="1000">
+            <p>
+                there are no result
+            </p>
+        </div>
+
+        <?php
     }
 }
- 
-?>       </section>
+?>      
+    </section> 
+</section>
     </main>
 </div>
 
