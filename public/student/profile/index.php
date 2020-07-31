3ef4b5ab211f10f86edf328931f41051b78eae39
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-01 00:27:45 +0530

    Update index.php

--- public/student/profile/index.php
+++ public/student/profile/index.php
@@ -30,122 +30,161 @@
                 </header>
                 
             </div>
-            <div class="section-body">
-                <section class="section-short-info">
-                    <article class="article-profile" data-aos="zoom-out-up" data-aos-duration="1000">
-                        <figure>
-                            <img src="" alt="">
+        
+            <div class="section-body row">
+                <section class="col-md-4">
+                    <article class="article-profil" data-aos="zoom-out-up" data-aos-duration="1000">
+                        <figure class="text-center">
+                            <img src="<?php base_url()?>img/teacher/profile_pic/rohit.jpg" alt="<?php echo $row['student_first_name'];?>">
                         </figure>
-                        <header class="u-center-text">
-                            <h1 class="text-secondary">
+                        <header class=" u-center-text">
+                            <h1 class="text-dark py-5">
                             <?php 
                                 echo $row['student_first_name'];
                             ?>
                             </h1>
                         </header>
-                        <footer>
+                        <footer class="px-5">
                             <ul>
-                                <li>
+                                <li class="text-dark h4 py-1 font-weight-normal">
+                                <i class="h2 fa fa-mobile pr-3" aria-hidden="true"></i>
                                 
                             <?php 
                                 echo $row['student_email'];
                             ?>
                                 </li>
-                                <li>
-                                
+                                <li  class="text-dark h4 pb-4 font-weight-normal">
+                                <i class="h2 fa fa-mobile  pr-3" aria-hidden="true"></i>
                             <?php 
-                                echo $row['student_phone'];
+                                echo $row['student_phone']; 
                             ?>
+                                </li>
+
+                                <li class="text-center">
+                                    <a href="<?php base_url()?>student/profile/profile_update.php" class="w-100 h4 button-primary">edit profile</a>
                                 </li>
                             </ul>
                         </footer>
                     </article>
                 </section>
-                <section class="section-detail-info">
-                    <article class="article-profile" data-aos="zoom-out-up" data-aos-duration="1000">
-                        <header class="article__header">
-                            about me
+                <section class="col-md-8">
+                    
+                    <article class="article-profil py-5" data-aos="zoom-out-up" data-aos-duration="1000">
+                        <header class="p-4 h3 bg-dark text-light  m-0">
+                            Personal detail
                         </header>
-                        <div class="article-body">
-                        <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus, ipsa!
-                        </p>
-                        </div>
-                    </article>
-                    <article class="article-profile" data-aos="zoom-out-up" data-aos-duration="1000">
-                        <header class="article__header">
-                            personal detail
-                        </header>
-                        <div class="article-body">
+                        <div class="article-body p-4 bg-light border">
                             <div class="article-info">
-                                <ul>
-                                    <li>
-                                    <li>name</li>
+                                <ul class="row">
+                                    <li class="col-sm-2">name</li>
+                                    <li class="col-sm-10 h4 font-weight-normal">
                                         <?php 
                                             echo $row['student_first_name'] . " " . $row['student_last_name'];
                                         ?>
                                     </li>
                                 </ul>
                             </div>
-                            
                             <div class="article-info">
-                                <ul>
-                                    <li>phone</li>
-                                    <li></li>
-                                </ul>
-                            </div>
-                            
-                            <div class="article-info">
-                                <ul>
-                                    <li>email</li>
-                                    <li>
-                                        
-                                
-                            <?php 
-                                echo $row['student_email'];
-                            ?>
+                                <ul class="row">
+                                    <li class="col-sm-2">date of birth</li>
+                                    <li class="col-sm-10 h4 font-weight-normal">
+                                        <?php 
+                                            echo $row['student_date_of_birth'];
+                                        ?>
                                     </li>
                                 </ul>
                             </div>
                             <div class="article-info">
-                                <ul>
-                                    <li>address</li>
-                                    <li></li>
-                                    <li></li>
-                                    <li></li>
-                                    <li></li>
+                                <ul class="row">
+                                    <li class="col-sm-2">gender</li>
+                                    <li class="col-sm-10 h4 font-weight-normal">
+                                        <?php 
+                                            // echo $row['student_first_name'];
+                                            echo "female";
+                                        ?>
+                                    </li>
                                 </ul>
                             </div>
-                        </div>
+                            
+                                     </div>
                     </article>
-                    <article class="article-profile" data-aos="zoom-out-up" data-aos-duration="1000">
-                        <header class="article__header">
-                            professional detail
+                    <article class="article-profil" data-aos="zoom-out-up" data-aos-duration="1000">
+                        <header class="p-4 h3 bg-dark text-light  m-0">
+                            contact detail
                         </header>
-                        <div class="article-body">
-                        
+                        <div class="article-body p-4 border">
                         <div class="article-info">
-                                <ul>
-                                    <li>experience</li>
-                                    <li></li>
+                                <ul class="row">
+                                    <li class="col-sm-2">phone</li>
+                                    <li class="col-sm-10  h4 font-weight-normal">
+                                    <?php echo $row['student_phone']?>
+                                    </li>
                                 </ul>
                             </div>
                             
                             <div class="article-info">
-                                <ul>
-                                    <li>tuition type</li>
+                                <ul class="row">
+                                    <li class="col-sm-2">email</li>
+                                    <li class="col-sm-10 h4 font-weight-normal">
+                                        <?php 
+                                            echo $row['student_email'];
+                                        ?>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="article-info">
+                                <ul class="row">
+                                    <li class="col-sm-2">parent name</li>
+                                    <li class="col-sm-10 h4 font-weight-normal">
+                                        <?php 
+                                            echo $row['student_parent_name'];
+                                        ?>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="article-info">
+                                <ul class="row">
+                                    <li class="col-sm-2">parent phone</li>
+                                    <li class="col-sm-10 h4 font-weight-normal">
+                                        <?php 
+                                            echo $row['student_parent_phone'];
+                                        ?>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div class="article-info">
+                                <ul class="row">
+                                    <li class="col-sm-2">address</li>
+                                    
+                                    <li class="col-sm-10  h4 font-weight-normal">
+                                        <?php 
+                                            echo $row['student_address'];
+                                        ?>, 
+                                        <?php 
+                                            echo $row['city_id'];
+                                        ?>, 
+                                        <?php 
+                                            echo $row['state_id'];
+                                        ?>, 
+                                        <?php 
+                                            echo $row['student_city_pincode'];
+                                        ?>
+                                    </li>
+                                </ul>
+                            </div>
+  
+                          
+                            <div class="article-info">
+                                <ul class="row">
+                                    <li class="col-sm-4">tuition type</li>
                                     <li></li>
                                     <li></li>
                                     <li></li>
                                 </ul>
                             </div>
                             
-                            <div class="article-info">
-                                <ul>
-                                    <li>subject</li>
-                                    <li></li>
-                                </ul>
-                            </div>
+                           
                         </div>
                     </article>
                 </section>
