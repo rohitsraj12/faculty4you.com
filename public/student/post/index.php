1785e8763c085fbb846b75e9eaa214df828bb28b
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-02 00:57:46 +0530

    Update index.php

--- public/student/post/index.php
+++ public/student/post/index.php
@@ -22,12 +22,13 @@
         require("../include/banner.inc.php");
 
     ?>
-
+       
     <div class="body-container">
-        <main>
-            <section>
-            
-            
+                <main>
+                    <section class="wrap-container">
+                        <header class="text-primary-h text-center">
+                            students Post
+                        </header>
         <?php 
 
         $student_id = $row['student_id'];
@@ -36,43 +37,42 @@
         // $query = "SELECT posts.*, cities.city_name, states.state_name FROM posts LEFT JOIN cities ON posts.city_id = cities.city_id AND RIGHT JOIN states ON posts.state_id = states.state_id WHERE student_id = $student_id";
         // $query = "SELECT posts.*, cities.city_name FROM posts LEFT JOIN cities ON posts.city_id = cities.city_id WHERE student_id = $student_id";
         
-        $query = "SELECT posts.*, cities.city_name, states.state_name 
+        $query = "SELECT posts.*, study_types.study_type_name, study_categories.study_cat_type 
                     FROM posts
-                        JOIN cities
-                            ON cities.city_id = posts.city_id
-                        JOIN states
-                            ON states.state_id = posts.state_id
-                        WHERE student_id = $student_id";
+                    JOIN study_types
+                        ON study_types.study_type_id = posts.study_type_id
+                    JOIN study_categories
+                        ON study_categories.study_cat_id = posts.study_cat_id
+                    WHERE student_id = $student_id
+                    ORDER BY post_id DESC";
 
         $result = mysqli_query($conn, $query);
 
             while($row = mysqli_fetch_assoc($result)){
             ?>
-                <article>
-                <div class="right__block">
-                    <header>
-                        <h1>
-                            <?php echo $row['post_title'];?>
-                        </h1>
-                        <a href=""><?php echo $row['city_name'] ;?></a>
-                        <a href=""><?php echo $row['state_name'] ;?></a>
-                    </header>
-                    <div class="body">
-                        <p>
-                            <?php echo $row['post_detail'];?>
+                
+          <article class="mt-5 px-5 py-3 border bg-light">
+                        <header class="border-bottom">
+                            <h1 class="h1 py-3 text-dark font-weight-normal">
+                                <?php echo $row["post_title"];?>
+                            </h1>
+                        </header>
+                        <div class="body mb-4">
+                            <ul class="d-flex flex-row bd-highlight py-4 h4 font-weight-normal text-secondary">
+                                <li class="mr-5"><i class="fa fa-calendar mr-2" aria-hidden="true"></i><?php echo $row["post_date"];?></li>
+                                <li class="mr-5"><i class="fa fa-graduation-cap mr-2" aria-hidden="true"></i><?php echo $row["study_type_name"];?></li>
+                                <li class="mr-5"><i class="fa fa-university mr-2" aria-hidden="true"></i><?php echo $row["study_cat_type"];?></li>
+                                <!-- <li class="mr-5"><i class="fa fa-map-marker mr-2" aria-hidden="true"></i><?php// echo $row["city_name"];?></li> -->
+                            </ul>
+                        <p class="text-dark">
+                            <?php echo $row["post_detail"];?>
                         </p>
-                        <ul>
-                            <li>qualification: <?php $row['post_date'] ?> </li>
-                            <li>Experience: </li>
-                            
-                        </ul>
-                    </div>
+                        </div>
+                        <footer class="pb-3">
+                            <buttom style="font-size: 1.6rem" class="py-2 px-4 btn btn-primary">Edit post</buttom>
+                        </footer>
+                    </article>
                     
-                    <footer>
-                    <a href="detail.php?id=<?php echo $row['post_id']?>&title=<?php echo $row['post_title']?>">more details</a>
-                    </footer>
-                </div>
-          </article>
           <?php
             }
         ?>
