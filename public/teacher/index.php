7e0b8e97c8686512861edec3aa6a02407930aff0
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-02 00:58:06 +0530

    Update index.php

--- public/teacher/index.php
+++ public/teacher/index.php
@@ -10,7 +10,7 @@
 
     $page_title = "profile";
     include_once("../../private/config/config.php");
-        include_once'include/header.inc.php';
+        include_once('include/header.inc.php');
 ?>
 
                 
@@ -39,31 +39,28 @@
                         while($row = mysqli_fetch_assoc($result)){
                     ?>
 
-                    <article class="mt-5 px-5 py-3 border bg-light">
-                        <header class="border-bottom">
-                            <h1 class="h1 py-3 text-dark font-weight-normal">
-                                <?php echo $row["post_title"];?>
-                            </h1>
-                        </header>
-                        <div class="body mb-4">
-                            <ul class="d-flex flex-row bd-highlight py-4 h4 font-weight-normal text-secondary">
-                                <li class="mr-5"><i class="fa fa-calendar mr-2" aria-hidden="true"></i><?php echo $row["post_date"];?></li>
-                                <li class="mr-5"><i class="fa fa-graduation-cap mr-2" aria-hidden="true"></i><?php echo $row["study_type_name"];?></li>
-                                <li class="mr-5"><i class="fa fa-university mr-2" aria-hidden="true"></i><?php echo $row["study_cat_type"];?></li>
-                                <li class="mr-5"><i class="fa fa-map-marker mr-2" aria-hidden="true"></i><?php echo $row["city_name"];?></li>
-                            </ul>
-                        <p class="text-dark">
-                            <?php echo $row["post_detail"];?>
-                        </p>
-                        </div>
-                        <footer class="pb-3">
-                            <buttom style="font-size: 1.6rem" class="py-2 px-4 btn btn-primary">Contact details</buttom>
-                        </footer>
-                    </article>
-                    
-
-
- 
+                        <article class="mt-5 px-5 py-3 border bg-light">
+                            <header class="border-bottom">
+                                <h1 class="h1 py-3 text-dark font-weight-normal">
+                                    <?php echo $row["post_title"];?>
+                                </h1>
+                            </header>
+                            <div class="body mb-4">
+                                <ul class="d-flex flex-row bd-highlight py-4 h4 font-weight-normal text-secondary">
+                                    <li class="mr-5"><i class="fa fa-calendar mr-2" aria-hidden="true"></i><?php echo $row["post_date"];?></li>
+                                    <li class="mr-5"><i class="fa fa-graduation-cap mr-2" aria-hidden="true"></i><?php echo $row["study_type_name"];?></li>
+                                    <li class="mr-5"><i class="fa fa-university mr-2" aria-hidden="true"></i><?php echo $row["study_cat_type"];?></li>
+                                    <li class="mr-5"><i class="fa fa-map-marker mr-2" aria-hidden="true"></i><?php echo $row["city_name"];?></li>
+                                </ul>
+                            <p class="text-dark">
+                                <?php echo $row["post_detail"];?>
+                            </p>
+                            </div>
+                            <footer class="pb-3">
+                                <buttom style="font-size: 1.6rem" class="py-2 px-4 btn btn-primary">Contact details</buttom>
+                            </footer>
+                        </article>
+                        
                     <?php 
                         }
                     ?>
