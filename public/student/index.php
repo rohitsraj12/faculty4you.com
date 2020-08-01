9a8e5690e9c9f7d3a1fead72e4956d58b8a55693
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-01 23:09:33 +0530

    Update index.php

--- public/student/index.php
+++ public/student/index.php
@@ -1,15 +1,32 @@
 <?php
+    session_start();
+    //back function false
+    if(!isset($_SESSION['user_name'])){
+        header('location: ../login.php');
+    } 
     $page_title = "home page";
 
+    include_once("../../private/config/db_connect.php");
     include_once("../../private/config/config.php");
     require("include/header.inc.php");
-    require("include/banner.inc.php");
     
-?>
 
 
+    ?>
+    
+                    
+        <div class="header__profile u-right-text text-sub-primary">
+            <i class="fa fa-user" aria-hidden="true"></i>                        
+            <?php 
+               echo $row['student_user_name'];
+                
+            ?>
+        </div>
+    <?php 
+        require("include/banner.inc.php");
 
-            <div class="body-container">
+    ?>
+    <div class="body-container">
             <main></main>
             </div>
             <!-- end body container -->
