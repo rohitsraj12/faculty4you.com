2b0565c9951985ae5d4863d24823f55e5f82f65f
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-01 23:10:20 +0530

    Update login.php

--- public/teacher/login.php
+++ public/teacher/login.php
@@ -21,55 +21,56 @@
         <div class="body-container">
 
 
-        <div class="section-member">
+        <div class="section-member wrap-container">
 
-<div class="container" id="container">
-    <div class="form-container sign-up-container">
-        <form action="#">
-            <h1>Create Account</h1>
-            <div class="social-container">
-                <!-- <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a> -->
+            <div class="container" id="container">
+                <div class="form-container sign-up-container">
+                    <form  action="include/registration.teacher.inc.php" method="post">
+                        <h1>Create Account</h1>
+                        <div class="social-container">
+                            <!-- <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                            <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a> -->
+                        </div>
+                        <!-- <span>or use your email for registration</span> -->
+                        <input type="text" name="user_name" id="user_name" placeholder="user name" />
+                        <input name="email" type="email" id="email" placeholder="email" />
+                        <input name="password" type="password" id="password" placeholder="password" />
+                        <input name="re_password" type="password" id="re-password" placeholder="repeat password">
+                        <button  name="submit-register" >Sign Up</button>
+                    </form>
+                </div>
+                <div class="form-container sign-in-container">
+                    <form action="include/login.teacher.inc.php" method="post" >
+                        <h1>Teacher Sign In</h1>
+                        <div class="social-container">
+                            <!-- <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                            <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a> -->
+                        </div>
+                        <!-- <span>or use your account</span> -->
+                        <input type="text" id="user_name"  placeholder="User name / Email"  name="email" />
+                        <input type="password" placeholder="Password"  name="password"/>
+                        <a href="#">Forgot your password?</a>
+                        <button name="login-submit">Sign In</button>
+                    </form>
+                </div>
+                <div class="overlay-container">
+                    <div class="overlay">
+                        <div class="overlay-panel overlay-left">
+                            <h1>Welcome Back!</h1>
+                            <p>To keep connected with us please login with your personal info</p>
+                            <button class="ghost" id="signIn">Sign In</button>
+                        </div>
+                        <div class="overlay-panel overlay-right">
+                            <h1>Hello, Teachers!</h1>
+                            <p>Enter your personal details and start journey with us</p>
+                            <button class="ghost" id="signUp">Sign Up</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <!-- <span>or use your email for registration</span> -->
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>Sign Up</button>
-        </form>
-    </div>
-    <div class="form-container sign-in-container">
-        <form action="include/login.teacher.inc.php" method="post" >
-            <h1>Sign in</h1>
-            <div class="social-container">
-                <!-- <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a> -->
             </div>
-            <!-- <span>or use your account</span> -->
-            <input type="text" id="user_name"  placeholder="User name / Email"  name="email" />
-            <input type="password" placeholder="Password"  name="password"/>
-            <a href="#">Forgot your password?</a>
-            <button name="login-submit">Sign In</button>
-        </form>
-    </div>
-    <div class="overlay-container">
-        <div class="overlay">
-            <div class="overlay-panel overlay-left">
-                <h1>Welcome Back!</h1>
-                <p>To keep connected with us please login with your personal info</p>
-                <button class="ghost" id="signIn">Sign In</button>
-            </div>
-            <div class="overlay-panel overlay-right">
-                <h1>Hello, Teachers!</h1>
-                <p>Enter your personal details and start journey with us</p>
-                <button class="ghost" id="signUp">Sign Up</button>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
         </div>
     </div>
     
