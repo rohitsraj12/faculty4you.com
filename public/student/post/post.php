d51073e0acb79cd578615f0bf8e43f3ebf016258
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-02 11:38:34 +0530

    Update post.php

--- public/student/post/post.php
+++ public/student/post/post.php
@@ -105,82 +105,90 @@ require("../include/banner.inc.php");
         
         <div class="form-group">
             <label for="about">about me</label>
-            <textarea name="post_detail" class="form-control" id="about" placeholder="Briefly explain about yourself"></textarea>
+            <textarea name="post_detail" class="form-control" rows="10" id="about" placeholder="Briefly explain about yourself"></textarea>
             
         </div>
-        <fieldset class="form-group">
-            <div class="row">
-                <label class="label col-form-label col-sm-1 pt-0">Study type</label>
-                <div class="col-sm-8 row">
-                    <div class="form-check col-sm-12">
-                        <input class="form-check-input" name="study_type" type="radio" value="1" id="single">
-                    
-                        <label class="form-check-label" for="single">
-                            online one to one
-                        </label>
+        <div class="row">
+            <fieldset class="form-group col-sm-4">
+                <div class="row">
+                    <label class="label col-form-label col-sm-4 pt-0">Study type</label>
+                    <div class="col-sm-6 row">
+                        <div class="form-check col-sm-12">
+                            <input class="form-check-input" name="study_type" type="radio" value="1" id="single">
+                        
+                            <label class="form-check-label" for="single">
+                                online one to one
+                            </label>
+                        </div>
+                        <div class="form-check col-sm-12">
+                            <input class="form-check-input" name="study_type" type="radio" value="2" id="group">
+                        
+                            <label class="form-check-label" for="group">
+                                online group
+                            </label>
+                        </div>
+                        <div class="form-check col-sm-12">
+                            <input class="form-check-input" name="study_type" type="radio" value="3" id="home">
+                        
+                            <label class="form-check-label" for="home">
+                                home
+                            </label>
+                        </div>
+                        
                     </div>
-                    <div class="form-check col-sm-12">
-                        <input class="form-check-input" name="study_type" type="radio" value="2" id="group">
-                    
-                        <label class="form-check-label" for="group">
-                            online group
-                        </label>
-                    </div>
-                    <div class="form-check col-sm-12">
-                        <input class="form-check-input" name="study_type" type="radio" value="3" id="home">
-                    
-                        <label class="form-check-label" for="home">
-                            home
-                        </label>
-                    </div>
-                    
                 </div>
-            </div>
-        </fieldset>
-        
-        <fieldset class="form-group">
-            <div class="row">
-                <label class="label col-form-label col-sm-1 pt-0">Study category</label>
-                <div class="col-sm-8 row">
-                    <div class="form-check col-sm-12">
-                        <input class="form-check-input" name="study_category" type="radio" value="1" id="academic">
-                    
-                        <label class="form-check-label" for="academic">
-                            academic
-                        </label>
-                    </div>
+            </fieldset> 
+            <fieldset class="form-group col-sm-6">
+                <div class="row">
+                    <label class="label col-form-label col-sm-3 pt-0">Study category</label>
+                    <div class="col-sm-6 row">
+                        <div class="form-check col-sm-12">
+                            <input class="form-check-input" name="study_category" type="radio" value="1" id="academic">
+                        
+                            <label class="form-check-label" for="academic">
+                                academic
+                            </label>
+                        </div>
 
-                    <div class="form-check col-sm-12">
-                        <input class="form-check-input" name="study_category" type="radio" value="2" id="non-academic">
+                        <div class="form-check col-sm-12">
+                            <input class="form-check-input" name="study_category" type="radio" value="2" id="non-academic">
+                        
+                            <label class="form-check-label" for="non-academic">
+                                non-academic
+                            </label>
+                        </div>
                     
-                        <label class="form-check-label" for="non-academic">
-                            non-academic
-                        </label>
                     </div>
-                
                 </div>
-            </div>
-        </fieldset>
-
-        <div class="form-row">
-            <div class="form-group col-md-3">
-                <label for="city">City</label>
-                <input type="text" class="form-control" name="city" id="city" placeholder="city">
-            </div>
-            <div class="form-group col-md-3">
+            </fieldset>
+        </div>
+        <div class="form-row row">
+            <!-- <div class="form-group col-sm-2">
                 <label for="state">State</label>
                 <select id="state" class="form-control" name="state">
                     <option selected>Choose...</option>
-                    <option value="1">maharashtra</option>                   
-                     <option value="2">odisha</option>
+                    <?php
+                        // $state_query = "SELECT * from states";
+                        // $result = mysqli_query($conn, $state_query);
 
+                        // while($row = mysqli_fetch_assoc($result)){
+                            
+                    ?>
+                    <!-- <option value="<?php //echo $row['state_id'];?>"><?php //echo $row['state_id'];?></option>     -->
+                    <?php 
+                        //}
+                    ?>
                 </select>
+            </div> -->
+            <!-- <div class="form-group col-sm-6">
+                <label for="date">date</label>
+                <input name="post_date" class="w-25 form-control" type="date" id="date" placeholder="user name">
+            </div> -->
+            <div class="form-group col-sm-3">
+                <!-- <label for="city">City</label> -->
+                <!-- <input type="hidden" class="form-control" name="state" value="<?php $row['state_id']?>" id="city" placeholder="city">
+                <input type="hidden" class="form-control" name="city" value="<?php $row['city_id']?>" id="city" placeholder="city"> -->
             </div>
-        </div>
-
-         <div class="form-group">
-            <label for="date">date</label>
-            <input name="post_date" class="w-25 form-control" type="date" id="date" placeholder="user name">
         </div>
         <div class="form-row row">
         <input type="submit" class="col-3 w-100 btn-primary text-center h4" name="submit-post" value="submit">
@@ -196,3 +204,4 @@ require("../include/banner.inc.php");
 <?php 
     require("../include/footer.inc.php");
 ?>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti corporis officia iusto suscipit reprehenderit rem adipisci itaque nostrum laborum alias!
