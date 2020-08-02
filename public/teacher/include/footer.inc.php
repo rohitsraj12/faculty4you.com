07e0800b8fe487c24151e0344c374d6afd37924b
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-02 16:28:13 +0530

    Update footer.inc.php

--- public/teacher/include/footer.inc.php
+++ public/teacher/include/footer.inc.php
@@ -1,4 +1,52 @@
-<div class="body-footer"></div>
+<div class="body-footer">
+<footer class="wrap-container text-light pt-5">
+        <div class="row">
+                <section class="col-sm-3">
+                    <article>
+                        <header class="h2 border-bottom border-white-50 mb-5 pb-4">
+                            
+                            faculty4you
+                        </header>
+                        <p class="text-light h4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quidem.</p>
+                    </article>
+                </section>
+                <section class="col-sm-6">
+                    <article>
+                        <header class="h2 border-bottom border-white-50 mb-5 pb-4">
+                            Quick links
+                        </header>
+                        <div class="row">
+                            <ul class="col-sm-4">
+                                <li class="pb-2"><a class="text-light h4" href=""> home</a></li>
+                                <li class="pb-2"><a class="text-light h4" href=""> faq</a></li>
+                                <li class="pb-2"><a class="text-light h4" href=""> site map</a></li>
+                                <li class="pb-2"><a class="text-light h4" href=""> logout</a></li>
+                            </ul>
+                            <ul class="col-sm-4">
+                                <li class="pb-2"><a class="text-light h4" href=""> view my post</a></li>
+                                <li class="pb-2"><a class="text-light h4" href=""> add new post</a></li>
+                                <li class="pb-2"><a class="text-light h4" href=""> profile</a></li>
+                            </ul>
+                            <ul class="col-sm-4">
+                                <li class="pb-2"><a class="text-light h4" href=""> view my profile</a></li>
+                                <li class="pb-2"><a class="text-light h4" href=""> update profile</a></li>
+                            </ul>
+                        </div>
+                     
+                    </article>
+                </section>
+                <section class="col-sm-3">
+                    <article>
+                        <header class="h2 border-bottom border-white-50 mb-5 pb-4">
+                            Follow                        
+                        </header>
+                        <p class="text-light h4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quidem.</p>
+                    </article>
+                </section>
+
+        </div>
+    </footer>
+</div>
             <!-- end body footer -->
         </div>
         <!-- end body wrap -->
