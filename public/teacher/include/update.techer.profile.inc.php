78b1cd71bdcec929090dd48e6ff6c2eec020824a
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-03 01:48:40 +0530

    Update update.techer.profile.inc.php

--- public/teacher/include/update.techer.profile.inc.php
+++ public/teacher/include/update.techer.profile.inc.php
@@ -4,109 +4,51 @@ if(isset($_POST['submit-register'])){
 
     require_once '../../../private/config/db_connect.php';
     
-    $id = $_GET['student_id'];
-    $user_name = $_POST['user_name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $re_password = $_POST['re_password'];
+    $id = $_GET['teacher_id'];
+    // $user_name = $_POST['user_name'];
+    // $email = $_POST['email'];
+    // $password = $_POST['password'];
+    // $re_password = $_POST['re_password'];
 
     // fill empty data
-    $first_name = "";
-    $last_name = "";
-    $phone = "";
-    $address = "";
-    $city = "";
-    $state = "";
-    $pincode = "";
-    $image = "";
-    $membership = "";
-    $teaching_exp = "";
-    $about = "";
-    $online_one = "";
-    $online_group = "";
-    $home_tution = "";
-    $sub_id = "";
-    $category_id = "";
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+//     $phone = $_POST['phone'];
+//     $email = $_POST['email'];
+//     $address = $_POST['address'];
+//     $city = $_POST['city'];
+//     $state = $_POST['state'];
+//     // $pincode = $_POST['pincode'];
+//     // $image = "";
+//     // $membership = "";
+//     // $gender = $_POST['gender'];
+//     $teaching_exp = $_POST['exp'];
+//     $about = $_POST['about'];
+//     $online_one = $_POST['online_one'];
+//     $online_group = $_POST['online_group'];
+//     $home_tuition = $_POST['home_tuition'];
+//     $sub = $_POST['subject'];
+//     $category = $_POST['category'];
+  
+    $query = "UPDATE teachers set teacher_first_name = '$first_name' 
+--     teacher_last_name = '$last_name',
+--     teacher_phone = '$phone',
+--     teacher_email = '$email',
+--     teacher_address = '$address',
+--     city_id = '$city',
+--     state_id = '$state',
+--     teacher_experience = '$teaching_exp',
+--     teacher_about_me = '$about',
+--     teacher_online_one_to_one = '$online_one',
+--     teacher_online_group = '$online_group',
+--     teacher_home_tuition = '$home_tuition',
+--     subject_id = '$sub',
+--     category_id = '$category' 
+WHERE teacher_id=$id"; 
+    $result = mysqli_query($conn, $query);
+
     
-    // if empty field condition
-    if(empty($user_name) || empty($email) || empty($password) || empty($re_password)){
-        // redirect to register and empty field
-        header("location: ../login.php?error=emptyfields&user_name=".$user_name."&mail".$email);
-        //stop scripting
-        exit();
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $user_name)){
-        // redirect to register and empty field
-        header("location: ../login.php?error=invalidmailuser_name");
-        //stop scripting
-        exit();
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-        // redirect to register and empty field
-        header("location: ../login.php?error=invalidmail&user_name=".$user_name);
-        //stop scripting
-        exit();
-    } else if (!preg_match("/^[a-zA-Z0-9]*$/", $user_name)){
-        // redirect to register and empty field
-        header("location: ../login.php?error=invalidmail&mail=".$email);
-        //stop scripting
-        exit();
-    } else if ($password !== $re_password){
-        // redirect to register and empty field
-        header("location: ../login.php?error=passwordcheck&user_name=".$user_name."&mail".$email);
-        //stop scripting
-        exit();
-    } else {
-        // prepared statement (? placeholder for safty)
-        $sql = "SELECT teacher_user_name FROM teachers WHERE teacher_user_name=?";
-        // 
-        $stmt = mysqli_stmt_init($conn);
-
-        // 
-        if(!mysqli_stmt_prepare($stmt, $sql)){
-            
-            // redirect to register and empty field
-            header("location: ../login.php?error=sqlerror");
-            //stop scripting
-            exit();
-        } else {
-            // 
-            mysqli_stmt_bind_param($stmt, "s", $user_name);
-            mysqli_stmt_execute($stmt);
-            // is ther any match
-            mysqli_stmt_store_result($stmt);
-            $resultcheck = mysqli_stmt_num_rows($stmt);
-
-            if($resultcheck > 0 ){
-                
-                // redirect to register and empty field
-                header("Location: ../login.php?error=usertaken$mail=".$email);
-                //stop scripting
-                exit();
-            } else {
-                $first_name = "";
-                $sql = "INSERT INTO teachers (teacher_first_name, teacher_last_name, teacher_user_name, teacher_email, teacher_password, teacher_phone, teacher_address, city_id, state_id, teacher_photo, teacher_membership_status, teacher_experience, teacher_about_me, teacher_online_one_to_one, teacher_online_group, teacher_home_tuition, subject_id, category_id) 
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                $stmt = mysqli_stmt_init($conn);
-
-                if(!mysqli_stmt_prepare($stmt, $sql)){
-                    // redirect to register and empty field
-                    header("Location: ../login.php?error=sqlerror");
-                    //stop scripting
-                    exit();
-                } else {
-                    // secure password
-                    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-                    mysqli_stmt_bind_param($stmt, "sssssssiissisiiiii", $first_name, $last_name, $user_name, $email, $hashed_password, $phone, $address, $city, $state, $image, $membership, $teaching_exp, $about, $online_one, $online_group, $home_tution, $sub_id, $category_id);
-                    mysqli_stmt_execute($stmt);
-                    
-                    header("Location: ../login.php?register=success");
-                    exit();
-                }
-            }
-            }
-        }
-        mysqli_stmt_close($stmt);
-        mysqli_close($conn);
+//    header('location:'.$base_url.'index.php');
 
 } else {
      // redirect to register and empty field
