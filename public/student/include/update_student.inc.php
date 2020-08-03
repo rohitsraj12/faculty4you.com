6f32139216be8c2a93772ab851b9decdb2c9ef04
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-04 01:31:19 +0530

    Update update_student.inc.php

--- public/student/include/update_student.inc.php
+++ public/student/include/update_student.inc.php
@@ -2,56 +2,65 @@
     // $student_id = $_GET['student_id'];
 
 
+    // $id = $_GET['id'];
 
-if(isset($_POST['student_update'])){
-    //  $student_id = $_GET['student_id'];
-    // $student_name = $_POST['id'];
-     $student_name = $_POST['first_name'];
-    // $student_batch = $_POST['batch'];
-    // $student_phone =  $_POST['phone'];
-    // $student_email =  $_POST['email'];
-    // $student_detail =  $_POST['detail'];
-    // $active =  $_POST['active'];        
-    // //uploading imgage
-    // $files = $_FILES['file'];
+    // echo $id;
 
-    echo "hi";
-    //  echo $student_id;
-    echo $studnet_name;
+if(isset($_POST['update'])){
+    $id = $_GET['id'];
+    $first_name = $_POST['fname'];
+    $last_name = $_POST['lname'];
+    $email =  $_POST['email'];
+    $phone =  $_POST['phone'];
+    $address =  $_POST['address'];
+    $city =  $_POST['city'];    
+    $state =  $_POST['state'];
+    $pincode =  $_POST['pincode'];
+  
 
-    // //  accesing file details
-    //    $file_name = $files['name'];
-    //    $file_error = $files['error'];
-    //    $filetemp = $files['tmp_name'];
+    //if(){
+        // //uploading imgage
+            // $files = $_FILES['file'];
 
-    // // breakdown file name and extention
-    //     // after . will store in var
-    //     $file_ext = explode('.', $file_name);
-    //     // make it lowercase
-    //     $file_check = strtolower(end($file_ext));
-    
-    //     //file ext store in array which are png, jpeg n jpg
-    //     $file_ext_store = array('png', 'jpeg', 'jpg');
+            // echo "hi";
+            // //  echo $student_id;
+            // echo $studnet_name;
+
+            // //  accesing file details
+            //    $file_name = $files['name'];
+            //    $file_error = $files['error'];
+            //    $filetemp = $files['tmp_name'];
+
+            // // breakdown file name and extention
+            //     // after . will store in var
+            //     $file_ext = explode('.', $file_name);
+            //     // make it lowercase
+            //     $file_check = strtolower(end($file_ext));
+            
+            //     //file ext store in array which are png, jpeg n jpg
+            //     $file_ext_store = array('png', 'jpeg', 'jpg');
 
 
-    // if(in_array($file_check,$file_ext_store)){
-    //     //destination folder
-    //     $destination_file = 'img/'. $file_name;
-    //     //moving from tem to destination
-    //     move_uploaded_file($filetemp, $destination_file);
+            // if(in_array($file_check,$file_ext_store)){
+            //     //destination folder
+            //     $destination_file = 'img/'. $file_name;
+            //     //moving from tem to destination
+            //     move_uploaded_file($filetemp, $destination_file);
+    //}
+   
 
-    //     $query = "UPDATE students set student_id = $student_id, 
-    //     student_name = '$student_name', 
-    //     student_batch = '$student_batch', 
-    //     student_phone = '$student_phone', 
-    //     student_email = '$student_email', 
-        
-    //     student_image = '$destination_file', 
-    //     student_detail = '$student_detail' WHERE student_id=$student_id"; 
+        $query = "UPDATE std SET student_first_name = '$first_name',
+        student_last_name = '$last_name', 
+        student_email = '$email', 
+        student_phone = '$phone', 
+        student_address = '$address', 
+        city_id = $city, 
+        state_id = $state, 
+        student_city_pincode = '$pincode' WHERE student_id=$id"; 
 
-    //     $_result = mysqli_query($conn, $query);
+        $_result = mysqli_query($conn, $query);
 
-    //     header('location:view_students.php');
+        // header('location:index.php');
     // }
 
 }
