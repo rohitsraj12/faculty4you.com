2ba61a492e3aa43882348a0f552ef2c1cc9b8520
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-03 13:08:48 +0530

    Update index.php

--- public/student/index.php
+++ public/student/index.php
@@ -2,7 +2,7 @@
     session_start();
     //back function false
     if(!isset($_SESSION['user_name'])){
-        header('location: ../login.php');
+        header('location: login.php');
     } 
     $page_title = "home page";
     $banner_image = "post.svg";
@@ -15,7 +15,94 @@
 
     ?>
     <div class="body-container">
-            <main></main>
+        <main>
+            <section>
+                <div class="section-header"  data-aos="zoom-out-up" data-aos-duration="1000">
+                    <heeader class="text-primary-h u-center-text"> 
+                        Teacher detail
+                    </header>
+                    <section class="section-search-result wrap-container">
+                        
+<?php
+    $sql = "SELECT * FROM teachers";
+    $result = mysqli_query($conn, $sql);
+    $query_results = mysqli_num_rows($result);
+
+    ?>
+        
+        <div class="search-result-num" data-aos="zoom-out-up" data-aos-duration="1000">
+            <p>
+                    <?php echo $query_results; ?> results are matching
+            </p>
+        </div>
+        <section class="section-body"> 
+    <?php 
+
+   
+
+    if($query_results > 0){
+        while($row = mysqli_fetch_assoc($result)){
+
+            ?>      
+            
+            
+            <article class="article-search-result" data-aos="zoom-out-up" data-aos-duration="1000">
+                    <div class="block-left">
+                        <figure class="article-search__figure">
+                            <img src="<?php base_url()?>img/teacher/profile_pic/male_profile.svg" alt="">
+                        </figure>
+                    </div>
+                    <div class="block-right">
+                        <header class="article-search__header">
+                            <h1 class="text-secondary">
+                                <?php echo $row['teacher_user_name'];?>
+                            </h1>
+                        </header>
+                        <div class="article-search-body">
+                                <ul>
+                                    <li>qualification: <strong></strong></li>
+                                    <li>Experience: <strong></strong></li>
+                                    <li>City:<strong></strong></li>
+                                    <li>available for : 
+                                        <strong><?php if($row["teacher_online_one_to_one"] == 1){echo "Online one to one";} ?></strong>
+                                        <strong> <?php if($row["teacher_online_group"] == 1){echo "Online group";}?></strong>
+                                        <strong> <?php if($row["teacher_home_tuition"] == 1){echo "home tuition";}?> </strong>
+                                    
+                                    </li>
+                                    
+                                </ul>
+                        </div>
+                        
+                        <footer class="article-search-footer">
+                            <a class="button-text-1" href="detail.php?name=<?php echo $row['teacher_user_name']?>&batch=<?php echo $row['teacher_email']?>">more details</a>
+                        </footer>
+                    </div>
+                </article>
+            
+            
+            
+            <?php
+
+        }
+    } else {
+        //echo "there are no result";
+        
+        ?> 
+        
+        <div class="search-result-num" data-aos="zoom-out-up" data-aos-duration="1000">
+            <p>
+                there are no result
+            </p>
+        </div>
+
+        <?php
+    }
+ 
+?> 
+                    </section>
+                </div>
+            </section>
+        </main>
             </div>
             <!-- end body container -->
 
