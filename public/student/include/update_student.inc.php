5a53052cd86196672ffc82594f79034d3867f656
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-07 09:21:34 +0530

    Update update_student.inc.php

--- public/student/include/update_student.inc.php
+++ public/student/include/update_student.inc.php
@@ -18,49 +18,49 @@ if(isset($_POST['update'])){
     $pincode =  $_POST['pincode'];
   
 
-    //if(){
-        // //uploading imgage
-            // $files = $_FILES['file'];
+        //uploading imgage
+        $files = $_FILES['file'];
+        
+        //testing 
+        //    echo  $product_name;
+        //    echo "</br>";
+        //    print_r($files);
 
-            // echo "hi";
-            // //  echo $student_id;
-            // echo $studnet_name;
+        //  accesing file details
+            $file_name = $files['name'];
+            $file_error = $files['error'];
+            $filetemp = $files['tmp_name'];
 
-            // //  accesing file details
-            //    $file_name = $files['name'];
-            //    $file_error = $files['error'];
-            //    $filetemp = $files['tmp_name'];
+        // breakdown file name and extention
+            // after . will store in var
+            $file_ext = explode('.', $file_name);
+            // make it lowercase
+            $file_check = strtolower(end($file_ext));
+        
+            //file ext store in array which are png, jpeg n jpg
+            $file_ext_store = array('png', 'jpeg', 'jpg');
 
-            // // breakdown file name and extention
-            //     // after . will store in var
-            //     $file_ext = explode('.', $file_name);
-            //     // make it lowercase
-            //     $file_check = strtolower(end($file_ext));
-            
-            //     //file ext store in array which are png, jpeg n jpg
-            //     $file_ext_store = array('png', 'jpeg', 'jpg');
-
-
-            // if(in_array($file_check,$file_ext_store)){
-            //     //destination folder
-            //     $destination_file = 'img/'. $file_name;
-            //     //moving from tem to destination
-            //     move_uploaded_file($filetemp, $destination_file);
-    //}
+        if(in_array($file_check,$file_ext_store)){
+            //destination folder
+            $destination_file = '../../img/student/profile_pic/' . $file_name;
+            $profile_image_url = 'img/student/profile_pic/' . $file_name;
+            //moving from tem to destination
+            move_uploaded_file($filetemp, $destination_file);
    
 
-        $query = "UPDATE std SET student_first_name = '$first_name',
-        student_last_name = '$last_name', 
-        student_email = '$email', 
-        student_phone = '$phone', 
-        student_address = '$address', 
-        city_id = $city, 
-        state_id = $state, 
-        student_city_pincode = '$pincode' WHERE student_id=$id"; 
+            $query = "UPDATE std SET student_first_name = '$first_name',
+            student_last_name = '$last_name', 
+            student_email = '$email', 
+            student_phone = '$phone', 
+            student_address = '$address', 
+            city_id = '$city', 
+            state_id = '$state', 
+            student_city_pincode = '$pincode', 
+            student_photo = '$profile_image_url' WHERE student_id=$id"; 
 
-        $_result = mysqli_query($conn, $query);
+            $_result = mysqli_query($conn, $query);
 
         // header('location:index.php');
-    // }
+    }
 
 }
