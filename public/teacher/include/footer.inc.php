3fb505e37cdba8dce4c70a2e44577e2ab3290919
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-08 01:26:32 +0530

    Update footer.inc.php

--- public/teacher/include/footer.inc.php
+++ public/teacher/include/footer.inc.php
@@ -19,19 +19,14 @@
                         </header>
                         <div class="row">
                             <ul class="col-sm-4">
-                                <li class="pb-2"><a class="text-light h4" href=""> home</a></li>
-                                <li class="pb-2"><a class="text-light h4" href=""> faq</a></li>
-                                <li class="pb-2"><a class="text-light h4" href=""> site map</a></li>
-                                <li class="pb-2"><a class="text-light h4" href=""> logout</a></li>
+                                <li class="pb-2"><a class="text-light h4" href="<?php echo base_url();?>teacher/index.php"> home</a></li>
+                                <li class="pb-2"><a class="text-light h4" href="<?php echo base_url();?>faq.php"> faq</a></li>
+                                <li class="pb-2"><a class="text-light h4" href="<?php echo base_url();?>site_map.php"> site map</a></li>
+                                <li class="pb-2"><a class="text-light h4" href="<?php echo base_url();?>logout.php"> logout</a></li>
                             </ul>
                             <ul class="col-sm-4">
-                                <li class="pb-2"><a class="text-light h4" href=""> view my post</a></li>
-                                <li class="pb-2"><a class="text-light h4" href=""> add new post</a></li>
-                                <li class="pb-2"><a class="text-light h4" href=""> profile</a></li>
-                            </ul>
-                            <ul class="col-sm-4">
-                                <li class="pb-2"><a class="text-light h4" href=""> view my profile</a></li>
-                                <li class="pb-2"><a class="text-light h4" href=""> update profile</a></li>
+                                <li class="pb-2"><a class="text-light h4" href="<?php echo base_url();?>teacher/profile/index.php"> view my profile</a></li>
+                                <li class="pb-2"><a class="text-light h4" href="<?php echo base_url();?>teacher/profile/profile_update.php"> update profile</a></li>
                             </ul>
                         </div>
                      
@@ -42,7 +37,6 @@
                         <header class="h2 border-bottom border-white-50 mb-5 pb-4">
                             Follow                        
                         </header>
-                        <p class="text-light h4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, quidem.</p>
                     </article>
                 </section>
 
@@ -50,7 +44,7 @@
     </footer>
     <div class="footer-base bg-primary p-3">
         <ul class="wrap-container">
-            <li class="footer-base__list">copyright 2014-2020 <a class="footer-base__link" href="<?php echo base_url();?>">facultyforyou.com</a> </li>
+            <li class="footer-base__list">copyright 2014-2020 <a class="footer-base__link" href="<?php echo base_url();?>teacher/index.php">facultyforyou.com</a> </li>
             <li class="footer-base__list"><a class="footer-base__link" href="#">terms and conditions</a></li>
             <li class="footer-base__list">Developed by <a class="footer-base__link" href="#"></a></li>
         </ul>
