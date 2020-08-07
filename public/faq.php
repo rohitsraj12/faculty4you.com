8235fbbba78ecd0223eeef8ae7c70d61c923a8ba
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-07 15:30:50 +0530

    Create faq.php

---
+++ public/faq.php
@@ -0,0 +1,25 @@
+<?php
+    $page_title = "faq";
+    require("../private/config/config.php");
+    require_once("../private/config/db_connect.php");
+    include_once("../private/required/public/header.public.php");
+    // include("../private/required/public/banner/banner.public.php");
+
+?>
+
+<div class="body-container">
+    <main>
+        <section class="section-faq">
+            <div class="section-header u-center-text">
+                <heeader class="text-primary-h"> 
+                    FAQ
+                </header>
+            </div>
+        </section>
+    </main>
+</div>
+
+
+<?php
+    include_once("../private/required/public/footer.public.php");
+?>
