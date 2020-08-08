abd255765681a99d8003acac6649dde0320de69f
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-08 17:04:26 +0530

    Update teacher.query.inc.php

--- public/teacher/include/teacher.query.inc.php
+++ public/teacher/include/teacher.query.inc.php
@@ -2,7 +2,6 @@
 
 require_once '../../private/config/db_connect.php';
 
-
     $sql = "SELECT posts.*, std.*, cities.city_name, states.state_name, study_types.study_type_name, study_categories.study_cat_type 
     FROM posts
         JOIN std
@@ -15,5 +14,6 @@ require_once '../../private/config/db_connect.php';
             ON study_types.study_type_id = posts.study_type_id
         JOIN study_categories
             ON study_categories.study_cat_id = posts.study_cat_id
+        WHERE city_id = '$city'
         ORDER BY post_id DESC ";
     $result = mysqli_query($conn, $sql);
