11e1f28d2c693630df0b26d0f30089999c8d306a
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-10 02:54:13 +0530

    Update index.php

--- public/student/post/index.php
+++ public/student/post/index.php
@@ -33,80 +33,151 @@
     ?>
        
     <div class="body-container">
-                <main>
-                    <section class="wrap-container">
-                        <header class="text-primary-h text-center pb-5">
-                            students Post
-                        </header>
+        <main class="post__page">
+            <section class="wrap-container">
 
-                        <section class="row pt-5">
-                            <div class="col-sm-3">
-                                <ul>
-                                    <li class=" mb-1">
-                                        <a class="w-100 btn btn-primary">
-                                            view all posts
-                                        </a>
-                                    </li>
-                                    <li class="w-100 bg-primary text-center">
-                                        <a class="btn btn-primary">
-                                            add new post
-                                        </a></li>
-                                </ul>
-                            </div>
-                            <div class="col-sm-9">
-        <?php 
-
-        $student_id = $row['student_id'];
-            
-        // $query = "SELECT  posts.*, cities.*, states.* FROM posts, cities, states WHERE student_id = $student_id";
-        // $query = "SELECT posts.*, cities.city_name, states.state_name FROM posts LEFT JOIN cities ON posts.city_id = cities.city_id AND RIGHT JOIN states ON posts.state_id = states.state_id WHERE student_id = $student_id";
-        // $query = "SELECT posts.*, cities.city_name FROM posts LEFT JOIN cities ON posts.city_id = cities.city_id WHERE student_id = $student_id";
-        
-        $query = "SELECT posts.*, study_types.study_type_name, study_categories.study_cat_type 
-                    FROM posts
-                    JOIN study_types
-                        ON study_types.study_type_id = posts.study_type_id
-                    JOIN study_categories
-                        ON study_categories.study_cat_id = posts.study_cat_id
-                    WHERE student_id = $student_id
-                    ORDER BY post_id DESC";
-
-        $result = mysqli_query($conn, $query);
-
-            while($row = mysqli_fetch_assoc($result)){
-            ?>
-                
-          <article class="mb-5 px-5 py-3 border bg-light">
-                        <header class="border-bottom">
-                            <h1 class="h1 py-3 text-dark font-weight-normal">
-                                <?php echo $row["post_title"];?>
-                            </h1>
-                        </header>
-                        <div class="body mb-4">
-                            <ul class="d-flex flex-row bd-highlight py-4 h4 font-weight-normal text-secondary">
-                                <li class="mr-5"><i class="fa fa-calendar mr-2" aria-hidden="true"></i><?php echo $row["post_date"];?></li>
-                                <li class="mr-5"><i class="fa fa-graduation-cap mr-2" aria-hidden="true"></i><?php echo $row["study_type_name"];?></li>
-                                <li class="mr-5"><i class="fa fa-university mr-2" aria-hidden="true"></i><?php echo $row["study_cat_type"];?></li>
-                                <!-- <li class="mr-5"><i class="fa fa-map-marker mr-2" aria-hidden="true"></i><?php// echo $row["city_name"];?></li> -->
-                            </ul>
-                        <p class="text-dark">
-                            <?php echo $row["post_detail"];?>
-                        </p>
+                <section class="row section__post">
+                    <div class="col-sm-3">
+                        <div class="tab" data-aos="zoom-out-up" data-aos-duration="1000">
+                            <button class="tablinks active" data-post="view__post">View My Post</button>
+                            <button class="tablinks" data-post="compose__post">Compose New Post</button>
                         </div>
-                        <footer class="pb-3">
-                            <a href="<?php base_url();?>student/post/update.php?id=<?php echo $row['post_id'];?>" style="font-size: 1.6rem" class="py-2 px-4 btn btn-primary">Edit post</a>
-                        </footer>
-                    </article>
-                    
-          <?php
-            }
-        ?>
-        
+                    </div>
+                    <div class="col-sm-9">
+                        <section  class="view__post post__cat" id="viewPost">
                             
-        </div>
-                        </section>
-        </section>
+                            <header class="text-primary-h text-center pb-5 mb-5" data-aos="zoom-out-up" data-aos-duration="1000">
+                                View my past posts
+                            </header>
+                            <?php 
 
+                                $student_id = $row['student_id'];
+                                    
+                                // $query = "SELECT  posts.*, cities.*, states.* FROM posts, cities, states WHERE student_id = $student_id";
+                                // $query = "SELECT posts.*, cities.city_name, states.state_name FROM posts LEFT JOIN cities ON posts.city_id = cities.city_id AND RIGHT JOIN states ON posts.state_id = states.state_id WHERE student_id = $student_id";
+                                // $query = "SELECT posts.*, cities.city_name FROM posts LEFT JOIN cities ON posts.city_id = cities.city_id WHERE student_id = $student_id";
+                                
+                                $query = "SELECT posts.*, study_types.study_type_name, study_categories.study_cat_type 
+                                            FROM posts
+                                            JOIN study_types
+                                                ON study_types.study_type_id = posts.study_type_id
+                                            JOIN study_categories
+                                                ON study_categories.study_cat_id = posts.study_cat_id
+                                            WHERE student_id = $student_id
+                                            ORDER BY post_id DESC";
+
+                                $result = mysqli_query($conn, $query);
+
+                                while($row = mysqli_fetch_assoc($result)){
+
+                            ?>
+                                    <article class="mb-5 px-5 py-3 border bg-light" data-aos="zoom-out-up" data-aos-duration="1000">
+                                        <header class="border-bottom">
+                                            <h1 class="h1 py-3 text-dark font-weight-normal">
+                                                <?php echo $row["post_title"];?>
+                                            </h1>
+                                        </header>
+                                        <div class="body mb-4">
+                                            <ul class="d-flex flex-row bd-highlight py-4 h4 font-weight-normal text-secondary">
+                                                <li class="mr-5"><i class="fa fa-calendar mr-2" aria-hidden="true"></i><?php echo $row["post_date"];?></li>
+                                                <li class="mr-5"><i class="fa fa-graduation-cap mr-2" aria-hidden="true"></i><?php echo $row["study_type_name"];?></li>
+                                                <li class="mr-5"><i class="fa fa-university mr-2" aria-hidden="true"></i><?php echo $row["study_cat_type"];?></li>
+                                                <!-- <li class="mr-5"><i class="fa fa-map-marker mr-2" aria-hidden="true"></i><?php// echo $row["city_name"];?></li> -->
+                                            </ul>
+                                        <p class="text-dark">
+                                            <?php echo $row["post_detail"];?>
+                                        </p>
+                                        </div>
+                                        <footer class="pb-3">
+                                            <a href="<?php base_url();?>student/post/update.php?id=<?php echo $row['post_id'];?>" style="font-size: 1.6rem" class="py-2 px-4 btn btn-primary">Edit post</a>
+                                        </footer>
+                                    </article>
+                                        
+                            <?php
+                                }
+                            ?>
+                        </section>
+                        <section  class="compose__post post__cat" id="composePost">
+                            <div class="section-profile-update">
+                                <header class="text-primary-h text-center pb-5 mb-5" data-aos="zoom-out-up" data-aos-duration="1000">
+                                    Compose new post
+                                </header>
+                                <form action="../include/post.inc.php" method="post" class=" bg-light border py-5 px-5" data-aos="zoom-out-up" data-aos-duration="1000">
+                                    <div class="form-group">
+                                        <label for="title">Title</label>
+                                        <input name="post_title" class="form-control" type="text" id="title" placeholder="user name">
+                                    </div>
+                                    
+                                    <div class="form-group">
+                                        <label for="about">About me</label>
+                                        <textarea name="post_detail" class="form-control" rows="10" id="about" placeholder="Briefly explain about yourself"></textarea>
+                                        
+                                    </div>
+                                    <div class="row">
+                                        <fieldset class="form-group col-sm-12">
+                                            <div class="row">
+                                                <label class="label col-form-label col-sm-3 pt-0">Study type</label>
+                                                <div class="col-sm-9 row">
+                                                    <div class="form-check col-sm-4">
+                                                        <input class="form-check-input" name="study_type" type="radio" value="1" id="single">
+                                                    
+                                                        <label class="form-check-label" for="single">
+                                                            Online one to one
+                                                        </label>
+                                                    </div>
+                                                    <div class="form-check col-sm-4">
+                                                        <input class="form-check-input" name="study_type" type="radio" value="2" id="group">
+                                                    
+                                                        <label class="form-check-label" for="group">
+                                                            Online group
+                                                        </label>
+                                                    </div>
+                                                    <div class="form-check col-sm-4">
+                                                        <input class="form-check-input" name="study_type" type="radio" value="3" id="home">
+                                                    
+                                                        <label class="form-check-label" for="home">
+                                                            Home
+                                                        </label>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </fieldset> 
+                                        <fieldset class="form-group col-sm-12">
+                                            <div class="row">
+                                                <label class="label col-form-label col-sm-3 pt-0">Study category</label>
+                                                <div class="col-sm-9 row">
+                                                    <div class="form-check col-sm-4">
+                                                        <input class="form-check-input" name="study_category" type="radio" value="1" id="academic">
+                                                    
+                                                        <label class="form-check-label" for="academic">
+                                                            Academic
+                                                        </label>
+                                                    </div>
+
+                                                    <div class="form-check col-sm-4">
+                                                        <input class="form-check-input" name="study_category" type="radio" value="2" id="non-academic">
+                                                    
+                                                        <label class="form-check-label" for="non-academic">
+                                                            Non-academic
+                                                        </label>
+                                                    </div>
+                                                
+                                                </div>
+                                            </div>
+                                        </fieldset>
+                                    </div>
+                                    <div class="">
+                                        <input type="submit" style="font-size: 1.6rem;" class="py-2 px-4 btn-lg btn btn-primary" name="submit-post" value="submit">
+                                        <input type="reset"  style="font-size: 1.6rem;" class="btn btn-lg  btn btn-outline-secondary" value="reset">
+
+                                    </div>
+                                    
+                                </form>
+                            </div>
+                        </section>             
+                    </div>
+                </section>
+            </section>
         </main>
 
     </div>
