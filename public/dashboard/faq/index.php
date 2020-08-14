66b16ad12d21a8d23e8279bcbb12129a95a252f5
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-15 04:17:10 +0530

    Update index.php

--- public/dashboard/faq/index.php
+++ public/dashboard/faq/index.php
@@ -17,17 +17,27 @@ $sub = "faq_view";
             </div>
         </div>
         <section class="section-faq">
-            
+            <?php
+                $query = "SELECT * FROM faqs";
+                $result = mysqli_query($conn, $query);
+
+                while($row = mysqli_fetch_assoc($result)){
+            ?>
             <article class="mb-4 border">
                 <header class="bg-light text-dark border-bottom faq__header">
-                    some question
+                <?php echo $row['faq_id'];?> - <?php echo $row['faq_question'];?>
                     <span class="toggle__btn"><i class="fa fa-angle-down" aria-hidden="true"></i>
                     </span>
                 </header>
                 <footer class="faq__footer h3 text-dark">
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate, culpa. Non saepe ducimus dolore sint dolorem officiis in dolores voluptatum.</p>
+                    <p>
+                    <?php echo $row['faq_answer'];?>
+                    </p>
                 </footer>
             </article>
+            <?php 
+            }
+            ?>
         </section>
         <section class="section-bottom"></section>
         </div>
