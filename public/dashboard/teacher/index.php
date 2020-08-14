fe7b0839b02e8cf08de6bbf11b2957a0b5f900de
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-14 23:06:46 +0530

    Create index.php

---
+++ public/dashboard/teacher/index.php
@@ -0,0 +1,67 @@
+<?php 
+$active = "teacher";
+    require("../../../private/config/config.php");
+    require("../../../private/config/db_connect.php");
+    include("../include/header.inc.php");
+
+?>
+    <div class="body-container-right"> 
+        <div class="wrap-container">
+            <div class="page-header">
+                <div class="container">
+                    <header class="header-text-1" class="py-3">
+                        TEACHER RECORDS
+                    </header>
+                </div>
+            </div>
+            <section class="section-record">
+                <div class="container">
+                    <table class="bg-light table table-sm">
+                        <thead class="thead-dark">
+                            <tr>
+                            <th scope="col">id</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Location</th>
+                            <th scope="col">More Details</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                $sql = "SELECT teachers.*, cities.*, states.* FROM teachers 
+                                LEFT JOIN cities
+                                    ON cities.city_id = teachers.city_id
+                                LEFT JOIN states
+                                    ON states.state_id = teachers.state_id
+                                ORDER BY teacher_id ASC";
+                            
+                                $result = mysqli_query($conn, $sql);
+                                $row = mysqli_fetch_assoc($result);
+
+                                while($row = mysqli_fetch_assoc($result)){
+                            ?>
+                            <tr>
+                                <th scope="row"><?php echo $row['teacher_id'];?></th>
+                                <td><?php echo $row['teacher_first_name'] . " " . $row['teacher_last_name'] ;?></td>
+                                <td><?php echo $row['teacher_email'];?></td>
+                                <td><?php echo $row['teacher_phone'];?></td>
+                                <td><?php echo $row['city_name'] . " ," . $row['state_name'] ;?></td>
+                                <td><a class="btn btn-primary" href="<?php base_url()?>dashboard/teacher/teacher_detail.php?id=<?php echo $row['teacher_id'];?>">more details</a></td>
+                            </tr>
+                            <?php 
+                            }
+                            ?>
+                        </tbody>
+                    </table>           
+                </div>
+
+
+            </section>
+            <section class="section-bottom"></section>
+        </div>
+    </div>
+
+<?php
+    include("../include/footer.inc.php");
+?>
