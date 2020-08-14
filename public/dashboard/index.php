e650d3cea45100388890c99db3cb78f566fa6a49
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-14 16:25:19 +0530

    Update index.php

--- public/dashboard/index.php
+++ public/dashboard/index.php
@@ -1,101 +1,51 @@
 <?php 
-    require("../../private/config/config.php");
-    require("../../private/config/db_connect.php");
+    include("include/header.inc.php");
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <div class="body-container-right"> 
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link
-        href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-        rel="stylesheet"
-        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-        crossorigin="anonymous"
-     />
-     <link rel="stylesheet" href="<?php base_url();?>css/style.css">
-     <link rel="stylesheet" href="css/dashboard.css">
-
-      
-</head>
-<body>
-        <div class="body-wrap">
-            <div class="body-header" data-aos="zoom-out-down" data-aos-duration="1000">
-              <header class="wrap-container w-100">
-                    <div class="header-brand">
-                        <a href="<?php base_url();?>index.php">
-                            <!-- <img src="<?php base_url();?>img/brand/header-logo.png" alt="faculty 4 you"> -->
-                            facultyforyou admin
-                        </a>
-                    </div>
-                    <!-- end header brand -->
-                    <nav class="header__nav">
-                        <ul>
-                            <li> 
-                                <form action="POST" class="header-search">
-                                <i class="fa fa-search pr-3" aria-hidden="true"></i>
-                                <input type="search" placeholder="search">
-                                </form>
-                                
-                            </li>
-                            <li class="nav__list"> <a href="#" class="nav__link">notification</a>
-                                <sub class="sub__nav" >
-                                    <ul>
-                                        <li>1</li>
-                                        <li>2</li>
-                                    </ul>
-                                </sub>
-                            </li>
-                           
-                            <li class="nav__list"><a href="#" class="nav__link">profile</a>
-                                <sub class="sub__nav">
-                                    <ul>
-                                        <li> <a href="">profile</a></li>
-                                        <li> <a href="">update profile</a></li>
-                                        <li> <a href="">logout</a></li>
-                                    </ul>
-                                </sub>
-                            </li>
-
-                        </ul>
-                    </nav>
-                    <!-- end header nav -->
+        <div class="page-header">
+            <div class="container">
+                <header class="py-3">
+                    DASHBOARD
                 </header>
             </div>
-            <!-- end body header -->
-            <div class="body-container">
-                <div class="body-container-left">
-                    <ul>
-                        <li>dashboard</li>
-                        <li>page content</li>
-                        <li>teachers</li>
-                        <li>students</li>
-                        
-                    </ul>
-                    <ul>
-                        <li>sdf</li>
-                        <li>asdc</li>
-                    </ul>
-                </div>
-                <div class="body-container-right">
-                
+        </div>
+        <section class="section-top">
+            <div class="container">
+                <div class="dashboard-top">
+                    <article class="border bg-light">
+                        <h2 class="text-center primary-text">
+                            registered students
+                        </h2>
+                        <p>
+                            3123213
+                        </p>
+                    </article>
+                    <article class="border bg-light">
+                        <h2 class="text-center primary-text">
+                            registered teachers
+                        </h2>
+                        <p>
+                            12321
+                        </p>
+                    </article>
+                    <article class="border bg-light">
+                        <h2 class="text-center primary-text">
+                            student's post
+                        </h2>
+                        <p>
+                            12312
+                        </p>
+                    </article>
                 </div>
             </div>
+        </section>
+        <section class="section-middle"></section>
+        <section class="section-bottom"></section>
 
+    </div>
 
-
-     <!-- script -->
-     <script src="<?php base_url();?>js/jquery-3.5.1.js"></script>
-            <script src="<?php base_url();?>js/owl.carousel.js"></script>
-            <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-            <script src="<?php base_url();?>js/script.js"></script>
-            
-            <script>
-                AOS.init();
-              </script>
-
-</body>
-</html>
+<?php
+    include("include/footer.inc.php");
+?>
