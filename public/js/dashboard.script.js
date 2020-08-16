8aa2a8ed9e113a7cf07d99a9deafb78f8bae0510
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-16 21:41:16 +0530

    Update dashboard.script.js

--- public/js/dashboard.script.js
+++ public/js/dashboard.script.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
   $(".side-nav-toggle").click(function (e) {
     e.preventDefault();
-    // $(".side-nav-toggle").parent().find(".side-sub-nav").slideUp(1000);
-    // $(".side-nav-toggle").find("i").toggleClass("fa-angle-down");
-    // $(".side-nav-toggle").find("i").toggleClass("fa-angle-up");
-
     $(this).parent().find(".side-sub-nav").slideToggle();
+    $(this).find("i").toggleClass("fa-angle-down");
+    $(this).find("i").toggleClass("fa-angle-up");
+  });
+
+  $(".nav__link").click(function (e) {
+    e.preventDefault();
+    $(this).parent().find(".sub__nav").slideToggle();
     $(this).find("i").toggleClass("fa-angle-down");
     $(this).find("i").toggleClass("fa-angle-up");
   });
