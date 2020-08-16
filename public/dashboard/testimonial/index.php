6a9a09ec95950fa1d05bc03de789e9d848a3fd15
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-16 21:41:05 +0530

    Update index.php

--- public/dashboard/testimonial/index.php
+++ public/dashboard/testimonial/index.php
@@ -11,37 +11,72 @@ $active = "testimonial";
         <div class="page-header">
             <div class="container">
                 <header class="header-text-1" class="py-3">
-                    testimonials
+                    testimonials List
                 </header>
             </div>
         </div>
         <section class="section-top">
             <div class="container">
-                <div class="dashboard-top">
-                    <article class="border bg-light">
-                        <h2 class="text-center primary-text">
-                            registered students
-                        </h2>
-                        <p>
-                            3123213
-                        </p>
-                    </article>
-                    <article class="border bg-light">
-                        <h2 class="text-center primary-text">
-                            registered teachers
-                        </h2>
-                        <p>
-                            12321
-                        </p>
-                    </article>
-                    <article class="border bg-light">
-                        <h2 class="text-center primary-text">
-                            student's post
-                        </h2>
-                        <p>
-                            12312
-                        </p>
-                    </article>
+                <div class="row">
+                <?php
+                    $sql = "SELECT testimonials.*, std.* FROM testimonials 
+                    LEFT JOIN std
+                        ON std.student_id = testimonials.student_id
+                        
+                    ORDER BY testimonial_id DESC";
+                
+                    $result = mysqli_query($conn, $sql);
+                    $query_results = mysqli_num_rows($result);
+
+                    // echo $query_results;
+                    // $row = mysqli_fetch_assoc($result);
+                    if($query_results > 0){
+
+                        while($row = mysqli_fetch_assoc($result)){
+                ?>
+
+                 
+                    <div class="col-sm-4">
+                        <article class="text-center member-left">
+                            <figure>
+                            <?php 
+
+                                if($row['student_photo'] == ""){
+                            ?>
+                                    <img class="member__img" style="max-height: 200px" src="<?php echo base_url()?>img/teacher/profile_pic/male_profile.svg" alt="">
+                            <?php
+                                } else {
+                            ?>
+                                    <img class="member__img" style="max-height: 300px" src="<?php echo base_url() . $row['student_photo'];?>" alt="">
+                            <?php
+                                }
+                            ?>
+                            </figure>
+                            <header class="member__header header-text-2">
+                               <?php echo $row['student_first_name']; ?>
+                            </header>
+                            
+                            <p class="sub-header-text-1 pt-2">
+                                    <?php 
+                                        $city = $row['city_id'];
+                                        if($city == true){
+                                            $sql = "SELECT * FROM cities WHERE city_id = '$city'";
+                                            $result = mysqli_query($conn, $sql);
+                                            $rows = mysqli_fetch_assoc($result);
+                                            echo $rows['city_name'];
+                                        };
+                                    ?>
+                            <p>
+                                <?php echo $row['testimonial_quote']; ?>
+                            </p>
+                        </article>
+                    </div>
+                <?php 
+                } 
+            }else {
+                    // echo "there are no result";
+                }
+                ?>
                 </div>
             </div>
         </section>
