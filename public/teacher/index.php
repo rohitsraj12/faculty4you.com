6cecde052aecc15e4841a3b9cd77fbf444ba9c84
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-18 18:10:52 +0530

    Update index.php

--- public/teacher/index.php
+++ public/teacher/index.php
@@ -22,7 +22,13 @@
                             Students Post
                         </header>
                     <?php 
-                        // $city = $rows['city_id'];
+                        $user_name = $_SESSION['user_name'];
+                        $user_query = "SELECT * FROM teachers WHERE teacher_user_name = '$user_name'";
+                        $user_result = mysqli_query($conn, $user_query);
+                        $user_row = mysqli_fetch_assoc($user_result);
+
+                        $city = $user_row['city_id'];
+                        $member = $user_row['teacher_membership_status'];
                         
                         $sql = "SELECT posts.*, std.*, cities.*, states.state_name, study_types.study_type_name, study_categories.study_cat_type 
                         FROM posts
@@ -36,6 +42,7 @@
                                 ON study_types.study_type_id = posts.study_type_id
                             JOIN study_categories
                                 ON study_categories.study_cat_id = posts.study_cat_id
+                            WHERE posts.city_id = $city
                             ORDER BY post_id DESC ";
                         $result = mysqli_query($conn, $sql);
                         while($row = mysqli_fetch_assoc($result)){
@@ -43,7 +50,7 @@
                     ?>
 
 
-                        <article class="mt-5 px-5 py-3 border bg-light" >
+                        <article class="student-post mt-5 px-5 py-3 border bg-light" >
                             <header class="border-bottom">
                                 <h1 class="h1 py-3 text-dark font-weight-normal">
                                     <?php echo $row["post_title"];?>
@@ -61,14 +68,45 @@
                             </p>
                             </div>
                             <footer class="pb-3">
-                                <buttom style="font-size: 1.6rem" class="py-2 px-4 btn btn-primary">Contact details</buttom>
+                                <?php 
+                                // echo $member;
+                                    if($member == "active"){
+                                ?>
+                                        <button class="active-member-btn btn btn-primary" style="font-size: 1.6rem">Contact details</button>
+                                <?php
+                                    }else{
+                                        // echo "become a member";
+                                        ?>
+                                        <button class="active-member-btn btn btn-lg btn-primary" type="button" disabled>Contact details</button> </br>
+                                        <!-- <small>you need to become a member to see the details</small> -->
+                                        <?php
+                                    }
+                                    
+                                ?>
                             </footer>
 
-                            <section>
+                            <section class="student-details">
                            
-                                <article>
-                                    Name: <?php echo $row["student_first_name"];?>
-                                </article>
+                                <?php
+                                    if($member == "active"){
+                                ?>
+                                    <article>
+                                        Name: <?php echo $row["student_first_name"];?>
+                                        Phone: <?php echo $row["student_phone"];?>
+                                        Email: <?php echo $row["student_email"];?>
+                                    </article>
+                                <?php
+                                    }else{
+                                ?>
+                                    <article class="">
+                                        <?php
+                                            echo "you need to become a member to see the details";
+                                        
+                                        ?>
+                                    </article>
+                                <?php    
+                                    }
+                                ?>
                             </section>
                         </article>
                         
