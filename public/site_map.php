a00cc503c4bfe72c7c4cc1c9dec3946392bc3e91
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-19 13:04:58 +0530

    Update site_map.php

--- public/site_map.php
+++ public/site_map.php
@@ -3,7 +3,6 @@
     require("../private/config/config.php");
     require_once("../private/config/db_connect.php");
     include_once("../private/required/public/header.public.php");
-    // include("../private/required/public/banner/banner.public.php");
 
 ?>
 
@@ -14,6 +13,30 @@
                 <heeader class="text-primary-h"> 
                     Site map
                 </header>
+            </div>
+            <div class="section-body">
+                <article>
+                    <ul>
+                        <li>
+                            <a href="">home</a>
+                        </li>
+                        <li> <a href="">trainer</a>
+                            <ul>
+                                <li> <a href="">login</a></li>
+                                <li> <a href="">register</a></li>
+                            </ul>
+                        </li>
+                        <li> <a href="">trainee</a>
+                            <ul>
+                                <li> <a href="">login</a></li>
+                                <li> <a href="">register</a></li>
+                            </ul>
+                        </li>
+                        <li> <a href="">faq</a></li>
+                        <li> <a href="">testimonial</a></li>
+                        
+                    </ul>
+                </article>
             </div>
         </section>
     </main>
