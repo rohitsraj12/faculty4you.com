b73bfde2d3d66e528076f3204c33c9f4ae080a1d
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-19 22:11:30 +0530

    Update create-new-password.php

--- public/student/create-new-password.php
+++ public/student/create-new-password.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -9,19 +9,19 @@
 
 <?php
 
-    $selector = $_GET['selector'];
-    $validator = $_GET['validator'];
+    //$selector = $_GET['selector'];
+   // $validator = $_GET['validator'];
 
     //security
-    if(empty($selector) || empty($validator)){
-        echo "could not validate your requiest";
-    } else {
-        if(ctype_xdigit($selector) !== false && ctype_xdigit($validator) !== false ){
+    //if(empty($selector) || empty($validator)){
+       // echo "could not validate your requiest";
+    //} else {
+        //if(ctype_xdigit($selector) !== false && ctype_xdigit($validator) !== false ){
         ?>
 
             <form action="include/reset-password.inc.php" method="post">
-                <input type="hidden" name="selector" value="<?php echo $selector?>">
-                <input type="hidden" name="validator" value="<?php echo $validator?>">
+                <input type="hidden" name="selector" value="<?php// echo $selector?>">
+                <input type="hidden" name="validator" value="<?php// echo $validator?>">
 
                 
                 <input type="password" name="pwd" placeholder="enter password">
@@ -32,8 +32,103 @@
             </form>
 
         <?php
-        }
+       // }
+    //}
+?>
+</body>
+</html> -->
+
+
+
+<?php
+    include_once("../../private/config/config.php");
+    require_once('../../private/config/db_connect.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>create new password</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+    <div class="body-wrap">
+        
+    <div class="member-body-header">
+    <div id="hamberger" class="hamberger">
+                    <span class="hamberger__line"></span>
+                    <span class="hamberger__line"></span>
+                    <span class="hamberger__line"></span>
+     </div>
+                <!-- end hamberger -->
+            <header class="wrap-container">
+                <div class="brand">
+                    <a href="<?php base_url();?>index.php">
+                        <img src="<?php base_url();?>img/brand/header-logo.png" alt="">
+                    </a>
+                </div>
+                <nav class="header__nav">
+                        <ul>
+                            <!-- <li class="nav__list"><a href="<?php base_url();?>student/registration.php" class="nav__link">become teacher</a></li> -->
+                            <li class="nav__list"><a href="<?php base_url();?>teacher/login.php" class="nav__link button-primary">trainer login</a></li>
+                        </ul>
+                    </nav>
+                    <!-- end header nav -->
+            </header>
+        </div>
+
+        <div class="body-container">
+        <div class="section-forgotpwd section-setpwd wrap-container">
+
+            <div class="container" id="container">
+                <div class="form-container password-container">
+                <?php
+
+$selector = $_GET['selector'];
+$validator = $_GET['validator'];
+
+// security
+if(empty($selector) || empty($validator)){
+   echo "could not validate your requiest";
+} else {
+    if(ctype_xdigit($selector) !== false && ctype_xdigit($validator) !== false ){
+    ?>
+
+                    <form action="include/reset-password.inc.php" method="post">
+                        <h1>Create new password</h1>
+                        <input type="hidden" name="selector" value="<?php echo $selector?>">
+                        <input type="hidden" name="validator" value="<?php echo $validator?>">
+
+                        
+                        <input type="password" name="pwd" placeholder="enter password">
+                        <input type="password" name="pwd-repeat" placeholder="reenter password">
+
+                        
+                        <button type="submit" name="reset_password_submit">reset password</button>
+                    </form>
+                    <?php
+       }
     }
 ?>
+                </div>
+               
+            </div>
+            </div>
+        </div>
+    </div>
+    
+
+    
+    <script src="<?php base_url();?>js/jquery-3.5.1.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="<?php base_url();?>js/script.js"></script>
+    
+    <script>
+        AOS.init();
+        </script>
+    
 </body>
 </html>
