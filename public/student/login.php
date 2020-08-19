f92a6e701be863f0f54448707d4090c1361bfa51
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-19 23:49:32 +0530

    Update login.php

--- public/student/login.php
+++ public/student/login.php
@@ -8,7 +8,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log in - teacher</title>
+    <title>Log in - trainee</title>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
