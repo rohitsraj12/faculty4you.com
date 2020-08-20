be305a8d3908f2ea263f80aa1a058ab2503ed424
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-20 22:49:38 +0530

    Update registration.php

--- public/teacher/registration.php
+++ public/teacher/registration.php
@@ -55,7 +55,7 @@
                         <!-- <span>or use your account</span> -->
                         <input type="text" id="user_name"  placeholder="User name / Email"  name="email" />
                         <input type="password" placeholder="Password"  name="password"/>
-                        <a href="#">Forgot your password?</a>
+                        <a href="<?php base_url();?>teacher/reset_password.php">Forgot your password?</a>
                         <button name="login-submit">Login</button>
                     </form>
                 </div>
