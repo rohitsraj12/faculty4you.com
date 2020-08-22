f78941adcaf7a38c345053f1831814c1d3d93ebe
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-23 00:02:44 +0530

    Update validation.js

--- public/js/validation.js
+++ public/js/validation.js
@@ -3,6 +3,7 @@ var emailExp = /^([a-zA-Z0-9\.-]+)([@])([a-zA-z0-9-]+.)([a-z]{2,8})(.[a-z]{2,8})
 var telExp = /^([0-9]{10})$/;
 var textOnlyExp = /^([a-zA-Z]+)$/;
 var pincode = /^([0-9]{6})$/;
+var numExp = /^([0-9]{2})$/;
 
 function validation(){
     var name = document.getElementsByClassName("name"); //13
@@ -11,6 +12,12 @@ function validation(){
     var gender = document.getElementsByClassName("gender"); //79
     var email = document.getElementsByClassName("email"); //115
     var phone = document.getElementsByClassName("phone"); //119
+    var city = document.getElementsByClassName("city");//133
+    var state = document.getElementsByClassName("state"); //147
+    var pin = document.getElementsByClassName("pincode"); //160
+    var teachingExp = document.getElementsByClassName("experience"); //193
+    var subject = document.getElementsByClassName("subject"); //198
+    var about = document.getElementsByClassName("about");//242
 
     //first name
     if(name[0].value == ""){
@@ -33,10 +40,12 @@ function validation(){
 
     //gender
 
-    if(gender[0].checked == false || gender[1].checked == false || gender[2].checked == false){
-        alert("please select any one");
-        return false;
-    }
+    // if(gender[0].checked == false || gender[1].checked == false || gender[2].checked == false){
+    //     alert("please select any one");
+    //     return false;
+    // }
+
+    //email
     if(email[0].value == ""){
         alert("please enter mail id");
         return false;
@@ -44,13 +53,65 @@ function validation(){
         alert("enter valid email");
         return false;
     }
+
+    //phone
     if(phone[0].value == ""){
         alert("enter phone number");
         return false;
     }else if(!telExp.test(phone[0].value)){
         alert("enter valid phone number.");
         return false;
-    }else {
+    }
+    
+    //city
+    if(city[0].value == "nooption"){
+        alert("please choose city");
+        return false;
+    }
+
+    //state
+    if(state[0].value == "nooption"){
+        alert("please choose state");
+        return false;
+    }
+
+
+    //study category
+    
+    //teaching experience
+
+    if(teachingExp[0].value == ""){
+        alert("please  enter your teaching experience");
+        return false;
+    }
+    
+    if(!numExp.test(teachingExp[0].value)){
+        alert("please enter valid number");
+        return false;
+    }
+
+    //academic subject
+
+    if(subject[0].value == "nooption"){
+        alert("please enter subject");
+        return false;
+    }
+
+    //about me
+
+    if(about[0].value == ""){
+        alert("please enter something about you");
+        return false;   
+    }
+
+
+    //pincode
+
+    if(!pincode.test(pin[0].value)){
+        alert("please enter valid pincode");
+        return false;
+    }
+    else {
         return true;
     }
 
@@ -61,4 +122,35 @@ function validation(){
     //image
 
 
+}
+
+
+//student post validation
+
+function studentPost(){
+    var title = document.getElementsByClassName("title");
+    var postDetail = document.getElementsByClassName("detail");
+
+
+    if(title[0].value == ""){
+        alert("please enter post title");
+        return false;
+    }else if(title[0].value.length > 30){
+        alert("not more than 30 ltrs");
+        return false;
+    }
+// about 
+
+if(postDetail[0].value == ""){
+    alert("please enter your requirement");
+    return false;
+
+}else if(postDetail[0].value.length < 20){
+    alert("you need to write atleast 20 letters");
+    return false;
+}
+
+    else{
+        return true;
+    }
 }
