0a9bb84953bc804b9c4ec3c05425d503adf2516c
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-23 13:04:44 +0530

    Update search_result.php

--- public/teacher/search_result.php
+++ public/teacher/search_result.php
@@ -12,103 +12,12 @@
 
     include("include/header.inc.php");
     
-        include_once'include/search-banner.inc.php';
+       // include_once'include/search-banner.inc.php';
 
 ?>
 <div class="body-container">
 
-    <main>
-        <section class="section-search-result wrap-container">
-
-<?php
-if(isset($_POST["submit-search"])){
-    // security
-    $search = mysqli_real_escape_string($conn, $_POST["search"]);
-
-    $sql = "SELECT * FROM teachers WHERE teacher_user_name LIKE '%$search%'";
-    $result = mysqli_query($conn, $sql);
-    $query_results = mysqli_num_rows($result);
-
-    ?>
-        <div class="section-header u-center-text"  >
-            <heeader class="text-primary-h"> 
-                Search result of student 
-            </header>
-        </div>
-        <div class="search-result-num" >
-            <p>
-                    <?php echo $query_results; ?> results are matching
-            </p>
-        </div>
-        <section class="section-body"> 
-    <?php 
-
-   
-
-    if($query_results > 0){
-        while($row = mysqli_fetch_assoc($result)){
-
-            ?>      
-            
-            
-            <article class="article-search-result" >
-                    <div class="block-left">
-                        <figure class="article-search__figure">
-                            <img src="<?php base_url()?>img/teacher/profile_pic/rohit.jpg" alt="">
-                        </figure>
-                    </div>
-                    <div class="block-right">
-                        <header class="article-search__header">
-                            <h1 class="text-secondary">
-                                <?php echo $row['teacher_user_name'];?>
-                            </h1>
-                        </header>
-                        <div class="article-search-body">
-                                <ul>
-                                    <li>Qualification: <strong></strong></li>
-                                    <li>Experience: <strong></strong></li>
-                                    <li>City:<strong></strong></li>
-                                    <li>Available for : 
-                                        <strong><?php if($row["teacher_online_one_to_one"] == 1){echo "Online one to one";} ?></strong>
-                                        <strong> <?php if($row["teacher_online_group"] == 1){echo "Online group";}?></strong>
-                                        <strong> <?php if($row["teacher_home_tuition"] == 1){echo "home tuition";}?> </strong>
-                                    
-                                    </li>
-                                    
-                                </ul>
-                        </div>
-                        
-                        <footer class="article-search-footer">
-                            <a class="button-text-1" href="detail.php?name=<?php echo $row['teacher_user_name']?>&batch=<?php echo $row['teacher_email']?>">more details</a>
-                        </footer>
-                    </div>
-                </article>
-            
-            
-            
-            <?php
-
-        }
-    } else {
-        //echo "there are no result";
-        
-        ?> 
-        
-        <div class="search-result-num" >
-            <p>
-                there are no result
-            </p>
-        </div>
-
-        <?php
-    }
-}
- 
-?>      
-            </section>
-        </section>
-
-    </main>
+    
 </div>
 
 <?php
