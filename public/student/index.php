2f0d695383d234c2c7bc8dfdc2bf6f6c0a6716e4
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-24 18:28:37 +0530

    Update index.php

--- public/student/index.php
+++ public/student/index.php
@@ -35,20 +35,74 @@
                 </div>    
                 <section class="row">
                     <div class="col-sm-3 mt-5">
-                        <ul class=" border py-5 px-4">
-                            <li class="study-type mb-4 pb-4 pl-2 border-bottom" data-study-type="study-type-1"><a href="#" class="text-danger text-primary h2">Academic</a></li>
-                            <li class="study-type  pl-2" data-study-type="study-type-2"><a href="#"  class=" text-primary h2">Non-academic</a></li>
+                        <ul class=" border py-5 px-4 tab">
+                            <li class="study-type pl-2 col-6 col-sm-12" data-study-type="study-type-1"><button class="tablinks active" data-study-type="study-type-1">academic</button></li>
+                            <li class="study-type  col-6 col-sm-12  pl-2" data-study-type="study-type-2"><button class="tablinks" data-study-type="study-type-2">non-academic</button></li>
                         </ul>
                     </div>
                     <div class="col-sm-9">
                         <section class="wrap-study-type study-type-1">
 
                             <?php
-                             $category_query = "SELECT * FROM teachers
-                             WHERE category_id = 1";
+                             $category_query = "SELECT teachers.*, cities.*, subjects.*, study_categories.* 
+                             FROM teachers
+                                JOIN cities
+                                    ON cities.city_id = teachers.city_id
+                                JOIN subjects
+                                    ON subjects.subject_id = teachers.subject_id
+                                JOIN study_categories
+                                    ON study_categories.category_id = teachers.category_id
+
+                             WHERE teachers.category_id = 1 AND teachers.teacher_membership_status = 'active' ";
+
                              $category_result = mysqli_query($conn, $category_query);
                              while($row = mysqli_fetch_assoc($category_result)){
-                                 echo $row["teacher_first_name"]. '</br>';
+                                 //echo $row["teacher_first_name"]. '</br>';
+
+                                 ?>
+
+                                 <article class="row mb-5 border">
+                                     <div class="col-sm-3">
+                                        <figure>
+                                            <?php
+                                                    if($row['teacher_photo'] == ""){
+                                            ?>
+                                                    <img class="img-fluid img-rounded" style="max-height: 200px" src="<?php base_url();?>img/teacher/profile_pic/male_profile.svg" alt="">
+                                            <?php
+                                                } else {
+                                            ?>
+                                                    <img class="img-fluid img-rounded" style="max-height: 300px" src="<?php echo base_url() . $row['teacher_photo'];?>" alt="">
+                                            <?php
+                                                }
+                                            ?>
+                                        </figure>
+                                                        
+                                     </div>
+                                     <div class="col-sm-9">
+                                       
+                                        <ul class="d-flex flex-row bd-highlight py-4 h4 font-weight-normal text-secondary">
+                                                <li class="mr-5"><i class="fa fa-calendar mr-2" aria-hidden="true"></i><?php echo $row["sub_name"];?></li>
+                                                <li class="mr-5"><i class="fa fa-graduation-cap mr-2" aria-hidden="true"></i><?php echo $row["teacher_experience"] . ' years of experience';?></li>
+                                                <li class="mr-5"><i class="fa fa-university mr-2" aria-hidden="true"></i><?php echo $row["study_cat_type"];?></li>
+                                                <li class="mr-5"><i class="fa fa-map-marker mr-2" aria-hidden="true"></i><?php echo $row["city_name"];?></li>
+                                            
+                                            </ul>
+                                            <h1 class="pb-3">
+                                        <?php
+                                         echo $row['teacher_first_name'] . " " . $row['teacher_last_name'];
+                                        ?>
+                                     </h1>
+                                     <p>
+                                        <?php
+                                            echo $row['teacher_about_me'];
+                                        ?>
+                                     </p>
+                                     </div>
+                                   
+
+                                 </article>
+                                 <?php
+                            
                              }
                              
                             ?>
@@ -56,9 +110,17 @@
                         </section>
                         <section class="wrap-study-type study-type-2">
                             <?php
-                                $category_query = "SELECT * FROM teachers
-                                WHERE category_id = 2";
-                                $category_result = mysqli_query($conn, $category_query);
+                             $category_query = "SELECT teachers.*, cities.*, subjects.*, study_categories.* 
+                             FROM teachers
+                                JOIN cities
+                                    ON cities.city_id = teachers.city_id
+                                JOIN subjects
+                                    ON subjects.subject_id = teachers.subject_id
+                                JOIN study_categories
+                                    ON study_categories.category_id = teachers.category_id
+
+                             WHERE teachers.category_id = 2 AND teachers.teacher_membership_status = 'active' ";
+                             $category_result = mysqli_query($conn, $category_query);
                                 while($row = mysqli_fetch_assoc($category_result)){
                                     echo $row["teacher_first_name"]. '</br>';
                                 }
