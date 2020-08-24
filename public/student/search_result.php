a3ae547f6b730f362919a7117307bd2b316adee0
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-25 04:01:43 +0530

    Update search_result.php

--- public/student/search_result.php
+++ public/student/search_result.php
@@ -24,77 +24,252 @@
 
  <div class="body-container">
     <main>
-        <section class="section-result wrap-container">
+    <main>
+    <section class="section-search-result wrap-container">
+                    <div class="section-header u-center-text">
+                        <heeader class="text-primary-h"> 
+                            Search result
+                        </header>
+                    </div>
 
-<?php
-if(isset($_POST["submit-search"])){
-    // security
-    $search = mysqli_real_escape_string($conn, $_POST["search"]);
+                    <?php
+                    
+                if(isset($_POST["submit-search"])){
+                    // security
+                    $search = mysqli_real_escape_string($conn, $_POST["search"]);
 
-    $sql = "SELECT * FROM teachers WHERE teacher_user_name LIKE '%$search%'";
-    $result = mysqli_query($conn, $sql);
-    $query_results = mysqli_num_rows($result);
+                    $sql = "SELECT * FROM posts WHERE post_title LIKE '%$search%'";
+                    $result = mysqli_query($conn, $sql);
+                    $query_results = mysqli_num_rows($result);
 
-    ?>
-        <div class="section-header u-center-text" >
-            <heeader class="text-primary-h"> 
-                Search result
-            </header>
-            <p class="u-padding-top-big u-left-text">
-                <?php 
-                        echo "there are ". $query_results . " results are matching";
                 ?>
-            </p>
-        </div>
-    <?php 
+                    <div class="search__form mb-5 bg-light border p-5">
+                        <form action="" method="post">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <input type="search" class="form-control" name="search" id="inputAddress" placeholder="">
+                                    <small class="msg">Enter your subject / pincode / study type</small>
+                                </div>
+                                <div class="col-sm-3">
+                                    <select id="inputState" name="city" class="form-control">
+                                        <option selected>Choose...</option>
+                                        <?php 
+                                            // #task city data fetch
+                                            $city_query = "SELECT * FROM cities";
+                                            $result = mysqli_query($conn, $city_query);
+                                            while( $row = mysqli_fetch_assoc($result)){
+                                            
+                                        ?>
+                                        <option value="<?php echo $row['city_id'];?>"><?php echo $row['city_name'];?></option>
+                                        <?php
+                                        }
+                                        ?>
+                                    </select>  
+                                    <small class="msg">Select city name</small>
 
-   
+                                </div>
+                                <div class="col-sm-3">
+                                    <input class="btn btn-primary w-100 py-2" style="font-size: 1.4rem" name="submit-search" type="submit" value="search">
+                                
+                                </div>
+                            </div>
+                            <!-- </div> -->
+                        </form>
+                    </div>
+                    
+                    <!-- <div class="search-result-num" >
+                        <p>
+                                <?php echo $query_results; ?> results are matching
+                        </p>
 
-    if($query_results > 0){
-        while($row = mysqli_fetch_assoc($result)){
+                    </div> -->
+                    <section class="section-body"> 
 
-    ?>      
-          <article>
-            <div class="left__block">
-                <figure>
-                    <img src="" alt="">
-                </figure>
+                    
+                        <?php 
+
+                            if($query_results > 0){
+                                while($row = mysqli_fetch_assoc($result)){
+
+                        ?>      
+                                
+                            <?php
+
+                                }
+                            } else {
+                                // echo "there are no result";
+                                ?> 
+                                
+                                <div class="search-result-num" >
+                                    <p>
+                                        there are no result
+                                    </p>
+                                </div>
+
+                                <?php
+                            }
+                        
+                        ?>
+                    </section>      
+        </section> 
+
+            <div class="wrap-container">
+                <section class="row section__post">
+                    <div class="col-sm-3">
+                        <div class="tab" >
+                            <button class="tablinks active" data-post="view__post">Search result</button>
+                            <!-- <button class="tablinks" data-post="compose__post">city</button>
+                            <button class="tablinks" data-post="compose__post">city</button> -->
+                        </div>
+                    </div>
+                    <div class="col-sm-9">
+                        <!-- <section  class="view__post post__cat" id="viewPost">
+                            <header class="text-primary-h text-center pb-5 mb-5" >
+                                search posts
+                            </header>
+                             -->
+
+                             <?php
+                                
+                                $search = mysqli_real_escape_string($conn, $_POST["search"]);
+
+                                $city = mysqli_real_escape_string($conn, $_POST["city"]);
+                                
+                                $sql = "SELECT posts.*, std.*, study_types.*, study_categories.*, cities.* 
+                                FROM posts
+                                JOIN std
+                                    ON std.student_id = posts.student_id
+                                    JOIN cities
+                                    ON cities.city_id = std.city_id
+                                JOIN study_types
+                                    ON study_types.study_type_id = posts.study_type_id
+                                JOIN study_categories
+                                    ON study_categories.category_id = posts.study_cat_id
+                                WHERE (posts.post_title LIKE '%$search%' AND std.city_id LIKE '%$city%') AND (posts.post_title LIKE '%$search%') AND (std.city_id LIKE '%$city%')
+                                ORDER BY post_date DESC";
+                                $result = mysqli_query($conn, $sql);
+                                $query_results = mysqli_num_rows($result);
+                             ?>
+                        <div class="search-result-nu" >
+                        <p>
+                                <?php echo $query_results; ?> results are matching
+                        </p>
+                    </div>
+                            <?php 
+
+                                while($row = mysqli_fetch_assoc($result)){
+
+                            ?>
+                            <article class="mb-5 px-5 py-3 border bg-light" >
+                                <header class="border-bottom">
+                                    <h1 class="h1 py-3 text-dark font-weight-normal">
+                                        <?php echo $row["post_title"];?>
+                                    </h1>
+                                </header>
+                                <div class="body mb-4">
+                                    <ul class="d-flex flex-row bd-highlight py-4 h4 font-weight-normal text-secondary">
+                                        <li class="mr-5"><i class="fa fa-calendar mr-2" aria-hidden="true"></i><?php echo $row["post_date"];?></li>
+                                        <li class="mr-5"><i class="fa fa-graduation-cap mr-2" aria-hidden="true"></i><?php echo $row["study_type_name"];?></li>
+                                        <li class="mr-5"><i class="fa fa-university mr-2" aria-hidden="true"></i><?php echo $row["study_cat_type"];?></li>
+                                        <li class="mr-5"><i class="fa fa-map-marker mr-2" aria-hidden="true"></i><?php echo $row["city_name"];?></li>
+                                    </ul>
+                                <p class="text-dark">
+                                    <?php echo $row["post_detail"];?>
+                                </p>
+                                </div>
+                                <footer class="pb-3">
+                                            <?php 
+                                                // echo $member;
+                                                if($member == "active"){
+                                            ?>
+                                                    <button class="active-member-btn btn btn-link" style="font-size: 1.6rem">Contact details</button>
+                                            <?php
+                                                }else{
+                                                    // echo "become a member";
+                                                    ?>
+                                                    <button class="active-member-btn btn btn-link"  style="font-size: 1.6rem">Contact details</button> </br>
+                                                    <!-- <small>you need to become a member to see the details</small> -->
+                                                    <?php
+                                                }
+                                            ?>
+                                        </footer>
+
+                                        <section class="student-details py-3">
+
+                                            <?php
+                                                if($member == "active"){
+                                            ?>
+                                                <article>
+                                                    <header>
+
+                                                    </header>
+
+                                                    <div class="row">
+                                                        <div class="col-sm-3">
+                                                            <figure>
+                                                                <?php
+                                                                     if($row['student_photo'] == ""){
+                                                                ?>
+                                                                        <img class="img-fluid img-rounded" style="max-height: 200px" src="<?php echo base_url()?>img/teacher/profile_pic/male_profile.svg" alt="">
+                                                                <?php
+                                                                    } else {
+                                                                ?>
+                                                                        <img class="img-fluid img-rounded" style="max-height: 300px" src="<?php echo base_url() . $row['student_photo'];?>" alt="">
+                                                                <?php
+                                                                    }
+                                                                    ?>
+                                                            </figure>
+                                                        </div>
+                                                        <div class="col-sm-9">
+                                                            <ul class="student-info">
+                                                                <li>
+                                                                    <i class="fa fa-user pr-2" aria-hidden="true"></i><?php echo $row["student_first_name"] ." " . $row["student_last_name"];?>    
+                                                                </li>
+                                                                <li>
+                                                                    <i class="fa fa-phone pr-2" aria-hidden="true"></i><a href="#"><?php echo $row["student_phone"];?></a>                                                                    
+                                                                </li>
+                                                                <li>
+                                                                    <i class="fa fa-envelope pr-2" aria-hidden="true"></i><a href="#"><?php echo $row["student_email"];?></a>
+                                                                </li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                                                                    
+                                                    
+                                                </article>
+                                            <?php
+                                                }else{
+                                            ?>
+                                                <article class="">
+                                                    <?php
+                                                        echo "you need to become a member to see the details";
+                                                    
+                                                    ?>
+                                                </article>
+                                            <?php    
+                                                }
+                                            ?>
+                                        </section>
+                            </article>
+                                
+                            <?php
+                                }
+                            ?>
+                        </section>
+                        <section  class="compose__post post__cat" id="composePost">
+                            <div class="section-profile-update">
+                                <header class="text-primary-h text-center pb-5 mb-5" >
+                                    Compose new post
+                                </header>
+                            </div>
+                        </section>             
+                    </div>
+                </section>
             </div>
-            <div class="right__block">
-                <header>
-                    <h1>
-                        <?php echo $row['teacher_user_name'];?>
-                    </h1>
-                </header>
-
-                <div class="body">
-                    <p>
-                        <?php echo $row['teacher_about_me'];?>
-                    </p>
-                    <ul>
-                        <li>Qualification: </li>
-                        <li>Experience: </li>
-                        <li>City:</li>
-                        <li><?php if($row["teacher_online_one_to_one"] == 1){echo "Online one to one";} ?> / <?php if($row["teacher_online_group"] == 1){echo "Online group";}?>/ <?php if($row["teacher_home_tuition"] == 1){echo "home tuition";}?> </li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </div>
-                
-                <footer>
-                    <a href="detail.php?name=<?php echo $row['teacher_user_name']?>&batch=<?php echo $row['teacher_email']?>">More details</a>
-                </footer>
-            </div>
-          </article>
-            
-    <?php
-        }
-    } else {
-        echo "there are no result";
-    }
-}
- 
-?>       </section>
+            <?php
+            }
+            ?>
+    </main>
     </main>
 </div>
 
