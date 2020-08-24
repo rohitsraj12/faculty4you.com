d7366930a5d281c7a70842f29134cfbc44af399c
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-25 04:01:40 +0530

    Update index.php

--- public/student/index.php
+++ public/student/index.php
@@ -71,7 +71,7 @@
                                             <?php
                                                 } else {
                                             ?>
-                                                    <img class="img-fluid img-rounded" style="max-height: 300px" src="<?php echo base_url() . $row['teacher_photo'];?>" alt="">
+                                                    <img class="img-fluid img-rounded" style="max-height: 150px" src="<?php echo base_url() . $row['teacher_photo'];?>" alt="">
                                             <?php
                                                 }
                                             ?>
@@ -81,7 +81,7 @@
                                      <div class="col-sm-9">
                                        
                                         <ul class="d-flex flex-row bd-highlight py-4 h4 font-weight-normal text-secondary">
-                                                <li class="mr-5"><i class="fa fa-book mr-2" aria-hidden="true"></i><?php echo $row["sub_name"];?></li>
+                                                <li class="mr-5"><i class="fa fa-book mr-2" aria-hidden="true"></i><?php echo $row["sub_name"];?> trainer</li>
                                                 <li class="mr-5"><i class="fa fa-paw mr-2" aria-hidden="true"></i><?php echo $row["teacher_experience"] . ' years of experience';?></li>
                                                 <li class="mr-5"><i class="fa fa-university mr-2" aria-hidden="true"></i><?php echo $row["study_cat_type"];?></li>
                                                 <li class="mr-5"><i class="fa fa-map-marker mr-2" aria-hidden="true"></i><?php echo $row["city_name"];?></li>
