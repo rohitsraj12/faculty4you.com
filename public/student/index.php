235c323ced45c4064bb123e837ba8046bec1e36b
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-25 00:20:03 +0530

    Update index.php

--- public/student/index.php
+++ public/student/index.php
@@ -27,15 +27,15 @@
     ?>
     <div class="body-container">
         <main>
-            <section>
+            <section class="wrap-container">
                 <div class="section-header u-center-text" >
                     <heeader class="text-primary-h"> 
                         Trainer detail
                     </header>
                 </div>    
                 <section class="row">
-                    <div class="col-sm-3 mt-5">
-                        <ul class=" border py-5 px-4 tab">
+                    <div class="col-sm-3">
+                        <ul class="px-4 tab">
                             <li class="study-type pl-2 col-6 col-sm-12" data-study-type="study-type-1"><button class="tablinks active" data-study-type="study-type-1">academic</button></li>
                             <li class="study-type  col-6 col-sm-12  pl-2" data-study-type="study-type-2"><button class="tablinks" data-study-type="study-type-2">non-academic</button></li>
                         </ul>
@@ -63,7 +63,7 @@
 
                                  <article class="row mb-5 border">
                                      <div class="col-sm-3">
-                                        <figure>
+                                        <figure class="pt-4">
                                             <?php
                                                     if($row['teacher_photo'] == ""){
                                             ?>
@@ -81,8 +81,8 @@
                                      <div class="col-sm-9">
                                        
                                         <ul class="d-flex flex-row bd-highlight py-4 h4 font-weight-normal text-secondary">
-                                                <li class="mr-5"><i class="fa fa-calendar mr-2" aria-hidden="true"></i><?php echo $row["sub_name"];?></li>
-                                                <li class="mr-5"><i class="fa fa-graduation-cap mr-2" aria-hidden="true"></i><?php echo $row["teacher_experience"] . ' years of experience';?></li>
+                                                <li class="mr-5"><i class="fa fa-book mr-2" aria-hidden="true"></i><?php echo $row["sub_name"];?></li>
+                                                <li class="mr-5"><i class="fa fa-paw mr-2" aria-hidden="true"></i><?php echo $row["teacher_experience"] . ' years of experience';?></li>
                                                 <li class="mr-5"><i class="fa fa-university mr-2" aria-hidden="true"></i><?php echo $row["study_cat_type"];?></li>
                                                 <li class="mr-5"><i class="fa fa-map-marker mr-2" aria-hidden="true"></i><?php echo $row["city_name"];?></li>
                                             
