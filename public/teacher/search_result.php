d3dcaee5445718e5c9919d8fdf93ead3ad4ceb19
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-25 12:07:13 +0530

    Update search_result.php

--- public/teacher/search_result.php
+++ public/teacher/search_result.php
@@ -177,7 +177,7 @@
                                     while($row = mysqli_fetch_assoc($result)){
 
                                 ?>
-                                <article class="mb-5 px-5 py-3 border bg-light" >
+                                <article class="student-post mb-5 px-5 py-3 border bg-light" >
                                     <header class="border-bottom">
                                         <h1 class="h1 py-3 text-dark font-weight-normal">
                                             <?php echo $row["post_title"];?>
@@ -309,7 +309,7 @@
                                         while($row = mysqli_fetch_assoc($result)){
 
                                     ?>
-                                    <article class="mb-5 px-5 py-3 border bg-light" >
+                                    <article class="student-post mb-5 px-5 py-3 border bg-light" >
                                         <header class="border-bottom">
                                             <h1 class="h1 py-3 text-dark font-weight-normal">
                                                 <?php echo $row["post_title"];?>
@@ -327,16 +327,85 @@
                                         </p>
                                         </div>
                                         <footer class="pb-3">
-                                            <a href="<?php base_url();?>teacher/registration.php" style="font-size: 1.6rem" class="py-2 px-4 btn btn-primary">Sign up</a>
-                                            <!-- <a href="<?php base_url();?>teacher/login.php">log in</a> -->
-                                        </footer>
+                                                <?php 
+                                                    // echo $member;
+                                                    if($member == "active"){
+                                                ?>
+                                                        <button class="active-member-btn btn btn-link" style="font-size: 1.6rem">Contact details</button>
+                                                <?php
+                                                    }else{
+                                                        // echo "become a member";
+                                                        ?>
+                                                        <button class="active-member-btn btn btn-link"  style="font-size: 1.6rem">Contact details</button> </br>
+                                                        <!-- <small>you need to become a member to see the details</small> -->
+                                                        <?php
+                                                    }
+                                                ?>
+                                            </footer>
+
+                                            <section class="student-details py-3">
+
+                                                <?php
+                                                    if($member == "active"){
+                                                ?>
+                                                    <article>
+                                                        <header>
+
+                                                        </header>
+
+                                                        <div class="row">
+                                                            <div class="col-sm-3">
+                                                                <figure>
+                                                                    <?php
+                                                                        if($row['student_photo'] == ""){
+                                                                    ?>
+                                                                            <img class="img-fluid img-rounded" style="max-height: 200px" src="<?php echo base_url()?>img/teacher/profile_pic/male_profile.svg" alt="">
+                                                                    <?php
+                                                                        } else {
+                                                                    ?>
+                                                                            <img class="img-fluid img-rounded" style="max-height: 300px" src="<?php echo base_url() . $row['student_photo'];?>" alt="">
+                                                                    <?php
+                                                                        }
+                                                                        ?>
+                                                                </figure>
+                                                            </div>
+                                                            <div class="col-sm-9">
+                                                                <ul class="student-info">
+                                                                    <li>
+                                                                        <i class="fa fa-user pr-2" aria-hidden="true"></i><?php echo $row["student_first_name"] ." " . $row["student_last_name"];?>    
+                                                                    </li>
+                                                                    <li>
+                                                                        <i class="fa fa-phone pr-2" aria-hidden="true"></i><a href="#"><?php echo $row["student_phone"];?></a>                                                                    
+                                                                    </li>
+                                                                    <li>
+                                                                        <i class="fa fa-envelope pr-2" aria-hidden="true"></i><a href="#"><?php echo $row["student_email"];?></a>
+                                                                    </li>
+                                                                </ul>
+                                                            </div>
+                                                        </div>
+                                                                                                        
+                                                        
+                                                    </article>
+                                                <?php
+                                                    }else{
+                                                ?>
+                                                    <article class="">
+                                                        <?php
+                                                            echo "you need to become a member to see the details";
+                                                        
+                                                        ?>
+                                                    </article>
+                                                <?php    
+                                                    }
+                                                ?>
+                                            </section>
                                     </article>
                                         
                                     <?php
                                         }
                                     ?>
                         </section>
-                        
+
                         <section  class="2 post__cat" id="composePost">
                                                        <!--     <header class="text-primary-h text-center pb-5 mb-5" >
                                 search posts
