1c145c8aa4af1e644e83af0d1ac8e1da3d8fbd71
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-29 23:55:15 +0530

    Update banner.inc.php

--- public/student/include/banner.inc.php
+++ public/student/include/banner.inc.php
@@ -2,7 +2,7 @@
             <div class="body-banner-member" >
 
 <div class="banner-wrap">
-    <section class="banner-content">
+    <!-- <section class="banner-content">
         <article>
             <header class="banner-content__header text-left">
                 <h2 class="text-primary-h pb-4 pt-4">Faculty4you take learning  to new heights</h2>
@@ -21,12 +21,21 @@
             </div>
         </article>
     </section>
-    <!-- end banner content -->
     <div class="banner-image">
         <img src="<?php base_url();?>img/banner/<?php echo $banner_image;?>" class="banner__image" alt="<?php echo $page_title;?>">
-    </div>
+    </div> -->
     <!-- end banner imager -->
-</div>
+</div> -->
 <!-- end banner wrap -->
+
+<div class="banner-content-body wrap-container">
+                <form action="<?php base_url();?>student/search_result.php" method="POST" class="banner__form">
+                    <input type="text" name="search" placeholder="search trainer: subject (academic) / activity (non-academic)"  class="banner-search" >
+                    <button type="submit" name="submit-search" class="banner__button" ><i class="fa fa-search"aria-hidden="true"></i></button>
+                </form>
+                <p class="text-left">
+                    <span class="highlight-secondary">Current  trending courses:</span> <strong class="note-primary">IIT-JEE</strong>, <strong class="note-primary">NEET</strong>, <strong class="note-primary">Dance</strong>, <strong class="note-primary">Yoga</strong>
+                </p>
+            </div>
 </div>
 <!-- end body banner -->
