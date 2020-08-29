293a1d94ae45726c9a5c0813b2f02cdd990b2c95
rohitsraj12 <rohitsraj12@gmail.com>
2020-08-29 23:52:06 +0530

    Update banner.public.php

--- private/required/public/banner/banner.public.php
+++ private/required/public/banner/banner.public.php
@@ -5,16 +5,16 @@
     <section class="banner-content">
         <article>
             <header class="banner-content__header">
-                <h2 class="text-primary-h">Heartly welcome to '<strong>Faculty for you</strong>'</h2>
+                <h2 class="text-primary-h-3 pb-4 mb-4">Hearty welcome to <strong class="extra-b f-link">'Faculty for you'</strong></h2>
                 <h5 class="text-secondary-h pb-4">
-                    we provide top <strong>trainers</strong> for <strong class="highlight-primary">academic/non-academic</strong> and <strong class="highlight-primary">online/offline</strong> activities.   
+                    we provide top <strong>trainers</strong> for <strong class="highlight-primary">academic/non-academic</strong> and <strong class="highlight-primary">online/offline</strong> activities all over <strong>India</strong>.   
                 </h5>
-                 <!-- <p class="text-primary-h"> 
+                 <p class="text-primary-h-1 mt-5"> 
                     Hurry... Register...
                 </p>
-                <p class="text-secondary-h">
-                    it's our responsibility to shift you from ordinary to the realm of extraordinary...
-                </p> -->
+                <p class="text-secondary-h-2 ">
+                    It's our responsibility to shift you from ordinary to the realm of extraordinary...
+                </p>
             </header>
             <div class="banner-content-body">
                 <!-- <div class="body-top">
@@ -32,7 +32,7 @@
                 </p> -->
                 
                 <form action="<?php base_url();?>search_result.php" method="POST" class="banner__form">
-                    <input type="text" name="search" placeholder="search top teachers city / subject / category"  class="banner-search" >
+                    <input type="text" name="search" placeholder="search: subject (academic) / activity (non-academic) / city"  class="banner-search" >
                     <button type="submit" name="submit-search" class="banner__button" ><i class="fa fa-search"aria-hidden="true"></i></button>
                 </form>
                 <p>
