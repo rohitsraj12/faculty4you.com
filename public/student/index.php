f683fdf0f85cff17cd7d4b259b4ea7e231728411
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-08-30 16:20:05 +0530

    Update index.php

--- public/student/index.php
+++ public/student/index.php
@@ -29,7 +29,7 @@
         <main>
             <section class="wrap-container">
                 <div class="section-header u-center-text" >
-                    <heeader class="text-primary-h"> 
+                    <heeader class="text-primary-h-3"> 
                         Trainer detail
                     </header>
                 </div>    
