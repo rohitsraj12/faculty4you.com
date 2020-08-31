e225ae319841126ed61190a38773cb6b88c26651
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-09-01 00:40:52 +0530

    Update profile_update.php

--- public/teacher/profile/profile_update.php
+++ public/teacher/profile/profile_update.php
@@ -43,14 +43,14 @@
 
             <div class="section-body">
                 <section class="section-update-form">
-                    <form action="" method="post" class="section__form section__form-update" enctype="multipart/form-data" onsubmit="return validation()">
+                    <form action="" method="post" class="section__form section__form-update" enctype="multipart/form-data" onsubmit="return trainerValidation()">
                         <article class="mb-5" >
                             <header class="p-4 h3 article-profile__header text-light m-0">
                                 Personal information
                             </header>
                             <div class="py-4 px-5 text-dark bg-light border">
                         
-                                <div class="form-row pt-3">
+                                <div class="form-row mb-4 mt-4">
                                     <div class="form-group wrap-form col-md-6">
                                         <label for="first_name">First name</label> 
                                         <span class="error-msg"></span>
@@ -58,15 +58,17 @@
                                     </div>
                                     <div class="form-group col-md-6">
                                     <label for="last_name">Last name</label>
+                                    <span class="error-msg"></span>
                                     <input type="text" name="last_name" class="form-control name" id="last_name" >
                                     </div>
                                 </div>
 
-                                <div class="form-group">
+                                <div class="form-group mb-4">
                                     <label for="photo">Upload image</label>
-                                    <input type="file" name="file" class="form-control-file" id="photo"  value="<?php //echo $row['teacher_photo'];?>" placeholder="<?php //echo $row['teacher_photo'];?>">
+                                    <span class="error-msg"></span>
+                                    <input type="file" name="file" class="form-control-file pt-0" id="photo"  value="<?php //echo $row['teacher_photo'];?>" placeholder="<?php //echo $row['teacher_photo'];?>">
                                 </div>
-                                <fieldset class="form-group">
+                                <fieldset class="form-group mb-4">
                                     <div class="row">
                                         <label class="label col-form-label col-sm-2 pt-0">Gender</label>
                                         <div class="col-sm-8 row">
@@ -102,26 +104,29 @@
                                     </div>
                                 </fieldset>
                         
-                                <div class="form-row">
+                                <div class="form-row mb-4">
                                     <div class="form-group col-md-6">
                                     <label for="email">Email</label>
+                                    <span class="error-msg"></span>
                                     <input type="email" name="email" class="form-control email" id="email" value="<?php //echo $row['teacher_email']; ?>" placeholder="<?php //echo $row['teacher_email']; ?>">
                                     </div>
                                     <div class="form-group col-md-6">
                                     <label for="phone">Telephone</label>
+                                    <span class="error-msg"></span>
                                     <input type="text" name="phone" class="form-control phone" id="phone" value="<?php //echo $row['teacher_phone']; ?>" placeholder="<?php //echo $row['teacher_phone']; ?>">
                                     </div>
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group mb-4">
                                     <label for="address">Address</label>
                                     <input type="text" name="address" class="form-control" id="address" value="<?php //echo $row['teacher_address']; ?>" placeholder="<?php //echo $row['teacher_address']; ?>">
                                 </div>
                                 
-                                <div class="form-row">
+                                <div class="form-row mb-4">
                                     
                                     
                                     <div class="form-group col-md-6">
                                         <label for="city">City</label>
+                                        <span class="error-msg"></span>
                                         <select id="state" name="city" class="form-control city">
                                             <option selected value="nooption">Choose city</option>
                                             <?php 
@@ -136,6 +141,7 @@
                                     </div>
                                     <div class="form-group col-md-4">
                                         <label for="state">State</label>
+                                        <span class="error-msg"></span>
                                         <select id="state" name="state" class="form-control state">
                                             <option value="nooption" selected>Choose state</option>
                                             <?php 
@@ -162,33 +168,35 @@
                             </header>
                             <div class="py-4 px-5 text-dark bg-light border mb-5">
                             <fieldset class="form-group">
-                                            <div class="row">
-                                                <div class="col-sm-3"> 
-                                                    <label class="label col-form-label col-sm-3 pt-0">Category</label>
-                                                </div>
-                                                <div class="col-sm-5">
-                                                    <select id="state" name="category" class="form-control city">
-                                                        <option value="nooption">Select category</option>
-                                                        <?php 
-                                                            $cat_query = "SELECT * FROM study_categories ORDER BY study_cat_type ASC";
-                                                            $cat_result = mysqli_query($conn, $cat_query);
+                                <div class="row">
+                                    <div class="col-sm-3"> 
+                                        <label class="label col-form-label col-sm-3 pt-0">Category</label>
+                                    </div>
+                                    <div class="col-sm-5">
+                                        <select id="state" name="category" class="form-control city">
+                                            <option value="nooption">Select category</option>
+                                            <?php 
+                                                $cat_query = "SELECT * FROM study_categories ORDER BY study_cat_type ASC";
+                                                $cat_result = mysqli_query($conn, $cat_query);
 
-                                                            while($row = mysqli_fetch_assoc($cat_result)){
-                                                        ?>
-                                                        <option value="<?php echo $row["category_id"];?>"><?php echo $row["study_cat_type"];?></option>
-                                                        <?php }?>
-                                                    </select>
-                                                </div>
-                                            </div>
+                                                while($row = mysqli_fetch_assoc($cat_result)){
+                                            ?>
+                                            <option value="<?php echo $row["category_id"];?>"><?php echo $row["study_cat_type"];?></option>
+                                            <?php }?>
+                                        </select>
+                                    </div>
+                                </div>
                             </fieldset>
                         
                                 <div class="form-row pt-3 mb-3">
                                     <div class="form-group col-md-6">
                                         <label for="teaching_exp">Teaching experience</label>
+                                        <span class="error-msg"></span>
                                         <input type="text" name="exp" class="form-control experience" id="teaching_exp" value="<?php //echo $row['teacher_experience']?>" placeholder="<?php //echo $row['teacher_experience']?> Years of experience">
                                     </div>
                                     <div class="form-group col-md-6">
                                         <label for="sub_id">Academic subjects</label>
+                                        <span class="error-msg"></span>
                                         <select id="state" name="subject" class="form-control subject">
                                             <option selected value="nooption">Choose your subject</option>
                                             <?php 
@@ -233,6 +241,7 @@
                                 </fieldset> -->
                                 <div class="form-group">
                                     <label for="about">About me</label>
+                                    <span class="error-msg"></span>
                                     <textarea name="about_me" class="form-control about" id="about" value="<?php //echo $row["teacher_about_me"];?>" placeholder="<?php //echo $row["teacher_about_me"];?>"></textarea>
                                     
                                 </div>
