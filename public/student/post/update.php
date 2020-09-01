8a97a4a03950262f5a146bda65779ffd943c28a9
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-09-02 00:07:00 +0530

    Update update.php

--- public/student/post/update.php
+++ public/student/post/update.php
@@ -26,25 +26,28 @@ session_start();
     <header class="text-primary-h-3 pb-5 text-center">
        Update post
     </header>
-    <form action="" method="post" onsubmit="return validation()">
+    <form action="" method="post" onsubmit="return studentPost()">
        
-        <div class="form-group">
+        <div class="form-group mb-5 mt-4 wrap-form">
             <label for="title">Title</label>
-            <input name="title" class="form-control title" type="text" id="title" value="<?php echo $row['post_title'];?>" placeholder="<?php echo $row['post_title'];?>">
+            <span class="error-msg"></span>
+            <input name="title" class="form-control title" type="text" id="title" placeholder="<?php //echo $row['post_title'];?>">
         </div>
         
-        <div class="form-group">
+        <div class="form-group wrap-form mb-5">
             <label for="about">Post details</label>
+            <span class="error-msg"></span>
             <textarea name="detail"  value="<?php echo $row['post_title'];?>"  class="form-control detail" rows="10" id="about" placeholder="Briefly describe"></textarea>
             
         </div>
         <div class="row">
-            <fieldset class="form-group col-sm-4">
+            <fieldset class="form-group wrap-form mb-5 col-sm-4">
                 <!-- 
                     #task fetch from database
                  -->
                 <div class="row">
                     <label class="label col-form-label col-sm-4 pt-0">Study type</label>
+                    <span class="error-msg"></span>
                     <div class="col-sm-6 row">
                         <div class="form-check col-sm-12">
                             <input class="form-check-input type" name="std" type="radio" value="1" id="single">
@@ -65,9 +68,10 @@ session_start();
                     </div>
                 </div>
             </fieldset> 
-            <fieldset class="form-group col-sm-6">
+            <fieldset class="form-group wrap-form mb-5 col-sm-6">
                 <div class="row">
                     <label class="label col-form-label col-sm-3 pt-0">Category</label>
+                    <span class="error-msg"></span>
                     <div class="col-sm-6 row">
                     <select id="state" name="cat" class="form-control category">
                         <option value="nooption">Select category</option>
