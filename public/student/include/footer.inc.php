17d066cb7a430794f8a65767d097bfda00ba9738
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-02 11:35:56 +0530

    Update footer.inc.php

--- public/student/include/footer.inc.php
+++ public/student/include/footer.inc.php
@@ -1,10 +1,4 @@
-<?php
-$social_media = [
-    "<i class='fa fa-facebook-official' aria-hidden='true'></i>" => "www.facebook.com",
-    "<i class='fa fa-instagram' aria-hidden='true'></i>" => "www.instagram.com",
-    "<i class='fa fa-twitter-square' aria-hidden='true'></i>" => "www.twitter.com"
-];
-?>
+
 
 <div class="body-footer">
     <footer class="wrap-container text-light pt-5">
