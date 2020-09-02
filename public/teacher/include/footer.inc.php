b44d9f1e042eb4c26096952c3be0f761cc12cfa7
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-02 11:36:20 +0530

    Update footer.inc.php

--- public/teacher/include/footer.inc.php
+++ public/teacher/include/footer.inc.php
@@ -1,11 +1,3 @@
-<?php
-$social_media = [
-    "<i class='fa fa-facebook-official' aria-hidden='true'></i>" => "www.facebook.com",
-    "<i class='fa fa-instagram' aria-hidden='true'></i>" => "www.instagram.com",
-    "<i class='fa fa-twitter-square' aria-hidden='true'></i>" => "www.twitter.com"
-];
-?>
-
 <div class="body-footer">
 
 
