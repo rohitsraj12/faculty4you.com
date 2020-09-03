b7b918294dc8aa7cf7e2982241edcd80b8890e11
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-09-04 02:24:07 +0530

    Create search.php

---
+++ private/required/public/components/search.php
@@ -0,0 +1,8 @@
+<div class="search-section">
+    <div class="wrap-container">
+    <form action="search_result.php" method="POST" class="search__form">
+        <input type="text" name="search" placeholder="search: subject (academic) / activity (non-academic) / city"  class="search__input" >
+        <button type="submit" name="submit-search" class="search__button" ><i class="fa fa-search"aria-hidden="true"></i>Search</button>
+    </form>
+    </div>
+</div>
