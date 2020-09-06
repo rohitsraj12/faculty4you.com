7061465a22f8baf22139fbf8fcf7b5a2bdc69c0d
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-09-06 12:47:29 +0530

    Update registration.php

--- public/student/registration.php
+++ public/student/registration.php
@@ -22,95 +22,111 @@
 
     <div class="body-wrap">
         
-    <div class="member-body-header">
-    <div id="hamberger" class="hamberger">
+    <!-- <div class="member-body-header">
+        <div id="hamberger" class="hamberger">
+            <span class="hamberger__line"></span>
+            <span class="hamberger__line"></span>
+            <span class="hamberger__line"></span>
+        </div>
+        <!-- end hamberger -->
+        <!-- <header class="wrap-container">
+            <div class="brand">
+                <a href="<?php base_url();?>index.php">
+                    <img src="<?php base_url();?>img/brand/header-logo.png" alt="">
+                </a>
+            </div> --> 
+            <!-- <nav class="header__nav">
+                    <ul>
+                        <li class="nav__list"><a href="<?php base_url();?>student/registration.php" class="nav__link">become teacher</a></li>
+                        <li class="nav__list"><a href="<?php base_url();?>teacher/registration.php" class="nav__link button-primary">trainer registration</a></li>
+                    </ul>
+                </nav> -->
+                <!-- end header nav -->
+        <!-- </header>
+        </div> -->
+
+            <div class="body-container">
+            <div class="member-body-header">
+                <div id="hamberger" class="hamberger">
                     <span class="hamberger__line"></span>
                     <span class="hamberger__line"></span>
                     <span class="hamberger__line"></span>
                 </div>
                 <!-- end hamberger -->
-            <header class="wrap-container">
-                <div class="brand">
-                    <a href="<?php base_url();?>index.php">
-                        <img src="<?php base_url();?>img/brand/header-logo.png" alt="">
-                    </a>
-                </div>
-                <!-- <nav class="header__nav">
-                        <ul>
-                            <li class="nav__list"><a href="<?php base_url();?>student/registration.php" class="nav__link">become teacher</a></li>
-                            <li class="nav__list"><a href="<?php base_url();?>teacher/registration.php" class="nav__link button-primary">trainer registration</a></li>
-                        </ul>
-                    </nav> -->
-                    <!-- end header nav -->
-            </header>
-        </div>
+                    <header class="wrap-container">
+                        <div class="brand">
+                            <a href="<?php base_url();?>index.php">
+                                <img src="<?php base_url();?>img/brand/header-logo.png" alt="">
+                            </a>
+                        </div>
+                    </header>
+            </div>
 
-        <div class="body-container">
-        <div class="section-member wrap-container">
+            <div class="section-member wrap-container">
 
-            <div class="container" id="container">
-                <div class="form-container sign-up-container">
-                <form action="include/login.student.inc.php" method="post"  onsubmit="return login()">
-                        <h1>Welcome back</h1>
-                        <div class="social-container">
-                            <!-- <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                            <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a> -->
-                        </div>
-                        <!-- <span>or use your account</span> -->
-                        <input type="text" id="user_name"  placeholder="User name / Email"  name="email" />
-                        <input type="password" id="password" placeholder="Password"  name="password"/>
-                        <a href="<?php base_url();?>student/reset_password.php">Forgot your password?</a>
-                        <button name="login-submit">Login</button>
-                    </form>
-                </div>
-                <div class="form-container sign-in-container">
-                    
-                    <form  action="include/registration.student.inc.php" method="post" onsubmit="return registration()">
-                        <h1>Create your account</h1>
-                        <div class="social-container">
-                            <!-- <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                            <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a> -->
-                        </div>
-                        <!-- <span>or use your email for registration</span> -->
-                          <span class="error-icon">
-                            <i class="fa fa-info-circle" aria-hidden="true"></i>
-                        </span>
-                        <input type="text" name="user_name" id="user_name" placeholder="user name" />
-                          <span class="error-icon">
-                            <i class="fa fa-info-circle" aria-hidden="true"></i>
-                        </span>
-                        <input name="email" type="email" id="email" placeholder="email" />
-                          <span class="error-icon">
-                            <i class="fa fa-info-circle" aria-hidden="true"></i>
-                        </span>
-                        <input name="password" type="password" id="password" placeholder="password" />
-                          <span class="error-icon">
-                            <i class="fa fa-info-circle" aria-hidden="true"></i>
-                        </span>
-                        <input name="re_password" type="password" id="re-password" placeholder="repeat password">
-                        <button  name="submit-register" >Sign Up</button>
-                    </form>
-                </div>
-                <div class="overlay-container">
-                    <div class="overlay">
-                        <div class="overlay-panel overlay-right">
-                            <h1>Hello, Student!</h1>
-                            <p class="text-light">If you are already a member</p>
-                            <button class="ghost" id="signIn">Login</button>
-                        </div>
-                        <div class="overlay-panel overlay-left">
-                            <h1>Hello, Student!</h1>
-                            <p class="text-light">If you are new to <strong>faculty for you</strong></p>
-                            <button class="ghost" id="signUp">Sign Up</button>
+                <div class="container" id="container">
+                    <div class="form-container sign-up-container">
+                    <form action="include/login.student.inc.php" method="post"  onsubmit="return login()">
+                            <h1>Welcome back</h1>
+                            <div class="social-container">
+                                <!-- <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                                <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a> -->
+                            </div>
+                            <!-- <span>or use your account</span> -->
+                            <input type="text" id="user_name"  placeholder="User name / Email"  name="email" />
+                            <input type="password" id="password" placeholder="Password"  name="password"/>
+                            <a href="<?php base_url();?>student/reset_password.php">Forgot your password?</a>
+                            <button name="login-submit">Login</button>
+                        </form>
+                    </div>
+                    <div class="form-container sign-in-container">
+                        
+                        <form  action="include/registration.student.inc.php" method="post" onsubmit="return registration()">
+                            <h1>Create your account</h1>
+                            <div class="social-container">
+                                <!-- <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                                <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a> -->
+                            </div>
+                            <!-- <span>or use your email for registration</span> -->
+                            <span class="error-icon">
+                                <i class="fa fa-info-circle" aria-hidden="true"></i>
+                            </span>
+                            <input type="text" name="user_name" id="user_name" placeholder="user name" />
+                            <span class="error-icon">
+                                <i class="fa fa-info-circle" aria-hidden="true"></i>
+                            </span>
+                            <input name="email" type="email" id="email" placeholder="email" />
+                            <span class="error-icon">
+                                <i class="fa fa-info-circle" aria-hidden="true"></i>
+                            </span>
+                            <input name="password" type="password" id="password" placeholder="password" />
+                            <span class="error-icon">
+                                <i class="fa fa-info-circle" aria-hidden="true"></i>
+                            </span>
+                            <input name="re_password" type="password" id="re-password" placeholder="repeat password">
+                            <button  name="submit-register" >Sign Up</button>
+                        </form>
+                    </div>
+                    <div class="overlay-container">
+                        <div class="overlay">
+                            <div class="overlay-panel overlay-right">
+                                <h1>Hello, Student!</h1>
+                                <p class="text-light">If you are already a member</p>
+                                <button class="ghost" id="signIn">Login</button>
+                            </div>
+                            <div class="overlay-panel overlay-left">
+                                <h1>Hello, Student!</h1>
+                                <p class="text-light">If you are new to <strong>faculty for you</strong></p>
+                                <button class="ghost" id="signUp">Sign Up</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                </div>
             </div>
         </div>
-    </div>
     
     <script src="<?php base_url();?>js/jquery-3.5.1.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
