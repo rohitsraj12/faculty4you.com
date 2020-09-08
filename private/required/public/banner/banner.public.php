8b2428457a3a75a156051de615f0448376ddcca9
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-09-08 11:00:20 +0530

    Update banner.public.php

--- private/required/public/banner/banner.public.php
+++ private/required/public/banner/banner.public.php
@@ -17,35 +17,11 @@
                 </p>
             </header>
             <div class="banner-content-body">
-                <!-- <div class="body-top">
-                    <h5 class="text-secondary-h pb-4">
-                        <strong>Faculty for you</strong>  is the best platform which helps you in reaching your destination.
-                    </h5>
-                </div>
-               
-            
-                <h5 class="text-secondary-h"> 
-                    Hurry... Register...
-                </h5>
-                <p class="text-secondary-h">
-                    it's our responsibility to shift you from ordinary to the realm of extraordinary...
-                </p> -->
-                
-                <!-- <form action="<?php base_url();?>search_result.php" method="POST" class="banner__form">
-                    <input type="text" name="search" placeholder="search: subject (academic) / activity (non-academic) / city"  class="banner-search" >
-                    <button type="submit" name="submit-search" class="banner__button" ><i class="fa fa-search"aria-hidden="true"></i></button>
-                </form>
-                <p>
-                    <span class="highlight-secondary">Current  trending courses:</span> <strong class="note-primary">IIT-JEE</strong>, <strong class="note-primary">NEET</strong>, <strong class="note-primary">Dance</strong>, <strong class="note-primary">Yoga</strong>
-                </p> -->
+              
             </div>
         </article>
     </section>
-    <!-- end banner content -->
-    <!-- <div class="banner-image">
-        <img src="<?php base_url();?>img/banner/teacher.svg" class="banner__image" alt="">
-    </div> -->
-    <!-- end banner imager -->
+   
 </div>
 <!-- end banner wrap -->
 </div>
