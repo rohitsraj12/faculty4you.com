5afdb198b34875893d79f5143fd3a723eeae6c75
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-09-08 16:14:22 +0530

    Update social_media.php

--- private/required/public/components/social_media.php
+++ private/required/public/components/social_media.php
@@ -1,8 +1,17 @@
 <?php
-$social_media = [
+// share sociual media links header section
+$social_media_share = [
     "facebook.svg" => "www.facebook.com",
     "instagram.svg" => "www.instagram.com",
     "twitter.svg" => "www.twitter.com"
 ];
+
+// follow social media links navigation section and footer section
+$social_media_follow = [
+    "facebook.svg" => "www.facebook.com",
+    "instagram.svg" => "www.instagram.com",
+    "twitter.svg" => "www.twitter.com"
+];
+
 ?>
 
