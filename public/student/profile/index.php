d7521c5f0d1e0fe5560be7423ea3512c225f5b62
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-09-09 23:29:06 +0530

    Update index.php

--- public/student/profile/index.php
+++ public/student/profile/index.php
@@ -11,7 +11,7 @@
     require("../../../private/config/config.php");
     include("../../../private/required/public/components/social_media.php");
     require("../include/header.inc.php");
-    include("../../../private/required/public/components/search.php");
+    // include("../../../private/required/public/components/search.php");
 
     $student_name = $_SESSION['user_name'];
 
