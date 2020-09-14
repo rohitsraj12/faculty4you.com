27bbdb46aea7d1218831c436f9b505e163867a87
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-14 19:17:12 +0530

    Update banner.public.php

--- private/required/public/banner/banner.public.php
+++ private/required/public/banner/banner.public.php
@@ -7,7 +7,7 @@
             <header class="banner-content__header">
                 <h2 class="text-primary-h-3 pb-4 mb-4">Hearty welcome to <strong class="extra-b f-link">'Faculty for you'</strong></h2>
                 <h5 class="text-secondary-h pb-4">
-                    we provide top <strong>trainers</strong> for <strong class="highlight-primary">academic/non-academic</strong> and <strong class="highlight-primary">online/offline</strong> activities all over <strong>India</strong>.   
+                    We provide top <strong>tutors</strong> for <strong class="highlight-primary">academic/non-academic</strong> and <strong class="highlight-primary">online/offline</strong> activities all over <strong>India</strong>.   
                 </h5>
                  <p class="text-primary-h-1 mt-5"> 
                     Hurry... Register...
@@ -24,10 +24,10 @@
                     <div class="wrap-container search-tab">
                         <ul>
                             <li class="form-tab" data-form="student-form">
-                                Search for  students
+                                Students for You
                             </li>
                             <li  class="form-tab not-active" data-form="teacher-form">
-                                Search for trainers
+                                Tutors for You
                             </li>
                         </ul>
                     </div>
@@ -36,11 +36,11 @@
                         <div class="search-form student-form">
                             <form action="<?php base_url();?>search_result.php" method="POST" class="form">
                                 <div class="row">
-                                    <div class="col-sm-7">
-                                        <input type="search" class="form-control" name="search" id="inputAddress" placeholder="search: subject (academic) / activity (non-academic)">
+                                    <div class="col-sm-8">
+                                        <input type="search" class="form-control" name="search" id="inputAddress" placeholder="Search: subject (academic) / activity (non-academic)">
                                         <!-- <small class="msg">Enter your subject / pincode / study type</small> -->
                                     </div>
-                                    <div class="col-sm-3">
+                                    <div class="col-sm-4">
                                         <select id="inputState" name="city" class="form-control">
                                             <option selected>Choose...</option>
                                             <?php 
@@ -58,8 +58,8 @@
                                         <small class="msg">Select city name</small>
 
                                     </div>
-                                    <div class="col-sm-2">
-                                        <input class="button-primary w-100 py-2" style="font-size: 1.4rem" name="submit-search" type="submit" value="search">
+                                    <div class="col-sm-12 text-center">
+                                        <input class="button-primary py-2" style="font-size: 2rem" name="submit-search" type="submit" value="Search">
                                     </div>
                                 </div>
                             </form>
@@ -67,11 +67,11 @@
                         <div class="search-form teacher-form hide">
                             <form action="<?php base_url();?>search_result.php" method="POST" class="form">
                                 <div class="row">
-                                    <div class="col-sm-7">
-                                        <input type="search" class="form-control" name="search" id="inputAddress" placeholder="search: subject (academic) / activity (non-academic)">
+                                    <div class="col-sm-8">
+                                        <input type="search" class="search__input form-control" name="search" id="inputAddress" placeholder="Search: subject (academic) / activity (non-academic)">
                                         <!-- <small class="msg">Enter your subject / pincode / study type</small> -->
                                     </div>
-                                    <div class="col-sm-3">
+                                    <div class="col-sm-4">
                                         <select id="inputState" name="city" class="form-control">
                                             <option selected>Choose...</option>
                                             <?php 
@@ -88,10 +88,10 @@
                                         <small class="msg">Select city name</small>
 
                                     </div>
-                                    <div class="col-sm-2">
-                                        <input class="button-primary w-100 py-2" style="font-size: 1.4rem" name="submit-search" type="submit" value="search">
-                                    
+                                    <div class="col-sm-4">
+                                        <input class="button-primary py-2" style="font-size: 2rem" name="submit-search" type="submit" value="Search">
                                     </div>
+                                        </div>
                             </form>
                         </div>
                     </div>
