9fdbeb40125cfe6419bbe6114fd063952edb8945
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-15 01:24:41 +0530

    Update search_engine.php

--- public/student/include/search_engine.php
+++ public/student/include/search_engine.php
@@ -1,6 +1,6 @@
 <div class="header-search">
     <form action="<?php base_url();?>student/search_result.php" method="POST" class="header-search__form">    
-        <input type="text" name="search" placeholder="search: subject (academic) / activity (non-academic)"  class="header-search__input" >
+        <input type="text" name="search" placeholder="Search: Subject (academic) / Activity (non-academic)"  class="header-search__input" >
         <button type="submit" name="submit-search" class="header-search__button" ><i class="fa fa-search"aria-hidden="true"></i></button>
     </form>
 </div>
