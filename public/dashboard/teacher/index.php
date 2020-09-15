08190a18bb5955f2fdc9f74bbc291731ba36372c
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-16 03:56:35 +0530

    Update index.php

--- public/dashboard/teacher/index.php
+++ public/dashboard/teacher/index.php
@@ -30,7 +30,7 @@ $active = "teacher";
                             <th scope="col">Id</th>
                             <th scope="col">Name</th>
                             <th scope="col">Email</th>
-                            <th scope="col">Phone</th>
+                            <th scope="col">Member expire date</th>
                             <th scope="col">Add testimonial</th>
                             <th scope="col">More Details</th>
                             </tr>
@@ -57,7 +57,22 @@ $active = "teacher";
                                 <th scope="row"><?php echo $row['teacher_id'];?></th>
                                 <td><?php echo $row['teacher_first_name'] . " " . $row['teacher_last_name'] ;?></td>
                                 <td><?php echo $row['teacher_email'];?></td>
-                                <td>+91 <?php echo $row['teacher_phone'];?></td>
+                                <td><?php 
+                                    $id = $row['teacher_id'];
+                                    $member_query = "SELECT * FROM memberships WHERE teacher_id = $id ";
+                                    $member_result = mysqli_query($conn, $member_query);
+                                    $member_row = mysqli_fetch_assoc($member_result);
+
+                                    $start = $member_row['membership_starting_date'];
+                                    $exp = $member_row['membership_expiry_date'];
+                                    $date = date("Y - m - d");
+                                    
+                                    if($exp <= $date){
+                                        echo "<a href='../add_records/add_membership.php?id=". $row['teacher_id'] . "' class='member-nonactive'>Member</a>";
+                                    } else {
+                                        echo "<span class='member-active'>Member</span>";
+                                    }
+                                ?></td>
                                 <td class="text-center"><a class="btn btn-link btn-sm" href="<?php base_url()?>dashboard/add_records/add_teacher_testimonials.php?id=<?php echo $row['teacher_id'];?>">Add testimonial</a></td>
                                 <td class="text-center"><a class="btn btn-link btn-sm" href="<?php base_url()?>dashboard/teacher/teacher_detail.php?id=<?php echo $row['teacher_id'];?>">more details</a></td>
                             </tr>
