d1b622077ede73990707422b431ce0b2d46615e0
sanghamitra-dalai <62993847+sanghamitra-dalai@users.noreply.github.com>
2020-09-16 12:24:14 +0530

    Update registration.php

--- public/student/registration.php
+++ public/student/registration.php
@@ -10,6 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration - trainee</title>
+    <link rel="icon" href="<?php base_url();?>img/brand/factulyforyou_fevicon.png" type="image/gif" sizes="32x32">
     <link
         href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
         rel="stylesheet"
