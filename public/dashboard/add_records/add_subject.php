15c3ecf2a798cfc93da22979545b8339ce7724b0
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-16 22:23:10 +0530

    Update add_subject.php

--- public/dashboard/add_records/add_subject.php
+++ public/dashboard/add_records/add_subject.php
@@ -32,19 +32,22 @@ $sub = "add subject";
             } else {
                 $subject = $_REQUEST['subject'];
                 $category = $_REQUEST['sub-cat'];
+                $std = $_REQUEST['std-cat'];
+
+                
     
-                $query = "INSERT INTO subjects (sub_name, sub_category_id)
-                          VALUES('$subject', '$category')";
+                $query = "INSERT INTO subjects (`sub_name`, `sub_cat_id`, `study_cat_id`)
+                          VALUES('$subject', '$category', '$std')";
                           
                 $result = mysqli_query($conn, $query); 
                 // test if there was a query error
                 if($result) {
 
-            ?>
+                ?>
                 <div class="alert alert-primary" role="alert">
                     <?php  echo "successfully Inserted " . $subject .".!";?>
                 </div>
-            <?php 
+                <?php 
                 }else{
                     die("Database query failed " . mysqli_connect_error($conn));
                 }
@@ -63,20 +66,38 @@ $sub = "add subject";
             </div>
             <div class="row">
 
-                <section class="section-faq col-sm-4">
+                <section class="section-faq col-sm-12 mb-5">
                     <header class="text-center border-bottom mb-5">
-                        <h5>Add subjects</h5>
+                        <h5>Add New subjects</h5>
                     </header>
-                    <form action="" method="POST" onsubmit="return subject_validation()">
-                        <div class="form-group">
+                    <form action="" method="POST" class="row" onsubmit="return subject_validation()">
+                        <div class="form-group col-sm-4">
                             <label for="q">Add subjects</label>
                             <input type="text" class="form-control subject" name="subject" id="q">
                             <small id="emailHelp" class="form-text text-muted">Add new subject name here.</small>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group  col-sm-4">
                             <label for="q">Select subject category</label>
                             <select name="sub-cat" class="form-control sub-cat">
                                 <option value="nooption">Select subject category</option>
+                                <?php 
+                                    $query = "SELECT * FROM subjects_categories ORDER BY sub_cat_name ASC";
+                                    $result = mysqli_query($conn, $query);
+                                
+                                    while($row = mysqli_fetch_assoc($result)){;
+                                ?>
+
+                                <option value="<?php echo $row['sub_cat_id'];?>"><?php echo $row['sub_cat_name'];?></option>
+
+                                <?php
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group  col-sm-4">
+                            <label for="q">Select study category</label>
+                            <select name="std-cat" class="form-control sub-cat">
+                                <option value="nooption">Select study category</option>
                                 <?php 
                                     $query = "SELECT * FROM study_categories ORDER BY study_cat_type ASC";
                                     $result = mysqli_query($conn, $query);
@@ -84,7 +105,7 @@ $sub = "add subject";
                                     while($row = mysqli_fetch_assoc($result)){;
                                 ?>
 
-                                <option value="<?php echo $row['study_cat_id'];?>"><?php echo $row['study_cat_type'];?></option>
+                                <option value="<?php echo $row['category_id'];?>"><?php echo $row['study_cat_type'];?></option>
 
                                 <?php
                                 }
@@ -92,16 +113,53 @@ $sub = "add subject";
                             </select>
                         </div> 
                         
-                        <div class="form-group text-right">
-                            <input class="btn btn-primary" type="submit" name="submit_subject" value="Submit subject">
+                        <div class="form-group text-right col-sm-2 pt-4">
+                            <input class="btn btn-primary mt-2" type="submit" name="submit_subject" value="Submit">
                         </div>
                     </form>
                 </section>
-                <!-- <section class="section-faq col-sm-5  align-self-start">
-                    
-                   
-                </section> -->
 
+                <section class="section-faq col-sm-12  align-self-start">
+                <table class="bg-light table">
+                        <thead class="thead-light">
+                            <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Subject Name</th>
+                            <th scope="col">Subject Category</th>
+                            <th scope="col">Study Category</th>
+                            <th scope="col">Edit</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                $study_query = "SELECT subjects.*, subjects_categories.* FROM subjects
+                                    LEFT JOIN subjects_categories
+                                        ON subjects_categories.sub_cat_id = subjects.sub_cat_id
+                                        ORDER BY subjects.sub_name ASC";
+                                $result = mysqli_query($conn, $study_query);
+
+                        // $query_results = mysqli_num_rows($result);
+
+                        // echo $query_results;
+
+                                while($row = mysqli_fetch_assoc($result)){
+                            ?>
+                            <tr>
+                                <th scope="row"><?php echo $row['subject_id'];?></th>
+                                <td><?php echo $row['sub_name']?></td>
+                                <td><?php echo $row['sub_cat_name'];?></td>
+                                <td><?php echo $row['study_cat_id'];?></td>
+                                <td><a href="<?php base_url()?>dashboard/edit_records/edit_subject.php?id=<?php echo $row['subject_id']?>">Edit</a><?php ?></td>
+                            </tr>
+                            <?php 
+                            }
+                            ?>
+                            <?php
+                                ?>
+                        </tbody>
+                    </table>
+                   
+                </section>
             </div>
         </div>
     </div>
