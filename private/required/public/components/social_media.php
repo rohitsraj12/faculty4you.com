a7590644963118bed0a23f9e749c31a38938d9df
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-16 22:22:46 +0530

    Update social_media.php

--- private/required/public/components/social_media.php
+++ private/required/public/components/social_media.php
@@ -3,7 +3,7 @@
 $social_media_share = [
     "facebook.svg" => "www.facebook.com",
     "instagram.svg" => "www.instagram.com",
-    "twitter.svg" => "www.twitter.com"
+    "twitter.svg" => "https://twitter.com/intent/tweet?text=Are%20you%20looking%20to%20become%20tutor.%20Register%20in%20http://facultyforyou.com"
 ];
 
 // follow social media links navigation section and footer section
