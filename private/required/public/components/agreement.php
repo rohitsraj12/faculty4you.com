3bc1c58f807664b366466b94786df209750e7c6e
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2020-09-20 21:15:23 +0530

    Update agreement.php

--- private/required/public/components/agreement.php
+++ private/required/public/components/agreement.php
@@ -3,8 +3,8 @@
 <div class="modal fade section__agreement" id="agreement" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Terms and Condition</h5>
+        <div class="modal-header px-5 pt-4">
+            <h5 class="modal-title h3" id="exampleModalLabel">Terms and Condition</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
             </button>
@@ -28,6 +28,85 @@
                     </li>
                     <li>
                         There is no registration fee for students and tutors to join as a member at <strong class="f-link">facultyforyou.com</strong>. Tutors must purchase active membership packages to view contact details of students as well as to view tutor contact details by students all over India.
+                    </li>
+                    <li>
+                    Students with free membership can see the details of the tutors and contact them through message, call or e-mail.
+                    </li>
+                    <li>
+                    Tutors with a free membership can see the post/requirements created by the students, but cannot view their contact details.
+                    </li>
+                    <li>
+                    Tutors with active membership can see the details of the students and contact them through the message, call or e-mail and the students all over India can view the details of the tutors and contact them through the message, call or e-mail based on tutor’s active membership.
+                    </li>
+                    <li>
+                    Please provide a valid mobile number and personal e-mail at the time of registration. We want our customers to interact with genuine persons with valid contact details, fake profiles can lead to termination of membership without any notice.
+                    </li>
+                    <li>
+                    Facultyforyou.com reserves the right to contact you by phone calls, SMS or e-mail. If you intend to request seeking information about our services, the <strong class="f-link">facultyforyou.com</strong> team will guide you by phone call, SMS or e-mail.
+                    </li>
+                    <li>
+                    For security reasons, we do NOT store your password. You are responsible for safeguarding the password that you use to access the Sites, Content and Services. You agree not to disclose your password to any third party. You agree to take sole responsibility for any activities or actions under your password, whether or not you have authorized such activities or actions.
+                    </li>
+                    <li>
+                    Through the use of this website, you agree to hold Facultyforyou.com harmless and its sponsors, owners, shareholders or employees against any claims.
+                    </li>
+                    <li>
+                    We reserve the right to deny students/tutors access to our site if they do not comply with our terms and conditions.
+                    </li>
+                    <li>
+                    Any profile created showing irrelevant, illegal and such content like porn is strictly prohibited.
+                    </li>
+                    <li>
+                    Facultyforyou.com should not be considered as the authority and final guide in making your decision.
+                    </li>
+                    <li>
+                    The details expressed on the website are not those of Facultyforyou.com, and any issues in them belong to the respective contributors only.
+                    </li>
+                    <li>
+                    Students/Parents acknowledge that it is his/her responsibility to verify the credentials and qualifications of Tutor while entering into tuition contract.
+                    </li>
+                    <li>
+                    Student/Parent can be asked to verify Tutor’s address and residential details before fixing the tuition or making any payments between them.
+                    </li>
+                    <li>
+                    Facultyforyou.com does not guarantee to secure a Tutor for a Student or fix-up any tuition fees on behalf of any tutor or student.
+                    </li><li>
+                    The opinions and reviews expressed on the Site belong to the users and Facultyforyou.com is not responsible in any way for the content of the opinions and reviews.
+                    </li>
+                    <li>
+                    A Tutor acknowledges that he/she is not an employee of <strong class="f-link">facultyforyou.com</strong> and accepts full responsibility for all Income Tax and other liabilities payable in relation to any fees generated from a tuition contract. We do not issue any experience certificate to our registered members.
+                    </li>
+                    <li>
+                    Members acknowledge that <strong class="f-link">facultyforyou.com</strong> is not liable for direct indirect consequential or any other form of loss or damage that may be suffered by a Member through the use of the Website including loss of date or information or any kind of financial or physical loss or damage.
+                    </li>
+                    <li>
+                    Facultyforyou.com is not responsible for any misbehavior of Tutors on Students or Students with Tutors. Both students and tutors, especially female students/tutors are advised to ensure safety before entering the tutoring contract with the other party. Facultyforyou.com is not liable for loss of any kind whatsoever.
+                    </li>
+                    <li>
+                    Facultyforyou.com reserves the right to update the terms, conditions and notices of this agreement without notice to you. It is your responsibility to review the most current version of this Agreement.
+                    </li>
+                    <li>
+                    Facultyforyou.com may, at its sole discretion, modify, delete or block any content, including content posted by a member, without notice and without liability. However, we will make reasonable efforts to notify you of the changes.
+                    </li>
+                    <li>
+                    You may not copy any content originating from Facultyforyou.com available for public access without obtaining prior written permission from Facultyforyou.com.
+                    </li>
+                    <li>
+                    Facultyforyou.com reserves the right to revise subscription charges without any prior notification.
+                    </li>
+                    <li>
+                    By registering as a Student/Tutor on our website constitutes automatic acceptance of these terms and conditions.
+                    </li>
+                    <li>
+                    If you do not agree to any of the terms and conditions of Facultyforyou.com, do not register and you will not be authorized to use Services provided by Facultyforyou.com.
+                    </li>
+                    <li>
+                    Facultyforyou.com serves as a platform to connect students/parents and tutors. If you enter into any tuition contracts or whatsoever with other members of <strong class="f-link">facultyforyou.com</strong>, such activity is solely between you and that third party only. We shall have no responsibility, liability or obligation for such activity. You hereby agree to release <strong class="f-link">facultyforyou.com</strong> from all claims arising from such activity.
+                    </li>
+                    <li>
+                    The foregoing are subject to the Republic of India and the courts in Bangalore, India shall have the exclusive jurisdiction on any dispute that may arise out of the use of this site. Please proceed only if you accept all the conditions enumerated herein above, out of your free will and consent.
+
+
                     </li>
                 </ul>
             </article>
