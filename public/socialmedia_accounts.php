cced7f6bdb4fb3e1397dbe3db587814d383ad32d
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2020-09-20 13:13:29 +0530

    Create socialmedia_accounts.php

---
+++ public/socialmedia_accounts.php
@@ -0,0 +1,28 @@
+<?php
+$page_title = "social media";
+ require_once("../private/config/db_connect.php");
+ require("../private/config/config.php");
+ include("../private/required/public/components/social_media.php");
+ include("../private/required/public/header.public.php");
+?>
+<div class="wrap-container">
+    <table>
+        <thead>
+        <tr>
+            <th></th>
+        </tr>
+        </thead>
+        <tbody>
+            <?php
+                foreach($social_media as $name => $url){
+            ?>
+                <td ><a href="<?php echo $url; ?>" target="_blank"alt="<?php echo $name;?>"></a></td>
+            <?php
+                }
+            ?>
+        </tbody>
+    </table>
+</div>
+<?php
+include("../private/required/public/footer.public.php");
+?>
