da37e3047c456db80b98ff255191c9f941a12214
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-20 19:24:49 +0530

    Create agreement.php

---
+++ private/required/public/components/agreement.php
@@ -0,0 +1,27 @@
+
+                        
+<div class="modal fade" id="agreement" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Terms and Condition</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <article>
+                <header>
+                    <h2>
+                        FACULTY FOR YOU TERMS & CONDITIONS
+                    </h2>
+                </header>
+            </article>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <a href="payment_method.php" type="button" class="btn btn-primary">Continue</a>
+        </div>
+        </div>
+    </div>
+</div>
