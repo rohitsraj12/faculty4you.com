a17f225afeeebfa9a15ec00b0cb255b66e6ea43b
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-20 20:08:48 +0530

    Update agreement.php

--- private/required/public/components/agreement.php
+++ private/required/public/components/agreement.php
@@ -1,7 +1,7 @@
 
                         
-<div class="modal fade" id="agreement" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+<div class="modal fade section__agreement" id="agreement" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
         <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">Terms and Condition</h5>
@@ -10,12 +10,23 @@
             </button>
         </div>
         <div class="modal-body">
-            <article>
-                <header>
+            <article class=" px-5">
+                <header class="my-5 text-center">
                     <h2>
                         FACULTY FOR YOU TERMS & CONDITIONS
                     </h2>
                 </header>
+                <p>
+                    Facultyforyou.com is an online networking community that connects the students and tutors through academic and non-academic activities all over India. Kindly read the terms and conditions of ‘Faculty for you’ to avoid misunderstanding or a needlessly unpleasant scenario, at any later stage. We sincerely advise that you must read the terms and conditions of faculty for you and clarify the doubts with us before registering.
+                </p>
+                <p>
+                It is to be done at the onset of the professional relationship. It is vitally important for the tutor and the student / parent.
+                </p>
+                <ul>
+                    <li>
+                        The website <strong>facultyforyou.com</strong> has been established to allow Students to connect with Tutors as well as Tutors to connect with Students on its platform.
+                    </li>
+                </ul>
             </article>
         </div>
         <div class="modal-footer">
