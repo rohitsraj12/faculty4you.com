b39eaf43bcd3af92903411c5b129bf2ea0683d1c
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-26 13:23:15 +0530

    Update index.php

--- public/teacher/index.php
+++ public/teacher/index.php
@@ -139,14 +139,16 @@
                                                                                             echo "You have " . $token_left ." token left in your membership.";
                                                                                         ?>
                                                                                         </p>
-                                                                                    <form action="<?php base_url();?>teacher/include/tokens.php" method="POST">  
+                                                                                    <form action="<?php base_url();?>teacher/include/tokens.php" method="POST">
+
+                                                                                            <input type="hidden" value="<?php echo $member_row['membership_id'];?>" name="membership_id">
                                                                                             <input type="hidden" value="<?php echo $token;?>" name="token">
                                                                                             <input type="hidden" value="<?php echo $row['student_id'];?>" name="student" >
                                                                                             <input type="hidden" value="<?php echo $row['student_first_name'] . ' ' . $row['student_last_name'];?>" name="student_name" >
                                                                                             <input type="hidden" value="<?php echo $row['student_phone'];?>" name="student_phone" >
                                                                                             <input type="hidden" value="<?php echo $row['student_email'];?>" name="student_email" >
                                                                                             <input type="hidden" value="<?php echo $row['post_id'];?>" name="post" >
-                                                                                            <button class="button-primary" data-toggle="modal" data-target="#approval" type="submit" name="token_update">Student detail</button>                                                                
+                                                                                            <button class="button-primary" type="submit" name="token_update">Student detail</button>                                                                
                                                                                     </form>
                                                                                 </div>
                                                                             </div>
