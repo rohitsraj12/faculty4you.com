a83d6aa83fe17fc5315243d16fa983b833a23b59
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-27 22:41:58 +0530

    Update load_subject.php

--- private/ajax/load_subject.php
+++ private/ajax/load_subject.php
@@ -3,15 +3,26 @@ require_once("../config/db_connect.php");
 
     if($_POST['type'] == ""){
 
-        $sql = "SELECT * FROM subjects_categories";
+        $sql = "SELECT * FROM study_categories";
         $result = mysqli_query($conn, $sql);
 
         $output = "";
         while($row = mysqli_fetch_assoc($result)){
             // echo "hi <br/>";
+            $output .= '<option value="'. $row["category_id"] . '">' . $row['study_cat_type'] . '</option>';
+        }
+    } else if($_POST['type'] == "subjectCat") {
+        $id = $_POST['id'];
+        $sql = "SELECT * FROM subjects_categories WHERE category_id = '$id' ORDER BY sub_cat_id ASC";
+        // $sql = "SELECT * FROM subjects WHERE sub_cat_id = '$id'";
+        $result = mysqli_query($conn, $sql);
+
+        $output = "";
+        while($row = mysqli_fetch_assoc($result)){
+            // echo $row['sub_cat_id'];
             $output .= '<option value="'. $row["sub_cat_id"] . '">' . $row['sub_cat_name'] . '</option>';
         }
-    } else if($_POST['type'] == "subjectName") {
+    }else if($_POST['type'] == "subject") {
         $id = $_POST['id'];
         $sql = "SELECT * FROM subjects WHERE sub_cat_id = '$id' ORDER BY subject_id ASC";
         // $sql = "SELECT * FROM cities";
@@ -19,7 +30,7 @@ require_once("../config/db_connect.php");
 
         $output = "";
         while($row = mysqli_fetch_assoc($result)){
-            echo $row['subject_name'];
+            // echo $row['subject_name'];
             $output .= '<option value="'. $row["subject_id"] . '">' . $row['sub_name'] . '</option>';
         }
     }
