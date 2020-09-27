3eecab81f23ac8c2391c13c01746283918a33caa
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2020-09-27 22:57:52 +0530

    Update student_post.php

--- public/dashboard/student/student_post.php
+++ public/dashboard/student/student_post.php
@@ -11,14 +11,45 @@ $sub = "faq_view";
 
    require("../../../private/config/db_connect.php");
    require("../../../private/config/config.php");
-   include("../dashboard/include/header.inc.php");
+   include("../include/header.inc.php");
 
+?>
+   <div class="body-container-right"> 
+       <div class="wrap-container">
+       <div class="page-header">
+           <div class="container">
+               <header class="header-text-1" class="py-3">
+                   Students Posts
+               </header>
+           </div>
+       </div>
+       <section class="section-faq">
+<?php
+   
    $post_query = "SELECT * FROM posts";
    $post_result = mysqli_query($conn, $post_query);
 
-    while($row = mysqli_fetch_assoc($post_result)){
-        echo $row['post_id'];
-        echo $row['post_title'];
-        echo $row['post_detail'];
+    while($row = mysqli_fetch_assoc($post_result)){ 
+?>
+        <article class="mb-4 border">
+            <header class="bg-light text-dark border-bottom faq__header">
+                <?php echo $row['post_id'];?> - <?php echo $row['post_title'];?>
+                <span class="toggle__btn"><i class="fa fa-angle-down" aria-hidden="true"></i>
+                </span>
+            </header>
+            <footer class="faq__footer h3 text-dark">
+                <p>
+                <?php echo $row['post_detail'];?>
+                </p>
+            </footer>
+        </article>
+
+<?php
     }
+
+?>
+</section>
+
+<?php
+    include("../include/footer.inc.php");
 ?>
