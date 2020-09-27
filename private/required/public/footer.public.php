9cf313ef00ef7b451aabdda9df5fe189608d1f36
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-27 11:30:06 +0530

    Update footer.public.php

--- private/required/public/footer.public.php
+++ private/required/public/footer.public.php
@@ -77,6 +77,7 @@
             <script src="<?php base_url();?>js/jquery-3.5.1.js"></script>
             <script src="<?php base_url();?>js/owl.carousel.js"></script>
             <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
             <script src="<?php base_url();?>js/script.js"></script>
             
             <script>
