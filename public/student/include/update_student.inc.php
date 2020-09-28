eaee68d7b823c8b73bfb51f4efe19c329e3c154d
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-28 23:36:48 +0530

    Update update_student.inc.php

--- public/student/include/update_student.inc.php
+++ public/student/include/update_student.inc.php
@@ -63,8 +63,9 @@ if(isset($_POST['update'])){
 
             $_result = mysqli_query($conn, $query);
 
-        // header('location: index.php');
-        $message = "Congratulations! You have successfully updated your profile detail.";
+            header("location: ../profile/index.php?message=success");
+
+        // $message = "Congratulations! You have successfully updated your profile detail.";
         
 
     }
