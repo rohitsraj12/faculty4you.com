675fd400b2c95a49f8888f8203724bff433977d1
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-29 19:48:33 +0530

    Update post.inc.php

--- public/student/include/post.inc.php
+++ public/student/include/post.inc.php
@@ -3,54 +3,56 @@
 <?php 
 
 
-session_start();
-//back function false
-if(!isset($_SESSION['user_name'])){
-    header('location: login.php');
-}
-require_once('../../../private/config/db_connect.php');
+// session_start();
+// //back function false
+// if(!isset($_SESSION['user_name'])){
+//     header('location: login.php');
+// }
+// require_once('../../../private/config/db_connect.php');
 
-$student_name = $_SESSION['user_name'];
-$sql = "SELECT * FROM std WHERE student_user_name = '$student_name'";
+// $student_name = $_SESSION['user_name'];
+// $sql = "SELECT * FROM std WHERE student_user_name = '$student_name'";
 
-$result = mysqli_query($conn, $sql);
-$row = mysqli_fetch_assoc($result);
+// $result = mysqli_query($conn, $sql);
+// $row = mysqli_fetch_assoc($result);
 
-if(isset($_POST['submit-post'])){
+// if(isset($_POST['submit-post'])){
 
-    // fill empty data
-    $student_id = $row['student_id'];
-    $post_title = $_POST['post_title'];
-    $post_detail = $_POST['post_detail'];
-    $study_type = $_POST['study_type'];
-    $study_category = $_POST['category'];
-    $subject = $_POST['subject'];
-    $city = $row['city_id'];
-    $state = $row['state_id'];
-    $post_date = date("Y/m/d");
-    $post_state = 1;
+//     // fill empty data
+//     $student_id = $row['student_id'];
+//     $post_title = $_POST['post_title'];
+//     $post_detail = $_POST['post_detail'];
+//     $study_type = $_POST['study_type'];
+//     $study_category = $_POST['study_category'];
+//     $subject_category = $_POST['subject_category'];
+//     $subject = $_POST['subject'];
+//     $city = $row['city_id'];
+//     $state = $row['state_id'];
+//     $post_date = date("Y/m/d");
+//     $post_state = 1;
+//     $block_post = "";
 
-    // echo $student_id . " - " .$post_title;
-    // if empty field condition
+//     // echo $student_id . " - " .$post_title;
+//     // if empty field condition
     
-    // // echo $post_title;
-    $sql = "INSERT INTO `posts` (`student_id`, `post_title`, `post_detail`, `study_type_id`, `category_id`, `subject_id`, `city_id`, `state_id`, `post_date`, `post_state`) 
-     VALUES ('$student_id', '$post_title', '$post_detail', '$study_type', '$study_category', '$subject', '$city', '$state', '$post_date', '$post_state')";
+//     // // echo $post_title;
+//     $sql = "INSERT INTO `posts` (`student_id`, `post_title`, `post_detail`, `study_type_id`, `study_cat_id`, `category_id`, `subject_id`, `city_id`, `state_id`, `post_date`, `post_state`, `block_date`) 
+//      VALUES ('$student_id', '$post_title', '$post_detail', '$study_type', '$study_category', '$subject_category', '$subject', '$city', '$state', '$post_date', '$post_state', '$block_post')";
 
-    $result = mysqli_query($conn, $sql);
+//     $result = mysqli_query($conn, $sql);
 
-    header("Location: ../post/index.php?register=success");
+//     header("Location: ../post/index.php?register=success");
 
 
-//         // exit();
+// //         // exit();
     
-//         if(!$result){
-//             die("failed query". mysqli_error($conn));
-//         }
+// //         if(!$result){
+// //             die("failed query". mysqli_error($conn));
+// //         }
 
-} else {
-     // redirect to register and empty field
-     header("location: ../post/post.php");
-     //stop scripting
-     exit();
-}
+// } else {
+//      // redirect to register and empty field
+//      header("location: ../post/post.php");
+//      //stop scripting
+//      exit();
+// }
