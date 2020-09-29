1f0d84a836bfba65418f1426bc0d78fc9c49e35e
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-29 05:41:53 +0530

    Update student_post.php

--- public/dashboard/student/student_post.php
+++ public/dashboard/student/student_post.php
@@ -16,39 +16,39 @@ $sub = "faq_view";
 ?>
    <div class="body-container-right"> 
        <div class="wrap-container">
-       <div class="page-header">
-           <div class="container">
-               <header class="header-text-1" class="py-3">
-                   Students Posts
-               </header>
-           </div>
-       </div>
-       <section class="section-faq">
-<?php
-   
-   $post_query = "SELECT * FROM posts";
-   $post_result = mysqli_query($conn, $post_query);
+            <div class="page-header">
+                <div class="container">
+                    <header class="header-text-1" class="py-3">
+                        Students Posts
+                    </header>
+                </div>
+            </div>
+            <section class="section-faq">
+                    <?php
+                    
+                    $post_query = "SELECT * FROM posts";
+                    $post_result = mysqli_query($conn, $post_query);
 
-    while($row = mysqli_fetch_assoc($post_result)){ 
-?>
-        <article class="mb-4 border">
-            <header class="bg-light text-dark border-bottom faq__header">
-                <?php echo $row['post_id'];?> - <?php echo $row['post_title'];?>
-                <span class="toggle__btn"><i class="fa fa-angle-down" aria-hidden="true"></i>
-                </span>
-            </header>
-            <footer class="faq__footer h3 text-dark">
-                <p>
-                <?php echo $row['post_detail'];?>
-                </p>
-            </footer>
-        </article>
+                        while($row = mysqli_fetch_assoc($post_result)){ 
+                    ?>
+                            <article class="mb-4 border">
+                                <header class="bg-light text-dark border-bottom faq__header">
+                                    <?php echo $row['post_id'];?> - <?php echo $row['post_title'];?>
+                                    <span class="toggle__btn"><i class="fa fa-angle-down" aria-hidden="true"></i>
+                                    </span>
+                                </header>
+                                <footer class="faq__footer h3 text-dark">
+                                    <p>
+                                    <?php echo $row['post_detail'];?>
+                                    </p>
+                                </footer>
+                            </article>
 
-<?php
-    }
+                    <?php
+                        }
 
-?>
-</section>
+                    ?>
+                </section>
 
 <?php
     include("../include/footer.inc.php");
