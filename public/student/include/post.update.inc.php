be11e4049d95deda6a927a0b22edf867974b3f0e
rohitsraj12 <rohitsraj12@gmail.com>
2020-09-29 19:48:37 +0530

    Update post.update.inc.php

--- public/student/include/post.update.inc.php
+++ public/student/include/post.update.inc.php
@@ -4,13 +4,17 @@
         $id = $_GET['id'];
         $title = $_POST['title'];
         $detail = $_POST['detail'];
-        $stype = $_POST['std'];
-        $cat =  $_POST['cat'];
+        $study_type = $_POST['study_type'];
+        $study_category = $_POST['study_category'];
+        $subject_category = $_POST['subject_category'];
+        $subject = $_POST['subject'];
      
         $query = "UPDATE posts SET post_title = '$title',
         post_detail = '$detail',
-        study_type_id = $stype,
-        category_id = $cat WHERE post_id = $id"; 
+        study_type_id = '$study_type',
+        study_cat_id = '$study_category',
+        category_id = '$subject_category',
+        subject_id = $subject WHERE post_id = $id"; 
         $result = mysqli_query($conn, $query);
 
         
