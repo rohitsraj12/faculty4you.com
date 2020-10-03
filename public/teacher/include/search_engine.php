e759d07be33fe55d3365ff0d2b762f2a6adfb621
rohitsraj12 <rohitsraj12@gmail.com>
2020-10-04 00:31:03 +0530

    Create search_engine.php

---
+++ public/teacher/include/search_engine.php
@@ -0,0 +1,7 @@
+<div class="header-search">
+    <form action="<?php base_url();?>teacher/student_detail.php" method="POST" class="header-search__form form-field">    
+        <input type="text" name="user_name_search" placeholder="Search: Student user name"  class="header-search__input search_subject" >
+        <!-- <div class="subject-list" id="subjectList"></div> -->
+        <button type="submit" name="search_student_profile" class="header-search__button" ><i class="fa fa-search"aria-hidden="true"></i></button>
+    </form>
+</div>
