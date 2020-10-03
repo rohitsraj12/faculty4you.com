bf99fc818ce8b4815e4ada62450478c667b2a81b
rohitsraj12 <rohitsraj12@gmail.com>
2020-10-03 11:15:22 +0530

    Update footer.public.php

--- private/required/public/footer.public.php
+++ private/required/public/footer.public.php
@@ -81,7 +81,63 @@
             <script src="<?php base_url();?>js/script.js"></script>
             
             <script>
-                AOS.init();
+                // AOS.init();
+
+                
+$(document).ready(function(){
+ // ajax search subjects
+    $(".search_subject").keyup(function () {
+        var query = $(this).val();
+        // alert(query);
+        if (query != "") {
+        $.ajax({
+            url: "aj/load_data.php",
+            method: "POST",
+            data: { subject: query },
+            success: function (data) {
+            $(".subject-list").slideDown();
+            $(".subject-list").html(data);
+            },
+        });
+        } else {
+        $(".subject-list").slideUp();
+        $(".subject-list").html();
+        }
+    });
+    $(".subject-list").on('click', 'li', function(){
+        // alert('hi');
+        $('.search_subject').val($(this).text());
+        $('.subject-list').slideUp();
+       
+    })
+
+    
+ // ajax search subjects
+    $(".city_name").keyup(function () {
+        var query = $(this).val();
+        // alert(query);
+        if (query != "") {
+        $.ajax({
+            url: "aj/load_data.php",
+            method: "POST",
+            data: { city: query },
+            success: function (data) {
+            $(".city_list").slideDown();
+            $(".city_list").html(data);
+            },
+        });
+        } else {
+        $(".city_list").slideUp();
+        $(".city_list").html();
+        }
+    });
+    $(".city_list").on('click', 'li', function(){
+        // alert('hi');
+        $(this).closest('.city_name').val($(this).text());
+        $('.city_list').slideUp();
+       
+    })
+})
               </script>
     </body>
 </html>
