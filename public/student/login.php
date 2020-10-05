67d544a45b786fc815f78e16361f195ece504104
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2020-10-05 22:46:19 +0530

    Update login.php

--- public/student/login.php
+++ public/student/login.php
@@ -9,7 +9,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log in - trainee</title>
+    <title>Log in - student</title>
     <link rel="icon" href="<?php base_url();?>img/brand/factulyforyou_fevicon.png" type="image/gif" sizes="32x32">
     <link
         href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
