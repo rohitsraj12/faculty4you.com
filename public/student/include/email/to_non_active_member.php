097e3e3700dd515b6cf1391315d0e7d2d45fbb70
rohitsraj12 <rohitsraj12@gmail.com>
2020-10-07 04:13:13 +0530

    Update to_non_active_member.php

--- public/student/include/email/to_non_active_member.php
+++ public/student/include/email/to_non_active_member.php
@@ -42,7 +42,7 @@
     $subject = "New student has viewd your profile | facultyforyou.com";
     $message = "<p>Dear,". $teacher_name ."</p></br>";
     $message .= "<p>Welcome to facultyforyou.com. A student of the following details has viewed your profile. Respond now before someone else is hired. </p></br>";
-    // $message .= "<h4>Student details</h4>";
+    // $message .= "<p>If you are intrested to see student post details, then please login facultyforyou.com and seach student user name in search field.</p>";
     $message .= "<table> <tr><th><h4>Student details</h4></th> <th></th></tr>";
     $message .= "<tr><td>Student user name: </td><td><b>" . $student_user_name . "</b></td></tr>";
     $message .= "<tr><td>Student Name: </td><td>" . $student_name . "</td></tr>";
