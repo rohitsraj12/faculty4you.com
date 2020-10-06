843fa5ac784415aa03ef5d031bfaf1e506ace7b2
rohitsraj12 <rohitsraj12@gmail.com>
2020-10-07 03:45:09 +0530

    Update student_search.php

--- private/required/public/student_search.php
+++ private/required/public/student_search.php
@@ -15,33 +15,39 @@
         </h1>
     </header>
     <div class="post-body row">  
-        <div class="col-sm-2">
-            <figure class="pt-4">
-                <?php
-                        if($row['teacher_photo'] == ""){
-                ?>
-                        <img class="img-fluid img-rounded" style="max-height: 200px" src="<?php base_url();?>img/teacher/profile_pic/male_profile.svg" alt="">
-                <?php
-                    } else {
-                ?>
-                        <img class="img-fluid img-rounded" style="max-height: 150px" src="<?php echo base_url() . $row['teacher_photo'];?>" alt="">
-                <?php
-                    }
-                ?>
-            </figure>
-        </div>
-        <div class="col-sm-9">
+       
+        <div class="col-sm-12">
             <ul class="d-flex flex-row flex-wrap bd-highlight py-4 h4 font-weight-normal text-secondary">
                 <li class="mr-5"><i class="fa fa-book mr-2" aria-hidden="true"></i><?php echo $row["sub_name"];?> trainer</li>
                 <li class="mr-5"><i class="fa fa-paw mr-2" aria-hidden="true"></i><?php echo $row["teacher_experience"] . ' years of experience';?></li>
                 <li class="mr-5"><i class="fa fa-university mr-2" aria-hidden="true"></i><?php echo $row["study_cat_type"];?></li>
                 <li class="mr-5"><i class="fa fa-map-marker mr-2" aria-hidden="true"></i><?php echo $row["city_name"];?></li>
             </ul>
+        </div>
+        <div class="row px-4">
+            <div class="col-sm-4">
+                <figure class="pt-2">
+                    <?php
+                            if($row['teacher_photo'] == ""){
+                    ?>
+                            <img class="img-fluid img-rounded" style="max-height: 400px" src="<?php base_url();?>img/teacher/profile_pic/male_profile.svg" alt="">
+                    <?php
+                        } else {
+                    ?>
+                            <img class="img-fluid img-rounded" style="max-height: 400px" src="<?php echo base_url() . $row['teacher_photo'];?>" alt="">
+                    <?php
+                        }
+                    ?>
+                </figure>
+            </div>
+            <div class="col-sm-8">
+                
             <p>
             <?php
                 echo $row['teacher_about_me'];
             ?>
             </p>
+            </div>
         </div>
     </div>
     <footer class="post-footer">
