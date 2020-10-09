150501e039ca62bd351ad3a87c11001f259abd72
rohitsraj12 <rohitsraj12@gmail.com>
2020-10-09 16:51:46 +0530

    Update compose_post.inc.php

--- public/student/include/compose_post.inc.php
+++ public/student/include/compose_post.inc.php
@@ -28,14 +28,15 @@ if(isset($_POST['submit-post'])){
     $state = $row['state_id'];
     $post_date = date("Y/m/d");
     $post_state = 1;
+    $block_date = date("Y-m-d h:i:s");
     // $block_post = "";
 
     // echo $student_id . " - " .$post_title;
     // if empty field condition
     
     // // echo $post_title;
-    $sql = "INSERT INTO `posts` (`student_id`, `post_title`, `post_detail`, `study_type_id`, `study_cat_id`, `category_id`, `subject_id`, `city_id`, `state_id`, `post_date`, `post_state`) 
-     VALUES ('$student_id', '$post_title', '$post_detail', '$study_type', '$study_category', '$subject_category', '$subject', '$city', '$state', '$post_date', '$post_state')";
+    $sql = "INSERT INTO `posts` (`student_id`, `post_title`, `post_detail`, `study_type_id`, `study_cat_id`, `category_id`, `subject_id`, `city_id`, `state_id`, `post_date`, `post_state`, `block_date`) 
+     VALUES ('$student_id', '$post_title', '$post_detail', '$study_type', '$study_category', '$subject_category', '$subject', '$city', '$state', '$post_date', '$post_state', '$block_date')";
 
     $result = mysqli_query($conn, $sql);
 
