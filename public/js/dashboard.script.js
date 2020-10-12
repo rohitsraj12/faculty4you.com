4d13a4e4d3a9bc11ba2cb16d3b3b6aea91767b2f
rohitsraj12 <rohitsraj12@gmail.com>
2020-10-12 08:18:47 +0530

    Update dashboard.script.js

--- public/js/dashboard.script.js
+++ public/js/dashboard.script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  var baseURL = "http://localhost/Projects/faculty4you.com/public/";
+  
   $(".side-nav-toggle").click(function (e) {
     e.preventDefault();
     $(this).parent().find(".side-sub-nav").slideToggle();
@@ -29,6 +31,50 @@ $(document).ready(function () {
     $("." + att).fadeIn(500);
     
   });
+  
+
+  //posts update
+  $(document).on('click', '.activete_post', function(){
+    // alert("clicked");
+    var id = $(this).data('id');
+    var element = this;
+      $.ajax({
+        url: baseURL + "dashboard/ajax/posts.php",
+        method: "POST",
+        data: { id: id},
+        success: function (data) {
+          // $(".subject-list").slideDown();
+          // $(".subject-list").html(data);
+          if(data == 1){
+          $(element).closest('article').fadeOut();
+          alert("post has moved to active section");
+        } else {
+          alert("failed");
+        }
+        },
+      });
+  })
+
+  $(document).on('click', '.deactivete_post', function(){
+    // alert("clicked");
+    var id = $(this).data('id');
+    var element = this;
+      $.ajax({
+        url: baseURL + "dashboard/ajax/post_deactivate.php",
+        method: "POST",
+        data: { id: id},
+        success: function (data) {
+          // $(".subject-list").slideDown();
+          // $(".subject-list").html(data);
+          if(data == 1){
+          $(element).closest('article').fadeOut();
+          alert("post has moved to deactive section");
+        } else {
+          alert("failed");
+        }
+        },
+      });
+  })
 
 });
 
