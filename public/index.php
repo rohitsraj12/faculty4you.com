1a78b0e238e2fb50285f2b656cc41355357032bf
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2020-10-13 21:42:33 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -11,7 +11,6 @@
 
             <div class="body-container">
                 <main class="page-home">
-
                     <section class="section-student">
                         <div class="section-header u-center-text">
                             <heeader class="text-primary-h-3"> 
@@ -20,8 +19,7 @@
                             <div class="sub-heading wrap-container">
                                 <h6 class="text-sub-primary">
                                     Register <strong class="f-link">facultyforyou.com</strong> and connect with top <strong>academic</strong> / <strong>non-academic tutors</strong> on the platform.
-                                </h6>
-                            
+                                </h6>                            
                         </div>
                         <section class="section-body wrap-container">
                             <div class="section-body-header">
@@ -36,7 +34,6 @@
 
                                 </figure>
                                 <header class="article-header student-section__header">
-                                    <!-- <h4 class="text-secondary-h">Register</h4> -->
                                 </header>
                                 <div class="article-body student-body">
                                     <p>
@@ -51,7 +48,6 @@
 
                                 </figure>
                                 <header class="article-header student-section__header">
-                                    <!-- <h4 class="text-secondary-h">Post your requirements</h4> -->
                                 </header>
                                 <div class="article-body student-body">
                                     <p>
@@ -66,7 +62,6 @@
 
                                 </figure>
                                 <header class="article-header student-section__header">
-                                    <!-- <h4 class="text-secondary-h">Schedule lesson</h4> -->
                                 </header>
                                 <div class="article-body student-body">
                                     <p>
@@ -79,10 +74,7 @@
                             <a href="<?php echo base_url();?>student/registration.php" class="button-primary">Registration </a>
                         </footer>
                     </section>
-                   
-
-                    <section class="section-teacher">
-                        
+                    <section class="section-teacher">    
                         <div class="section-header u-center-text"  >
                             <heeader class="text-primary-h-3"> 
                                 How <strong class="highlight-primary">Faculty for you</strong> works for <strong class="highlight-primary">Tutors</strong>
@@ -91,10 +83,8 @@
                                 <h6 class="text-sub-primary">
                                     Join <strong class="f-link">facultyforyou.com</strong> and connect with number of <strong>students</strong> on the platform.
                                 </h6>
-                            </div>
-                            
-                        </div>
-                        
+                            </div>                            
+                        </div>                        
                         <section class="section-body wrap-container">
                             <div class="section-body-header">
                                 <p class="note-1">
@@ -107,7 +97,6 @@
                                     <img src="<?php base_url();?>img/member/t1.png" alt="">
                                 </figure>
                                 <header class="article-header student-section__header">
-                                    <!-- <h4 class="text-secondary-h">Register</h4> -->
                                 </header>
                                 <div class="article-body student-body">
                                     <p>
@@ -121,7 +110,6 @@
                                     <img src="<?php base_url();?>img/member/t2.png" alt="">
                                 </figure>
                                 <header class="article-header student-section__header">
-                                    <!-- <h4 class="text-secondary-h">Activate membership</h4> -->
                                 </header>
                                 <div class="article-body student-body">
                                     <p>
@@ -135,7 +123,6 @@
                                     <img src="<?php base_url();?>img/member/t3.png" alt="">
                                 </figure>
                                 <header class="article-header student-section__header">
-                                    <!-- <h4 class="text-secondary-h">Find students</h4> -->
                                 </header>
                                 <div class="article-body student-body">
                                     <p>
@@ -149,7 +136,6 @@
                                     <img src="<?php base_url();?>img/member/t4.png" alt="">
                                 </figure>
                                 <header class="article-header student-section__header">
-                                    <!-- <h4 class="text-secondary-h">Schedule lesson</h4> -->
                                 </header>
                                 <div class="article-body student-body">
                                     <p>
@@ -162,12 +148,10 @@
                             <a href="<?php echo base_url();?>teacher/registration.php" class="button-primary">Registration</a>
                         </footer>
                     </section>
-                   
-
                     <section class="section-data py-5 text-light">
-                    <header class="text-center py-5">
-                        The <strong>Students</strong> and <strong>Tutors</strong> registered in our Platform all over  India..
-                    </header>
+                        <header class="text-center py-5">
+                            The <strong>Students</strong> and <strong>Tutors</strong> registered in our Platform all over  India..
+                        </header>
                         <div class="wrap-container row text-center py-5">
                             <article class="col-sm-4 py-5"  >
                                 <p class="text-white pb-3" style = "font-size : 2rem">Registered Students</p>
@@ -207,82 +191,73 @@
                             </article>
                         </div>
                     </section>
-
                     <section class="section-testimonial top-testimonial">
-                         
-                         <div class="section-header u-center-text" >
+                        <div class="section-header u-center-text" >
                              <heeader class="text-primary-h-3"> 
                                  Tutor community feed back
                              </header>
-                         </div>
-                         <blockquote class="section-body wrap-container owl-carousel owl-theme">
-                         <?php
-                             $testimonial_query = "SELECT teacher_testimonials.*, teachers.* FROM teacher_testimonials 
-                             LEFT JOIN teachers
-                                 ON teachers.teacher_id = teacher_testimonials.teacher_id LIMIT 4";
-                             $testimonial_result = mysqli_query($conn, $testimonial_query);
- 
-                             while($row = mysqli_fetch_assoc($testimonial_result)){
-                                
-                         ?>
-                         <article class="article-block" >
-                             <div class="testimonial-client-detail">
-                                 <p>
-                                     "
-                                     <?php echo $row['testimonial_quote'];?>
-                                     "
-                                 </p>
-                             </div>        
-                             <footer class="article-footer">
-                                 <figure>
-                                     <?php 
- 
-                                         if($row['teacher_photo'] == ""){
-                                     ?>
-                                             <img class="member__img" style="max-height: 200px; border-radius= 50%;" src="<?php echo base_url()?>img/teacher/profile_pic/male_profile.svg" alt="">
-                                     <?php
-                                         } else {
-                                     ?>
-                                             <img class="member__img" style="max-height: 200px; border-radius= 50%;" src="<?php echo base_url() . $row['teacher_photo'];?>" alt="">
-                                     <?php
-                                         }
-                                     ?>
-                                 </figure>
-                                 <ul>
-                                     <li class="testimonial-client-name"><cite><?php echo $row['teacher_first_name'] . " " . $row['teacher_last_name'];?></cite></li>
-                                     <li class="testimonial-client-place">
-                                         <?php
-                                     $city = $row['city_id'];
-                                     $state = $row['state_id'];
-                                 if($city == true){
-                                     $sql = "SELECT cities.*, states.* FROM cities 
-                                     LEFT JOIN states
-                                 ON states.state_id = cities.state_id
-                                      WHERE city_id = '$city'";
-                                     $result = mysqli_query($conn, $sql);
-                                     $rows = mysqli_fetch_assoc($result);
-                                     echo $rows['city_name'] . ' - ' . $rows['state_name'];;
- 
-                                 };  
-                                 ?>  
- 
-                                     </li>
-                                 </ul>
-                             </footer>
-                         </article>
-                         <?php
-                         }
-                         ?>
-                         
-                         </blockquote>
-                         <!-- <div class="svg">
-                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                                 <path fill="#0099ff" fill-opacity="1" d="M0,64L48,74.7C96,85,192,107,288,106.7C384,107,480,85,576,96C672,107,768,149,864,186.7C960,224,1056,256,1152,229.3C1248,203,1344,117,1392,74.7L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-                             </svg>
-                         </div> -->
+                        </div>
+                        <blockquote class="section-body wrap-container owl-carousel owl-theme">
+                            <?php
+                                $testimonial_query = "SELECT teacher_testimonials.*, teachers.* FROM teacher_testimonials 
+                                LEFT JOIN teachers
+                                    ON teachers.teacher_id = teacher_testimonials.teacher_id LIMIT 4";
+                                $testimonial_result = mysqli_query($conn, $testimonial_query);
+    
+                                while($row = mysqli_fetch_assoc($testimonial_result)){
+                                    
+                            ?>
+                            <article class="article-block" >
+                                <div class="testimonial-client-detail">
+                                    <p>
+                                        "
+                                        <?php echo $row['testimonial_quote'];?>
+                                        "
+                                    </p>
+                                </div>        
+                                <footer class="article-footer">
+                                    <figure>
+                                        <?php 
+    
+                                            if($row['teacher_photo'] == ""){
+                                        ?>
+                                                <img class="member__img" style="max-height: 200px; border-radius= 50%;" src="<?php echo base_url()?>img/teacher/profile_pic/male_profile.svg" alt="">
+                                        <?php
+                                            } else {
+                                        ?>
+                                                <img class="member__img" style="max-height: 200px; border-radius= 50%;" src="<?php echo base_url() . $row['teacher_photo'];?>" alt="">
+                                        <?php
+                                            }
+                                        ?>
+                                    </figure>
+                                    <ul>
+                                        <li class="testimonial-client-name"><cite><?php echo $row['teacher_first_name'] . " " . $row['teacher_last_name'];?></cite></li>
+                                        <li class="testimonial-client-place">
+                                            <?php
+                                        $city = $row['city_id'];
+                                        $state = $row['state_id'];
+                                    if($city == true){
+                                        $sql = "SELECT cities.*, states.* FROM cities 
+                                        LEFT JOIN states
+                                    ON states.state_id = cities.state_id
+                                        WHERE city_id = '$city'";
+                                        $result = mysqli_query($conn, $sql);
+                                        $rows = mysqli_fetch_assoc($result);
+                                        echo $rows['city_name'] . ' - ' . $rows['state_name'];;
+    
+                                    };  
+                                    ?>  
+    
+                                        </li>
+                                    </ul>
+                                </footer>
+                            </article>
+                            <?php
+                            }
+                            ?>
+                        </blockquote>
                     </section>
                     <section class="section-testimonial">
-                         
                         <div class="section-header u-center-text" >
                             <heeader class="text-primary-h-3"> 
                                 Student community feed back
@@ -335,10 +310,8 @@
                                     $result = mysqli_query($conn, $sql);
                                     $rows = mysqli_fetch_assoc($result);
                                     echo $rows['city_name'] . ' - ' . $rows['state_name'];;
-
                                 };  
                                 ?>  
-
                                     </li>
                                 </ul>
                             </footer>
@@ -348,61 +321,13 @@
                         ?>
                         
                         </blockquote>
-                        <!-- <div class="svg">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                                <path fill="#0099ff" fill-opacity="1" d="M0,64L48,74.7C96,85,192,107,288,106.7C384,107,480,85,576,96C672,107,768,149,864,186.7C960,224,1056,256,1152,229.3C1248,203,1344,117,1392,74.7L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-                            </svg>
-                        </div> -->
                     </section>
  
-
-                    <!-- <section class="section-partner">
-                        
-                        <div class="section-header u-center-text" >
-                            <heeader class="text-primary-h-3"> 
-                                Our partners
-                            </header>
-                        </div>
-                        
-                        <section class="section-body wrap-container">
-                            <article class="article-block"> 
-                                <figure class="article-block__figure">
-                                    
-                                    <img src="<?php base_url();?>img/member/register.svg" alt="">
-
-                                </figure>
-                               
-                            </article>
-                            <article class="article-block" >
-                                <figure class="article-block__figure">
-                                    
-                                    <img src="<?php base_url();?>img/member/post-requirement.svg" alt="">
-
-                                </figure>
-                                
-                            </article>
-                            <article class="article-block">
-                                <figure class="article-block__figure">
-                              
-                                    <img src="<?php base_url();?>img/member/schedule.svg" alt="">
-
-                                </figure>
-                               
-                            </article>
-                        </section>
-                    </section> -->
                 </main>
                 <!-- end page home -->
             </div>
             <!-- end body container -->
-
-
 <?php
     include("../private/required/public/components/agreement.php");
-
     include("../private/required/public/footer.public.php");
-
 ?>
-
-
-
