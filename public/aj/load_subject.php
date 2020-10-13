ee667bd95744cc30395da99b7b304ddc6d504a17
rohitsraj12 <rohitsraj12@gmail.com>
2020-10-13 16:25:45 +0530

    Update load_subject.php

--- public/aj/load_subject.php
+++ public/aj/load_subject.php
@@ -1,15 +1,5 @@
 <?php
     require_once("../../private/config/db_connect.php");
-    // $db_host = "localhost";
-    // $db_user = "root";
-    // $db_pass = "";
-    // $db_name = "faculty_for_you";
-
-    // $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
-    // if(mysqli_connect_errno()){
-    //     die("database connection failed: ");
-    // }
 
     if($_POST['type'] == ""){
 
