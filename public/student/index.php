f549b690bb94f2449a2f4937813e31146469a590
rohitsraj12 <rohitsraj12@gmail.com>
2020-10-20 17:35:56 +0530

    Update index.php

--- public/student/index.php
+++ public/student/index.php
@@ -192,29 +192,28 @@
 
 
     <?php 
-        if(empty($student_first_name) && ($page_title == "Home page")){                                                
-            ?>
-                <div class="modal" id="myModal">
-                    <div class="modal-dialog modal-dialog-centered">
-                        <div class="modal-content">
-                        <div class="modal-header post-header">
-                            <h5 class="modal-title text-light h2">Welcome to facultyforyou.com</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span class="h1 text-white" aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="text-left py-5 post-body">
-                            <p>Create your profile and post your requirement/s to see the details of Tutors on your requirement</p>
-                        </div>
-                        <div class="modal-footer">
-                            <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
-                            <a href="<?php base_url();?>student/profile/profile_update.php?id=<?php echo $row['student_id'];?>" type="button" class="button-primary">create profile</a>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-            <?php
-        }
+        // if(empty($student_first_name) && ($page_title == "Home page")){                                                
+        //     ?>
+        //         <!-- <div class="modal" id="myModal">
+        //             <div class="modal-dialog modal-dialog-centered">
+        //                 <div class="modal-content">
+        //                 <div class="modal-header post-header">
+        //                     <h5 class="modal-title text-light h2">Welcome to facultyforyou.com</h5>
+        //                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        //                     <span class="h1 text-white" aria-hidden="true">&times;</span>
+        //                     </button>
+        //                 </div>
+        //                 <div class="text-left py-5 post-body">
+        //                     <p>Create your profile and post your requirement/s to see the details of Tutors on your requirement</p>
+        //                 </div>
+        //                 <div class="modal-footer">
+        //                     <a href="<?php base_url();?>student/profile/profile_update.php?id=<?php echo $row['student_id'];?>" type="button" class="button-primary">create profile</a>
+        //                 </div>
+        //                 </div>
+        //             </div>
+        //         </div> -->
+        //     <?php
+        // }
     ?>
 
             <?php 
