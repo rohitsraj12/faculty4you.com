1ed3adcad693851f6f083ae5a8c387d6c1783f6e
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2020-10-29 17:13:49 +0530

    Update footer.public.php

--- private/required/public/footer.public.php
+++ private/required/public/footer.public.php
@@ -25,6 +25,7 @@
                                 <ul class="col-sm-4">
                                     <li class="pb-2"><a class="text-light h4" href="<?php echo base_url();?>index.php"> home</a></li>
                                     <li class="pb-2"><a class="text-light h4" href="<?php echo base_url();?>testimonials.php"> testimonials</a></li>
+                                    <li class="pb-2"><a class="text-light h4" href="<?php echo base_url();?>contact_us.php"> contact us</a></li>
                                     <li class="pb-2"><a class="text-light h4" href="<?php echo base_url();?>faq.php"> faq</a></li>
                                     <li class="pb-2"><a class="text-light h4" href="<?php echo base_url();?>site_map.php"> site map</a></li>
                                 </ul>
