5b5bf6cfd6c56780be41823759781b7c04087717
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2020-11-01 22:03:23 +0530

    Update active_member.php

--- public/dashboard/teacher/active_member.php
+++ public/dashboard/teacher/active_member.php
@@ -13,7 +13,7 @@
     require("../../../private/config/db_connect.php");
     include("../include/header.inc.php");
 
-    if(isset($_POST['email'])){
+    if(isset($_POST['reminder_email'])){
         $email = $_POST['email'];
         $teacher_name = $_POST['name'];
         $admin_email = "admin@facultyforyou.com";
@@ -106,7 +106,7 @@
                                                 <input type="hidden" name="name" value="<?php echo $row['teacher_first_name'] . " " . $row['teacher_last_name'] ; ?>">
                                                 <input type="hidden" name="id" value="<?php echo $row['teacher_id']; ?>">
                                                 <input type="hidden" name="email" value="<?php echo $row['teacher_email']; ?>">
-                                                <button class="member-nonactive" name="email">reminder email</button>
+                                                <button class="member-nonactive" name="reminder_email">reminder email</button>
                                             </form>
                                         </td>
                                         <td class="text-center"><a class="btn btn-link btn-sm" href="<?php base_url()?>dashboard/teacher/teacher_detail.php?id=<?php echo $row['teacher_id'];?>">more details</a></td>
