cf3a526b140a9c877d203edc02f7c74fa2224e1d
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2020-11-30 01:00:18 +0530

    Create update_teacher_image.php

---
+++ public/teacher/profile/update_teacher_image.php
@@ -0,0 +1,56 @@
+<?php
+    // $student_id = $_GET['student_id'];
+
+
+    // $id = $_GET['id'];
+
+    // echo $id;
+
+    require_once("../../../private/config/db_connect.php");
+    require("../../../private/config/config.php");
+
+if(isset($_POST['submit_image'])){
+    $id = $_POST['id'];
+   
+    //uploading imgage
+    $files = $_FILES['file'];
+    
+    //testing 
+    //    echo  $id;
+    //    echo "</br>";
+    //    print_r($files);
+
+    //  accesing file details
+        $file_name = $files['name'];
+        $file_error = $files['error'];
+        $filetemp = $files['tmp_name'];
+
+    // breakdown file name and extention
+        // after . will store in var
+        $file_ext = explode('.', $file_name);
+        // make it lowercase
+        $file_check = strtolower(end($file_ext));
+    
+        //file ext store in array which are png, jpeg n jpg
+        $file_ext_store = array('png', 'jpeg', 'jpg');
+
+        if(in_array($file_check,$file_ext_store)){
+            //destination folder
+            $destination_file = '../../img/teacher/profile_pic/' . $file_name;
+            $url = 'img/teacher/profile_pic/' . $file_name;
+            //moving from tem to destination
+            move_uploaded_file($filetemp, $destination_file);
+   
+
+            $query = "UPDATE teachers SET teacher_photo = '$url' WHERE teacher_id=$id"; 
+
+            $_result = mysqli_query($conn, $query);
+
+            header("location: index.php?message=success");
+
+        // $message = "Congratulations! You have successfully updated your profile detail.";
+        
+
+    }
+
+}
