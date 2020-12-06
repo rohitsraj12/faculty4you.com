b36d32e55db10c9f40f2fc12761cc8bb2b007f91
rohitsraj12 <rohitsraj12@gmail.com>
2020-12-06 05:59:11 +0530

    Update personal_info.php

--- public/student/profile/personal_info.php
+++ public/student/profile/personal_info.php
@@ -14,28 +14,55 @@
  
     $student_name = $_SESSION['user_name'];
 
-    $sql = "SELECT students.*, cities.*, states.*, gender.* FROM students 
-    JOIN cities
-        ON cities.city_id = students.city_id
-    JOIN states
-        ON states.state_id = students.state_id
-    JOIN gender
-        ON gender.gender_id = students.gender_id
-     WHERE student_user_name = '$student_name'";
+    $sql = "SELECT * FROM students WHERE student_user_name = '$student_name'";
     // $sql = "SELECT * FROM students WHERE student_user_name = '$student_name'";
 
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_assoc($result);
     $id = $row['student_id'];
-    $email = $row['student_email'];
-    $phone = $row['student_phone'];
-    $address = $row['student_address'];
-    $pin_code = $row['city_pincode'];
-    $city_name = $row['city_name'];
-    $city_id = $row['city_id'];
+    $uname= $row['student_user_name'];
+
+    // echo "sdssd" . $student_name;
+    // $email = $row['student_email'];
+    // $phone = $row['student_phone'];
+    // $address = $row['student_address'];
+    // $pin_code = $row['city_pincode'];
+    // $city_name = $row['city_name'];
+    // $city_id = $row['city_id'];
 
 
     // echo "hi " . $pin_code;
+
+ 
+//  require_once("../../../private/config/db_connect.php");
+//  require("../../../private/config/config.php");
+
+if(isset($_POST['update_personal_info'])){
+    $id = $_GET['id'];
+    $first_name = $_POST['fname'];
+    $last_name = $_POST['lname'];
+    $gender = $_POST['gender'];
+    $dob = $_POST['date'];
+
+    // echo 'id is ' . $id . " " . $first_name ." " . $last_name . " " . $gender . " " . $dob;
+
+            $query = "UPDATE students SET student_first_name = '$first_name',
+            student_last_name = '$last_name',
+            gender_id = $gender,
+            
+            student_date_of_birth = '$dob' WHERE student_id=$id"; 
+
+            $result = mysqli_query($conn, $query);
+
+            echo "<script type='text/javascript'> document.location = 'index.php?message=success'; </script>";
+
+            // header("location: index.php?message=success");
+
+        // $message = "Congratulations! You have successfully updated your profile detail.";
+        
+
+
+}
     
     if(!empty($message)){
 ?>
@@ -58,13 +85,12 @@
                 <heeader class="text-primary-h-3"> 
                     Update profile
                 </header>
-                
             </div>
 
             <div class="section-body">
                 <section class="section-update-form">
-                    <form action="update_student_personal_details.php" method="post" class="section__form section__form-update" enctype="multipart/form-data" onsubmit="return studentValidation()">
-                                <article class="mb-5" >
+                    <form action="" method="post" class="section__form section__form-update" enctype="multipart/form-data" onsubmit="return studentValidation()">
+                        <article class="mb-5" >
                             <header class="article-profile__header p-4 h3 bg-dark text-light m-0">
                                 Primary information
                             </header>
@@ -74,12 +100,12 @@
                                     <div class="form-group col-md-6">
                                     <label for="first_name">First name</label>
                                     <span class="error-msg"></span>
-                                    <input type="text" name="fname" class="form-control name" id="first_name" value="<?php echo $row['student_first_name'];?>" placeholder="Enter your first name">
+                                    <input type="text" name="fname" class="form-control name" id="first_name" placeholder="Enter your first name">
                                     </div>
                                     <div class="form-group col-md-6">
                                     <label for="last_name">Last name</label>
                                     <span class="error-msg"></span>
-                                    <input type="text" name="lname" class="form-control name" id="last_name"  value="<?php echo $row['student_last_name'];?>" placeholder="Enter your last name">
+                                    <input type="text" name="lname" class="form-control name" id="last_name" placeholder="Enter your last name">
                                     </div>
                                 </div>
 
@@ -87,8 +113,7 @@
                                     <div class="col-sm-6">
                                         <label for="dob">Date of birth</label>
                                         <span class="error-msg"></span>
-                                        <input type="date" name="date" class="form-control dob" id="dob"  value="<?php echo $row['student_date_of_birth'];?>" placeholder="<?php echo $row['student_date_of_birth'];?>">
-                                    
+                                        <input type="date" name="date" class="form-control dob" id="dob">
                                     </div>    
                                 </div>
 
@@ -114,17 +139,16 @@
                                                         </div>        
                                                     <?php
                                                 }
-
                                             ?>
-                                          
                                         </div>
                                     </div>
                                 </fieldset>
                                                   
                             </div>
-                            <input type="hidden" name="id" value="<?php echo $id;?>">
+                            <input type="hidden" name="id" value="<?php echo $student_name;?>">
                         </article>
                         <button type="submit" class="button-primary" name="update_personal_info">Submit</button>
+                        <a href="update_student_personal_details.php?id=<?php echo $id;?>"></a>
                     </form>
                 </section>
             </div>
