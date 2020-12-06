cc04f4da28c22334734507cdca7af717a1b1a307
rohitsraj12 <rohitsraj12@gmail.com>
2020-12-06 05:59:24 +0530

    Update update_student_personal_details.php

--- public/student/profile/update_student_personal_details.php
+++ public/student/profile/update_student_personal_details.php
@@ -10,15 +10,15 @@ if(isset($_POST['update_personal_info'])){
     $gender = $_POST['gender'];
     $dob = $_POST['date'];
 
+    echo 'id is ' . $id . " " . $first_name ." " . $last_name . " " . $gender . " " . $dob;
 
-            $query = "UPDATE students SET student_first_name = '$first_name',
-            student_last_name = '$last_name',
-            gender_id = $gender, 
-            student_date_of_birth = '$dob' WHERE student_id=$id"; 
+            // $query = "UPDATE students SET student_first_name = '$first_name',
+            // student_last_name = '$last_name',
+            // gender_id = $gender WHERE student_id=$id"; 
 
-            $_result = mysqli_query($conn, $query);
+            // $result = mysqli_query($conn, $query);
 
-            header("location: index.php?message=success");
+            // header("location: index.php?message=success");
 
         // $message = "Congratulations! You have successfully updated your profile detail.";
         
