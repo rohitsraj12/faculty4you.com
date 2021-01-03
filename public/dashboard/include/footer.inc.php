6646cf8b62a1f33a5c3c0faf37b186615712d744
rohitsraj12 <rohitsraj12@gmail.com>
2021-01-03 22:33:25 +0530

    Update footer.inc.php

--- public/dashboard/include/footer.inc.php
+++ public/dashboard/include/footer.inc.php
@@ -9,6 +9,7 @@
             <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
             <script src="<?php base_url();?>js/dashboard.script.js"></script>
             <script src="<?php base_url();?>js/dashboard/validation.js"></script>
+            <script src="<?php base_url();?>js/dashboard/dashboardScript.js"></script>
             <script>
                 AOS.init();
               </script>
