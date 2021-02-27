504535de0b603da7ef45ec11656044609bb2939e
sanghamitra-dalai <sanghamitradalai65@gmail.com>
2021-02-27 22:40:45 +0530

    Update footer.inc.php

--- public/dashboard/include/footer.inc.php
+++ public/dashboard/include/footer.inc.php
@@ -8,7 +8,9 @@
             <script src="<?php base_url();?>js/owl.carousel.js"></script>
             <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
             <script src="<?php base_url();?>js/dashboard.script.js"></script>
-            <script src="<?php base_url();?>js/dashboard/validation.js"></script>
+            <script src="<?php base_url();?>js/dashboard/validation.js"></script> 
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+            
             <script src="<?php base_url();?>js/dashboard/dashboardScript.js"></script>
             <script>
                 AOS.init();
